ddba4339317fce39362e55347b85dfaf73a4f5eb
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-08 18:10:12 +0000

    Added a function to test if the current platform is Octave or Matlab. Updated mnist1NNdemo to use waitbar iff running Matlab.
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@22 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/mnist1NNdemo.m
+++ demos/mnist1NNdemo.m
@@ -43,16 +43,16 @@ if ntest > 1000
 end
 batches = mat2cell(1:ntest,1,(ntest/nbatches)*ones(1,nbatches));
 ypred = zeros(ntest,1);
-%wbar = waitbar(0,sprintf('%d of %d classified',0,ntest)); % waitbar only works in Matlab
+if ~isOctave(), wbar = waitbar(0,sprintf('%d of %d classified',0,ntest)); end % waitbar only works in Matlab
 %% Classify
 for i=1:nbatches
-    t = toc; %waitbar(i/nbatches,wbar,sprintf('%d of %d Classified\nElapsed Time: %.2f seconds',(i-1)*(ntest/nbatches),ntest,t));
+    t = toc; if ~isOctave(), waitbar(i/nbatches,wbar,sprintf('%d of %d Classified\nElapsed Time: %.2f seconds',(i-1)*(ntest/nbatches),ntest,t));end
     dst = sqDistance(Xtest(batches{i},:),Xtrain,XtestSOS(batches{i},:),XtrainSOS);
     [junk,closest] = min(dst,[],2);
     ypred(batches{i}) = ytrain(closest);
 end
 %% Report
-%close(wbar);
+if ~isOctave(), close(wbar); end
 errorRate = mean(ypred ~= ytest);
 fprintf('Error Rate: %.2f%%\n',100*errorRate);
 t = toc; fprintf('Total Time: %.2f seconds\n',t);
