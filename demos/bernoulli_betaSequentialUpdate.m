f60cbbb1e40b89b707f85c72aa20fe7e1891a776
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-19 22:54:40 +0000

    small cosmetic changes
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@43 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bernoulli_betaSequentialUpdate.m
+++ demos/bernoulli_betaSequentialUpdate.m
@@ -6,13 +6,12 @@
 %% Sample
 setSeed(0);
 mu = 0.7; % 70% probability of success
-n = 100;
+n = 100;  % number of samples 
 X = sum(rand(n, 1) < repmat(mu, n, 1), 2);
-
 %% Update & Plot
 figure; hold on;
 [styles, colors, symbols] =  plotColors();
-ns = [0 5 50 100];
+ns = [0 5 50 100];        
 legendstr = cell(length(ns)+1,1);
 betaPrior = [0.5, 0.5]; % uninformative prior
 for i=1:length(ns)
@@ -23,20 +22,20 @@ for i=1:length(ns)
     nfail = ntrials - nsucc;
     a = betaPrior(1) + nsucc;
     b = betaPrior(2) + nfail;
-    xs = linspace(0, 1, 100); 
+    xs = linspace(0, 1, 50); 
     logkerna = (a-1).*log(xs);       logkerna(a==1 & xs==0) = 0;
     logkernb = (b-1).*log(1-xs);     logkernb(b==1 & xs==1) = 0;
     p = exp(logkerna + logkernb - betaln(a,b));
     plotArgs = {'linewidth', 2};
     plot(colvec(xs), colvec(p), styles{i}, 'linewidth', 2);
-    hold on
     legendstr{i} = sprintf('n=%d', n);
 end
 box on;
 xbar = mean(X);
-pmax = 10;
-h=line([xbar xbar], [0 pmax]); 
-set(h, 'linewidth', 3,'Color','c');
+pmax = 9.95;
+h=line([xbar xbar], [-0.2 pmax]); 
+set(h, 'linewidth', 3, 'Color', 'c');
 legendstr{length(ns)+1} = 'truth';
 legend(legendstr, 'Location', 'NorthWest');
+axis([-0.01,1.01,-0.2,10])
 printPmtkFigure betaSeqUpdate
