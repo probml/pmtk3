7dd97905f6a6cdbc497852d3dc1c364e5b3c3f16
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-19 21:30:07 +0000

    Added acf function listed in PML.
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@40 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ util/acf.m
@@ -0,0 +1,16 @@
+function y=acf(x,lagmax)
+%ACF Autocorrelation function
+% ACF(X,maxlag)
+
+x = x(:)'-mean(x);
+n = length(x);
+if nargin<2
+  lagmax = n-1;
+end
+y  = conv(fliplr(x),x);
+y = y(1:n);
+y = y(n:-1:1)/n;
+s2 = y(1);
+assert(approxeq(s2, var(x,1)))
+y = y/s2;
+y = y(1:lagmax+1);
