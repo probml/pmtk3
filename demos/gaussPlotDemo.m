2bef9057a9e9c53068a9ec9500f3d76fde7c1db1
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-22 19:44:41 +0000

    Uses mvnLogprob now instead of foreign gauss function.
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@75 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/gaussPlotDemo.m
+++ demos/gaussPlotDemo.m
@@ -1,7 +1,7 @@
 %% Plot a Gaussian
 xs = -3:0.01:3;
 mu = 0; sigma2 = 1;
-p = gauss(mu, sigma2, xs');
+p = exp(mvnLogprob(xs', mu, sigma2));
 figure; plot(xs, p,'LineWidth',2.5);
 title('PDF');
 printPmtkFigure gaussian1d
