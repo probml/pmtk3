da3ed110488dac62eca61cdfcba461de4f3f725c
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-22 19:41:55 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@74 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/mvnLogprob.m
+++ toolbox/mvnLogprob.m
@@ -1,13 +1,15 @@
-function logp = mvnLogpdf(X, mu, Sigma)
+function logp = mvnLogprob(X, mu, Sigma)
 % Multivariate Gaussian distribution, log pdf
 % X(i,:) is i'th case
-d = size(X,2);
-XC = bsxfun(@minus,X,rowvec(mu));
-logp = -0.5*sum((XC*inv(Sigma)).*XC,2); %#ok
+
+d = size(X ,2);
+XC = bsxfun(@minus, X, rowvec(mu));
+logp = -0.5*sum((XC*inv(Sigma)).*XC,2);
 logZ = (d/2)*log(2*pi) + 0.5*logdet(Sigma);
 logp = logp - logZ;
-        
-if 0
-  logp2 = log(mvnpdf(X, rowvec(mu), Sigma));
+       
+test = false;
+if test
+  logp2 = log(mvnpdf(X, rowvec(mu), Sigma)); %#ok
   assert(approxeq(logp, logp2))
 end
