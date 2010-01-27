497f5f89389321108bcc1333a70479eb32c73aa1
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-27 23:34:59 +0000

    Switched stats kmeans with kmeansSimple
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@110 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/clusterDemos/kmeansHeightWeight.m
+++ demos/clusterDemos/kmeansHeightWeight.m
@@ -9,11 +9,12 @@ setSeed(1);
 Ks = [2 3];
 for ki=1:length(Ks)
   K = Ks(ki);
-  Z = kmeans(X, K);
+  %Z = kmeans(X, K);
+  [mu, Z] = kmeansSimple(X, K);
   figure;
   for c=1:K
-    ndx = find(Z==c);
-    h=scatter(X(ndx,1), X(ndx,2), 100, marker{c}, 'linewidth', 2); 
+    ndx = Z==c;
+    h=scatter(X(ndx, 1), X(ndx, 2), 100, marker{c}, 'linewidth', 2); 
     hold on;
   end
   xlabel('height'); ylabel('weight')
