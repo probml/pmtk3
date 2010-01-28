5179024298b0dc02277a6aa5dd85cbba58bab1b9
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-28 21:01:25 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@121 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- util/studentTlogprob.m
+++ util/studentTlogprob.m
@@ -13,4 +13,20 @@ function logp = studentTlogprob(X, mu, Sigma, dof)
     v = dof;
     logZ = -gammaln(v/2 + d/2) + gammaln(v/2) + 0.5*logdet(Sigma) + (d/2)*log(v) + (d/2)*log(pi);
     logp = -0.5*(v+d)*log(1+(1/v)*mahal) - logZ;
+    
+    test = true;
+    if test % make sure multivar equations give same results as scalar equations in the scalar case
+        s2 = Sigma;
+        logZ = -gammaln(v/2 + 1/2) + gammaln(v/2) + 0.5 * log(v .* pi .* s2);
+        M = repmat(rowvec(mu), N, 1);
+        S2 = repmat(rowvec(s2), N, 1);
+        V = repmat(rowvec(v), N, 1);
+        LZ = repmat(rowvec(logZ), N, 1);
+        Lij = (-(V+1)/2) .* log(1 + (1./V).*( (X-M).^2 ./ S2 ) ) - LZ;
+        L = sum(Lij,2);
+        assert(approxeq(L, logp))
+    end
+    
+    
+    
 end
