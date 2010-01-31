7a357a180fa46b198abc443933959020e4db4de2
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-01-31 20:03:47 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@142 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/generativeClassifiersDemos/discrimAnalysisDboundariesDemo.m
+++ demos/generativeClassifiersDemos/discrimAnalysisDboundariesDemo.m
@@ -3,14 +3,14 @@
 
 %% Two class linear
 model(1).classPrior  = [1 1]/2;
-model(1).mu          = [1 1 ; -1 -1]';
+model(1).mu          = [1.5 1.5 ; -1.5 -1.5]';
 model(1).Sigma       = repmat(eye(2), [1 1 2]);
 model(1).SigmaPooled = eye(2);
 model(1).type        = 'linear';
 %% Two class quadratic
 model(2).classPrior  = [1 1]/2;
-model(2).mu          = [1 1 ; -1 -1]';
-model(2).Sigma       = cat(3, [2 0; 0 1], eye(2));
+model(2).mu          = [1.5 1.5 ; -1.5 -1.5]';
+model(2).Sigma       = cat(3, [1.5 0; 0 1], 0.7*eye(2));
 model(2).type        = 'quadratic';
 %% Three class linear
 model(3).classPrior  = [1 1 1] /3;
@@ -26,13 +26,15 @@ model(4).type        = 'quadratic'; % i.e. not tied
 %%
 titles = {'Linear Boundary', 'Parabolic Boundary', 'All Linear Boundaries ', 'Some Linear, Some Quadratic'};
 fnames = {'dboundaries2classLinear', 'dboundaries2classParabolic', 'dboundaries3classLinear', 'dboundaries3classParabolic'};
-setSeed(0); nsamples = 30; 
+setSeed(3); nsamples = 30; 
 colors = pmtkColors();
+xyRange = [-10 10 -10 10];
 for i = 1:numel(model)
     [X, y] = mixGaussSample(model(i).mu, model(i).Sigma, model(i).classPrior, nsamples); 
     plotDecisionBoundary(X, y, @(Xtest)discrimAnalysisPredict(model(i), Xtest));
     for j = 1:size(model(i).Sigma, 3)
-        plotDistribution(@(x)gaussLogpdf(x, model(i).mu(:, j), model(i).Sigma(:, :, j)), '-plotArgs', {'LineColor', colors{j}});
+        fn = @(x)exp(gaussLogpdf(x, model(i).mu(:, j), model(i).Sigma(:, :, j)));
+        plotContour(fn, xyRange, 'LineColor', colors{j});
     end
     set(gca, 'XTick', -10:2:10, 'YTick', -10:2:10);
     title(titles{i});
---
+++ graphics/plotContour.m
@@ -0,0 +1,12 @@
+function [h, p] = plotContour(fn, xyRange, varargin)
+% Plot contours of the function evalatuated at xyRange = [xmin xmax ymin ymax]
+% All other args (varargin) are passed directly to the contour function. 
+% Returns, the plot handle h. 
+
+    npoints = 100;
+    [X1, X2] = meshgrid( linspace(xyRange(1), xyRange(2), npoints)', ...
+                         linspace(xyRange(3), xyRange(4), npoints)'  );
+    nr = size(X1, 1);  nc = size(X2, 1);
+    p = reshape(fn([X1(:) X2(:)]), nr, nc);
+    [c, h] = contour(X1, X2, p, varargin{:});    
+end
