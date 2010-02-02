ce317f6ccb8b29e60e416d70cc416af9f1524ca8
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-02 20:43:55 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@161 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- util/addtosystempath.m
+++ util/addtosystempath.m
@@ -1,4 +1,4 @@
-function addtopath(p)
+function addtosystempath(p)
 % Add p to the system path. If p is already on the path, this does nothing.  
 % Note, this change only persists for the duration of the Matlab session.
 % Do not include path delimiters like ; or : .
