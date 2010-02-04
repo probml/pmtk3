3114452df47cf3e2b508045ec8bc60d0e6f8df11
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-04 19:11:55 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@191 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/gaussDemos/gaussPlotDemo.m
+++ demos/gaussDemos/gaussPlotDemo.m
@@ -2,9 +2,9 @@
 xs = -3:0.01:3;
 mu = 0; sigma2 = 1;
 p = gausspdf(xs', mu, sigma2);
-figure; plot(xs, p,'LineWidth',2.5);
+figure; plot(xs, p, 'LineWidth', 2.5);
 title('PDF');
 printPmtkFigure gaussian1d
-figure; plot(xs,cumsum(p),'LineWidth',2.5);
+figure; plot(xs, cumsum(p), 'LineWidth',2.5);
 title('CDF');
 printPmtkFigure gaussianCDF
