0e0dcb8c3ecb50070331ddae585c8ca6533c0b43
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-04 00:41:02 +0000

    Fixed
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@186 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/gaussDemos/gaussBayesUnequalCov.m
+++ demos/gaussDemos/gaussBayesUnequalCov.m
@@ -15,11 +15,13 @@ function helper(Sigmas, suffix)
 y1 = [0 -1]';
 y2 = [1 0]';
 y = [y1(:); y2(:)];
-prior = MvnDist([0 0]', 1e10*eye(2));
+prior.mu = [0 0]';
+prior.Sigma = 1e10*eye(2);
 A = repmat(eye(2), 2, 1);
-py = MvnDist(zeros(4,1), blkdiag(Sigmas{1}, Sigmas{2})); 
-post = softCondition(prior, py, A, y);
+py.mu = zeros(4, 1);
+py.Sigma = blkdiag(Sigmas{1}, Sigmas{2});
 
+post = gaussSoftCondition(prior, py, A, y);
 figure;
 gaussPlot2d(y1, Sigmas{1}, '-color', 'r');
 hh=plot(y1(1), y1(2),  'rx', 'markersize', 12, 'linewidth', 3);
@@ -27,7 +29,7 @@ hold on
 grid off;
 gaussPlot2d(y2, Sigmas{2}, 'g');
 hh=plot(y2(1), y2(2),  'go', 'markersize', 12, 'linewidth', 3);
-mu = post.params.mu; Sigma = post.params.Sigma;
+mu = post.mu; Sigma = post.Sigma;
 gaussPlot2d(mu, Sigma, '-color', 'k');
 hh=plot(mu(1), mu(2),  'k+', 'markersize', 12, 'linewidth', 3);
 axis([-1.5 1.5 -1.5 1.5]);
