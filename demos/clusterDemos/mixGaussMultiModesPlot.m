2938833565cc5128627809a196c17175926c84aa
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-04 19:34:06 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@194 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/clusterDemos/mixGaussMultiModesPlot.m
+++ demos/clusterDemos/mixGaussMultiModesPlot.m
@@ -1,47 +1,31 @@
+%% Plot Mixtures of 2, 2D Gaussians with 1,2, and 3 modes Respectively. 
 
-% 2d, 2 components
-
-model.mixweights = [0.5;0.5];
+model.mixweights = [0.5; 0.5];
 model.K = 2;
- % M = desired num. modes
- for M=1:3
-   
-switch M
-  case 3, 
-    % 3 modes
-    model.mu = [0 0; 1 1]';
-    model.Sigma(:,:,1) = [1 0; 0 0.05];
-    model.Sigma(:,:,2) = [0.05 0; 0 1];
-    xrange = [-3 3 -3 3];
-  case 2, 
-    model.mu = [-1 -1; 1 1]';
-    model.Sigma(:,:,1) = 0.5*eye(2);
-    model.Sigma(:,:,2) = 0.5*eye(2);
-    xrange = [-3 3 -3 3];
-  case 1,
-    model.mu = [-0.5 -0.5; 0.5 0.5]';
-    model.Sigma(:,:,1) = 0.5*eye(2);
-    model.Sigma(:,:,2) = 0.5*eye(2);
-    xrange = [-3 3 -3 3];
+% M = desired num. modes
+for M=1:3    
+    switch M
+        case 3,
+            % 3 modes
+            model.mu = [0 0; 1 1]';
+            model.Sigma(:,:,1) = [1 0; 0 0.05];
+            model.Sigma(:,:,2) = [0.05 0; 0 1];
+            xrange = [-3 3 -3 3];
+        case 2,
+            model.mu = [-1 -1; 1 1]';
+            model.Sigma(:,:,1) = 0.5*eye(2);
+            model.Sigma(:,:,2) = 0.5*eye(2);
+            xrange = [-3 3 -3 3];
+        case 1,
+            model.mu = [-0.5 -0.5; 0.5 0.5]';
+            model.Sigma(:,:,1) = 0.5*eye(2);
+            model.Sigma(:,:,2) = 0.5*eye(2);
+            xrange = [-3 3 -3 3];
+    end
+    plotSurface(@(x)exp(mixGaussLogprob(model, x)), xrange);
+    shading interp
+    view([118, 46])
+    xlabel('x1'); ylabel('x2');
+    printPmtkFigure(sprintf('gmmMultiModes%d', M))
 end
-
-
-stepsize = 0.1;
-[x1,y1] = meshgrid(xrange(1):stepsize:xrange(2), xrange(3):stepsize:xrange(4));
-[nrows,ncols] = size(x1);
-xx = x1(:); yy = y1(:);
-z = exp(mixGaussLogprob(model, [xx,yy]));
-z = reshape(z,nrows,ncols);
-
-figure;
-surf(x1, y1, z);
-shading interp
-view([33, 54])
-xlabel('x1'); ylabel('x2');
-
-%plotDistribution(@(X)mixGaussLogprob(model, X), ...
-%  '-useLog', false, '-useContour', false, '-npoints', 500, '-xrange', xrange);
-
-printPmtkFigure(sprintf('gmmMultiModes%d', M))
- end
- 
+view([150, 42])
--- toolbox/clusterTools/mixGaussLogprob.m
+++ toolbox/clusterTools/mixGaussLogprob.m
@@ -5,9 +5,9 @@ K = model.K;
 N = size(X,1);
 logPost = zeros(N, K);
 for k=1:K
-  modelK.mu = model.mu(:, k); modelK.Sigma = model.sigma(:, :, k);
+  modelK.mu = model.mu(:, k); modelK.Sigma = model.Sigma(:, :, k);
   logPost(:, k) = log(model.mixweights(k)+eps) + ...
-    gaussLogprob(model, X);
+    gaussLogprob(modelK, X);
 end
 logp = logsumexp(logPost, 2);
 
