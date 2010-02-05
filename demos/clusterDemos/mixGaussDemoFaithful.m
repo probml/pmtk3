327f05352b1bc164482beb546f6ac061d8120c3a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-05 18:58:58 +0000

    Added data point coloring
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@214 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/clusterDemos/mixGaussDemoFaithful.m
+++ demos/clusterDemos/mixGaussDemoFaithful.m
@@ -1,39 +1,47 @@
 function mixGaussDemoFaithful
-% reproduce Bishop fig 9.8
-
-close all
-setSeed(0);
-K = 2;
-X = load('faithful.txt');
-X = standardizeCols(X);
-
-% specify initial params to make for a pretty plot
-mu = [-1.5 1.5; 1.5 -1.5]' + 1*randn(2,2);
-Sigma = repmat(0.1*eye(2),[1 1 K]);
-mixweight = normalize(ones(1,K));
-[model, loglikHist] = mixGaussFitEm(X, K, ...
-   'maxIter', 10, 'plotfn', @plotfn,...
-   'mu', mu, 'Sigma', Sigma, 'mixweight', mixweight);
-figure;
-plot(loglikHist, 'o-', 'linewidth', 3)
-xlabel('iter')
-ylabel('average loglik')
+    % reproduce Bishop fig 9.8
+    
+    close all
+    setSeed(0);
+    K = 2;
+    X = load('faithful.txt');
+    X = standardizeCols(X);
+    
+    % specify initial params to make for a pretty plot
+    mu = [-1.5 1.5; 1.5 -1.5]' + 1*randn(2,2);
+    Sigma = repmat(0.1*eye(2),[1 1 K]);
+    mixweight = normalize(ones(1,K));
+    [model, loglikHist] = mixGaussFitEm(X, K, ...
+        'maxIter', 10, 'plotfn', @plotfn,...
+        'mu', mu, 'Sigma', Sigma, 'mixweight', mixweight);
+    figure;
+    plot(loglikHist, 'o-', 'linewidth', 3)
+    xlabel('iter')
+    ylabel('average loglik')
 end
 
 %%%%%%
 
-function plotfn(X,  mu, Sigma, mixweight, post, loglik, iter)
-
-
-str = sprintf('iteration %d, loglik %5.4f\n', iter, loglik);
-figure; plot(X(:,1), X(:,2), '.', 'markersize', 10)
-hold on
-plot(mu(1,:), mu(2,:),'or','linewidth',2);
-K  = size(mu,2);
-for k=1:K
-   gaussPlot2d(mu(:,k), Sigma(:,:,k));
-end
-title(str)
-axis square
-
+function plotfn(X, mu, Sigma, mixweight, post, loglik, iter)
+    
+    str = sprintf('iteration %d, loglik %5.4f\n', iter, loglik);
+    
+    n = size(X, 1); 
+    if isempty(post)
+       colors = repmat([0 1 0], [n, 1]); 
+    else
+       colors = [post(1, :)', zeros(n, 1), post(2, :)']; 
+    end
+    figure; hold on; 
+    for i=1:n
+        plot(X(i, 1), X(i, 2), '.', 'MarkerSize', 10, 'Color', colors(i, :)); 
+    end
+    plot(mu(1,:), mu(2,:),'or','linewidth',2);
+    K  = size(mu,2);
+    for k=1:K
+        gaussPlot2d(mu(:,k), Sigma(:,:,k));
+    end
+    title(str)
+    axis square
+    
 end
