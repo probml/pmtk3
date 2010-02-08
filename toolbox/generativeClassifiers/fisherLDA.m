6017d54da3d8a89d935ef24178f61fe4a68d6217
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-08 05:55:12 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@236 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/generativeClassifiers/fisherLDA.m
@@ -0,0 +1,19 @@
+function [W, Z]  = fisherLDA(Xtrain, ytrain, K)
+% Optimal linear projection from D dimensions to C-1
+% Xtrain is N*D, ytrain(i) is in {1,2,...,C}
+% W is D*K, where K <= C-1
+
+D = size(Xtrain,2);
+C = max(ytrain);
+if nargin < 3, K = C-1; end
+muC = zeros(C,D);
+for c=1:C
+  muC(c,:) = mean(Xtrain(ytrain==c,:),1);
+end
+
+Sw = (Xtrain  - muC(ytrain,:))'*(Xtrain  - muC(ytrain,:));
+muOverall = mean(Xtrain, 1);
+Sb = (ones(C,1)*muOverall-muC)'*(ones(C,1)*muOverall-muC);
+[W,D] = eig(inv(Sw)*Sb);
+W = W(:, 1:K);
+Z = Xtrain*W;
