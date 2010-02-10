3e37c93487e21b968e1f4b6648ca13f333bf1ec9
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-10 20:53:35 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@263 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/linearRegressionDemos/prostateComparison.m
+++ demos/linearRegressionDemos/prostateComparison.m
@@ -1,6 +1,7 @@
- %% Compare L1 vs L2 linear regression on the prostate data set
+function prostateComparison() 
+%% Compare L1, L2, allSubsets, and OLS linear regression on the prostate data set
  
-load prostate
+data = load('prostate.mat');
 includeOffset = true;
 fitFns        = {@(X, y, lambda)linregL1Fit(X, y, lambda, includeOffset) 
                  @(X, y, lambda)linregL2Fit(X, y, lambda, includeOffset)};
@@ -14,11 +15,38 @@ useLogScale = true;
 
 for i=1:numel(fitFns);
     [model, lambdaStar, mu, se] = ...
-        fitCv(lambdas, fitFns{i}, predictFn, lossFn, Xtrain, ytrain, nfolds);
+        fitCv(lambdas, fitFns{i}, predictFn, lossFn, data.Xtrain, data.ytrain, nfolds);
     figure;
-    h = plotCVcurve(lambdas(end:-1:1), mu, se, lambdaStar, useLogScale); 
+    plotCVcurve(lambdas(end:-1:1), mu, se, lambdaStar, useLogScale); 
     xlabel('lambda value');
-    yhat = linregPredict(model, Xtest); 
-    title(sprintf('%s, mseTest = %5.3f', titlePrefixes{i}, lossFn(yhat, ytest)));
+    yhat = linregPredict(model, data.Xtest); 
+    title(sprintf('%s, mseTest = %5.3f', titlePrefixes{i}, lossFn(yhat, data.ytest)));
     printPmtkFigure(figureNames{i});
+end
+
+
+%% All subsets
+    function model = fitFn(X, y, ndx)    
+        model = linregFit(X(:, ndx{:}), y, true);
+        w = zeros(size(X, 2) + 1, 1); 
+        w([1, ndx{:}+1]) = model.w; % +1 for offset   
+        model.w = w;  % make sure w is correct size by using 0's for excluded dims. 
+    end
+d = size(data.Xtrain, 2); 
+ss = powerset(1:d); % 256 models
+[modelFull, ssStar, mu, se] = ...
+        fitCv(ss, @fitFn, predictFn, lossFn, data.Xtrain, data.ytrain, nfolds);
+
+% for plotting purposes, lets look at fewer subsets
+ssSmall = {[], 1, 1:2, 1:3, 1:4, 1:5, 1:6, 1:7, 1:8};
+[model, ssStar, mu, se] = ...
+        fitCv(ssSmall, @fitFn, predictFn, lossFn, data.Xtrain, data.ytrain, nfolds);
+ssStarNdx = cellfind(ssSmall, ssStar) - 1; % -1 since we are counting from 0
+useLogScale = false; 
+figure;
+plotCVcurve(0:8, mu, se, ssStarNdx, useLogScale);
+xlabel('subset size');
+yhat = linregPredict(modelFull, data.Xtest); 
+title(sprintf('%s, mseTest = %5.3f', 'all subsets', lossFn(yhat, data.ytest)));
+
 end
---
+++ util/cellfind.m
@@ -0,0 +1,11 @@
+function ndx = cellfind(cellarray, value)
+% Like find function but for cell arrays
+%
+% example
+%
+% a = {[], 1, 1:2, 1:3, 1:4, 1:5, 1:6, 1:7, 1:8}
+% cellfind(a, 1:3)
+% ans = 4
+    if iscell(value) && ~iscell(cellarray{1}), value = value{:}; end
+    ndx = find(cellfun(@(x)isequal(x, value), cellarray));
+end
