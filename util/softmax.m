aacadb4c598cf0ed4024f3d200b3231a18b74d9a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-12 23:56:58 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@288 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- util/softmax.m
+++ util/softmax.m
@@ -3,5 +3,8 @@ function mu = softmax(eta)
 tmp = exp(eta);
 denom = sum(tmp, 2);
 [D, C] = size(eta); %#ok
-mu = tmp ./ repmat(denom, 1, C);
+%mu = tmp ./ repmat(denom, 1, C);
+mu = bsxfun(@rdivide, tmp, denom);
+
+
 
