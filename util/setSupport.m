136dd21022b322583c3fbaeb49763cd785b6a5e9
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-12 01:40:14 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@272 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ util/setSupport.m
@@ -0,0 +1,20 @@
+function [y, initSupport] = setSupport(labels, newSupport, initSupport)
+% Transform the support the labels from one space to another, i.e.
+% from say [0, 1] to [-1 1] or {'setosa', 'versicolor', virginica'} to
+% 1:3. 
+%
+% This is a generalization of canonizeLabels to arbitary spaces. 
+% 
+% If the input labels do not include at least one example from every
+% class, you must also specify initSupport. 
+%
+% examples:
+%
+% setSupport([1 2 1 1 1 2 1 2 2 2 2 1], [-1, 1])
+% ans =
+%    -1     1    -1    -1    -1     1    -1     1     1     1     1    -1
+    
+    if nargin > 2, opt = {initSupport}; else opt={}; end
+    [yC, initSupport] = canonizeLabels(labels, opt{:});
+    y = reshape(newSupport(yC), size(labels));
+end
