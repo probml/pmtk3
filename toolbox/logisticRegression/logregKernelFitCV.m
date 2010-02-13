96179b708b8e8a700e707e98d592977169786aeb
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-13 00:33:39 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@290 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/logisticRegression/logregKernelFitCV.m
@@ -0,0 +1,32 @@
+function [model, lambdaStar, mu, se] = logregKernelFitCV(X, y, regularizerFn, lambdaRange, kernelParamRange, kernelType, nfolds)
+    
+    if nargin < 3, regularizerFn = @penalizedL2; end
+    if nargin < 4, 
+        lambdaRange = [0, logspace(-2.5, 0, 5), ...
+                          logspace(0.1, 1.5, 10), ...
+                          logspace(1.6, 2.5, 4)];
+    end
+    if nargin < 5, 
+        kernelParamRange = linspace(0.5, 12, 10);
+    end
+    
+    if nargin < 6, kernelType = 'rbf'; end
+    if nargin < 7, nfolds = 5; end
+    
+    Nclasses = numel(unique(y)); 
+    if Nclasses < 3
+       [y, support] = setSupport(y, [-1, 1]); 
+    else
+       [y, support] = setSupport(y, 1:Nclasses); 
+    end
+    
+    parameterSpace = makeModelSpace(lambdaRange, kernelParamRange);
+    
+    coreFitFn = @(X, y, lambda, includeOffset)logregFitCore(X, y, lambda, includeOffset, regularizerFn, Nclasses);    
+    fitFn = @(X, y, params)logregKernelFit(X, y, coreFitFn, params{1}{1}, params{1}{2}, kernelType);
+    lossFn = @(yhat, ytest)mean(yhat ~= ytest); 
+    
+    [model, lambdaStar, mu, se] = ...
+        fitCv(parameterSpace, fitFn, @logregPredict, lossFn, X, y, nfolds);
+    model.ySupport = support; 
+end
