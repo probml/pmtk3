c947350b709c406b85721edb09c4f1a567158dd7
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-13 01:06:37 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@293 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/logisticRegression/logregKernelFitL2CV.m
@@ -0,0 +1,19 @@
+function [model, lambdaStar, mu, se] = logregKernelFitL2CV(X, y, lambdaRange, kernelParamRange, kernelType, nfolds)
+    
+    switch nargin 
+        case 2, args = {};
+        case 3, args = {lambdaRange};
+        case 4, args = {lambdaRange, kernelParamRange};
+        case 5, args = {lambdaRange, kernelParamRange, kernelType};
+        case 6, args = {lambdaRange, kernelParamRange, kernelType, nfolds};
+    end
+    [model, lambdaStar, mu, se] = logregKernelFitCV(X, y, @penalizedL2, args{:}); 
+end
+
+
+
+%#test
+% load crabs
+% model = logregKernelFitL2CV(Xtrain, ytrain)
+% yhat = logregPredict(model, Xtest)
+% nerrors = sum(yhat ~= ytest)
