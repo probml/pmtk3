fb53f4d55c3ce081b638c4d23867c377f04020fe
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-16 21:52:51 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@310 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/varSelDemos/prostateSubsets.m
+++ demos/varSelDemos/prostateSubsets.m
@@ -9,7 +9,7 @@ nfolds    = 10;
         d = size(X, 2);
         model.w     = padZeros(model.w,     ndx{:}, [d, 1]);
         model.Xmu   = padZeros(model.Xmu,   ndx{:}, [1, d]);
-        model.Xstnd = padOnes(model.Xstnd, ndx{:}, [1, d]);
+        model.Xstnd = padOnes(model.Xstnd,  ndx{:}, [1, d]);
     end
 %%    
 [n, d] = size(data.Xtrain); 
@@ -18,14 +18,21 @@ ss(1) = []; % remove empty set
 [model, ssStar, mu, se] = ...
         fitCv(ss, @fitFn, predictFn, lossFn, data.Xtrain, data.ytrain, nfolds);
 
-sz = cellfun(@numel, ss); % sizes of each subset. 
+%% 
+% don't use any features
+fitFn0 = @(X, y)linregFitL2(X, y, 0, 'QR', false);
+[mu0, se0] = cvEstimate(fitFn0, predictFn, lossFn, ones(n, 1), data.ytrain,  nfolds);
+%%
+mu = [mu0, mu]; 
+se = [se0, se];
+sz = [0; cellfun(@numel, ss)]; % sizes of each subset. 
 figure; hold on;
 plot(sz, mu, '.');
-bestScores = zeros(1, d);
-for i=1:d
-    bestScores(i) = min(mu(sz==i));
+bestScores = zeros(1, d + 1);
+for i=0:d
+    bestScores(i+1) = min(mu(sz==i));
 end
-plot(1:d, bestScores, 'ro-', 'MarkerSize', 8, 'LineWidth', 2);
+plot(0:d, bestScores, 'ro-', 'MarkerSize', 8, 'LineWidth', 2);
 xlabel('subset size')
 ylabel('CV error');
 title('all subsets on prostate cancer')
