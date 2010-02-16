2659cc32a09d7b22249bfe3ff04ffb6a10108388
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-16 18:17:24 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@304 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/logisticRegressionDemos/svmLogregComparison.m
+++ demos/logisticRegressionDemos/svmLogregComparison.m
@@ -2,14 +2,19 @@
 %#slow
 load crabs
 %% SVM
-Sigmas = logspace(-1, 1, 20);
+Sigmas = logspace(-1, 0.5, 20);
 Nfolds = 5;
 lossFn = @(yhat, ytest)mean(yhat ~= ytest); 
 [SVMmodel, SigmaStar, SVMmu, SVMse] = fitCv(Sigmas, @svmFit, @svmPredict, lossFn, Xtrain, ytrain,  Nfolds);
 yhat = svmPredict(SVMmodel, Xtest);
 svmNerrors = sum(yhat ~= ytest)
-%% LR
+%% LR L2
 % Cross validates over both lambda and 'rbf' Sigma
-[LRmodel, lambdaStar, LRmu, LRse] = logregKernelFitL2CV(Xtrain, ytrain);
-yhat = logregPredict(LRmodel, Xtest);
-lrNerrors = sum(yhat ~= ytest)
+[LRL2model, lambdaStar, LRmu, LRse] = logregKernelFitL2CV(Xtrain, ytrain);
+yhat = logregPredict(LRL2model, Xtest);
+lrL2Nerrors = sum(yhat ~= ytest)
+%% LR L1
+lambdaL1 = 0.001; SigmaL1 = 8; % use logregKernelFitL1CV to cross validate both - slow
+LRL1model = logregKernelFitL1(Xtrain, ytrain, lambdaL1, SigmaL1);
+yhat = logregPredict(LRL1model, Xtest);
+lrL1Nerrors = sum(yhat ~= ytest)
