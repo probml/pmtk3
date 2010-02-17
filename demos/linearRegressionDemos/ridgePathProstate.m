737264c23e45c39625b324b7842a8c1e64ac5ac3
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-17 20:05:42 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@326 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/linearRegressionDemos/ridgePathProstate.m
+++ demos/linearRegressionDemos/ridgePathProstate.m
@@ -1,12 +1,11 @@
 %% Plot the full L2 regularization path for the prostate data set 
 load prostate
  lambdas = [logspace(4, 0, 20) 0];
- includeOffset = false; 
  [n, d] = size(X);
  nlambdas = numel(lambdas);
  w = zeros(nlambdas, d);
  for i=1:nlambdas
-     model = linregL2Fit(X, y, lambdas(i), includeOffset);
+     model = linregFitL2(X, y, lambdas(i));
      w(i, :) = rowvec(model.w);
  end
 dof = dofRidge(X, lambdas);
