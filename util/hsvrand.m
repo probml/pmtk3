b5f4544d79bcaaaab2364223de45cfb76a3940f1
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-18 23:02:45 +0000

    deleted identical shadowed copies
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@354 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- util/hsvrand.m
+++
@@ -1,7 +0,0 @@
-function colors = hsvrand(N)
-% hsvrand Like built-in HSV, except it randomizes the order, so that adjacent colors are dis-similar
-% function colors = hsvrand(N)
-
-colors = hsv(N);
-perm = randperm(N);
-colors = colors(perm,:);
--- util/subplot2.m
+++
@@ -1,6 +0,0 @@
-function subplot2(nrows, ncols, i, j)
-% function subplot2(nrows, ncols, i, j)
-
-sz = [nrows ncols];
-k = sub2ind(sz(end:-1:1), j, i);
-subplot(nrows, ncols, k);
