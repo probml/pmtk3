438f4ac45d199fe1d9442c18c775c2765157989e
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-19 08:23:58 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@364 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/gaussDemos/gaussImputationDemo.m
+++ demos/gaussDemos/gaussImputationDemo.m
@@ -1,68 +1,77 @@
-function gaussImputationDemo()
-
-
+%% Make data
 setSeed(1);
-d = 10;
+d = 10; n = 100; pcMissing = 0.5;
 mu = randn(d,1); Sigma = randpd(d);
-pcMissing = 0.3;
-
-% Training data is larger than test data, and is missing data at random
-[XfullTrain, XmissTrain] = mkData(mu, Sigma, 100, true, pcMissing);
-
-% Test data omits 'stripe' rather than at random, for easier visualization
-[Xfull, Xmiss, Xhid] = mkData(mu, Sigma, 5, false, pcMissing);
-
-
-[model, LLtrace] = gaussMissingFitEm(XmissTrain, 'verbose', false);
-[XimputeEM, V] = gaussImpute(model, Xmiss);
-conf = 1./V;
-conf(isinf(conf))=0;
-confEM = conf;
-
 trueModel = struct('mu', mu, 'Sigma', Sigma);
-[XimputeTruth, V] = gaussImpute(trueModel, Xmiss);
-conf = 1./V;
-conf(isinf(conf))=0;
-confTruth = conf;
-  
-
-hintonScaleMulti( ...
-   {Xmiss}, {'-map', 'Jet', '-title', 'observed'}, ...
-   {Xhid}, {'-title', 'hidden truth'}, ...
-   {XimputeTruth, confTruth}, {'-title', sprintf('imputed using true params')}, ...
-   {XimputeEM, confEM}, {'-title', sprintf('imputed using estimated params')} ...
-   );
-
-figure(2); printPmtkFigure('mvnImputeObs');
-figure(3); printPmtkFigure('mvnImputeHid');
-figure(4); printPmtkFigure('mvnImputeImputeTruth');
-figure(5); printPmtkFigure('mvnImputeImputeEM');
-
-figure; imagesc(cov2cor(Sigma)); colorbar
-
-end
-
-
-function [Xfull, Xmiss, Xhid, missing] = mkData(mu, Sigma, n, rnd, pcMissing)
-
-
-
-d = length(mu);
-model = struct('mu', mu, 'Sigma', Sigma);
-Xfull = gaussSample(model, n);
-
-if rnd
-   % Random missing pattern
-   missing = rand(n,d) < pcMissing;
-else
-   % Make the first pc% stripes (features) be completely missing
-   missing = false(n,d);
-   missing(:, 1:floor(pcMissing*d)) = true;
-end
-
+Xfull = gaussSample(trueModel, n); 
+missing = rand(n,d) < pcMissing;
 Xmiss = Xfull;
 Xmiss(missing) = NaN;
-Xhid = Xfull;
-Xhid(~missing) = NaN;
 
+%% Impute
+[model, LLtrace] = gaussMissingFitEm(Xmiss, 'verbose', false);
+[XimputeEM] = gaussImpute(model, Xmiss);
+
+% oracle 
+[XimputeTruth] = gaussImpute(trueModel, Xmiss);
+
+
+%% Plotting
+Xmiss0 = Xmiss; Xmiss0(isnan(Xmiss0))=0;
+ndx = 1:20; % just plot first 20 rows
+hintonDiagram(Xmiss0(ndx,:)); title('observed data');
+printPmtkFigure('mvnImputeObs');
+
+if 0
+figure;
+imagesc(XimputeEM(ndx,:) - Xfull(ndx,:));
+colormap('gray'); colorbar
+title('imputed - truth');
+printPmtkFigure('mvnImputeDiff');
 end
+
+if 0
+figure; hintonDiagram(XimputeEM(ndx,:)); title('imputed');
+printPmtkFigure('mvnImputeImpute');
+figure; hintonDiagram(Xfull(ndx,:)); title('truth');
+printPmtkFigure('mvnImputeTruth');
+end
+
+% Scatter plots
+figure; nr = 2; nc = 2;
+for j=1:(nr*nc)
+  subplot(nr, nc, j);
+  miss = find(isnan(Xmiss(:,j)));
+  scatter(Xfull(miss, j), XimputeEM(miss,j))
+  xlabel('truth'); ylabel('imputed');
+  mini = min(Xfull(:,j)); maxi = max(Xfull(:,j));
+  line([mini maxi], [mini maxi]);
+  axis square
+  grid on
+   stats = regstats(Xfull(miss,j), XimputeEM(miss,j));
+  r = stats.rsquare;
+  title(sprintf('R^2 = %5.3f', r))
+end
+suptitle('Imputation using estimated parameters')
+printPmtkFigure('mvnImputeScatterEm');
+
+figure; 
+for j=1:(nr*nc)
+  subplot(nr, nc, j);
+  miss = find(isnan(Xmiss(:,j)));
+  scatter(Xfull(miss, j), XimputeTruth(miss,j))
+  xlabel('truth'); ylabel('imputed');
+  mini = min(Xfull(:,j)); maxi = max(Xfull(:,j));
+  line([mini maxi], [mini maxi]);
+  axis square
+  grid on
+   stats = regstats(Xfull(miss,j), XimputeEM(miss,j));
+  r = stats.rsquare;
+  title(sprintf('R^2 = %5.3f', r))
+end
+suptitle('Imputation using true parameters')
+printPmtkFigure('mvnImputeScatterTrue');
+
+
+
+
