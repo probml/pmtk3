b68b78b24921e77d88436c28c76920698ebbbbba
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-20 01:23:12 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@371 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/distributions/discreteLogprob.m
+++ toolbox/distributions/discreteLogprob.m
@@ -7,16 +7,15 @@ function [L, Lij] = discreteLogprob(model, X)
 %
 % Lij = log p(X(i, j) | params(j))
 % L   = sum(Lij, 2)  % i.e. summed across distributions, (not cases).
-
 X = reshape(X, [], model.d); 
 n = size(X, 1);
 T = model.T;
 if model.K == 2
-    % more efficient method if data is binary, we treat X01 as a mask for T
-    X01 = (X == 2);
+    % more efficient method if data is binary; we treat X as a mask for T
+    X = (X == 2);
     logT = log(T + eps);
-    L0 = bsxfun(@times, logT(1, :), not(X01));
-    L1 = bsxfun(@times, logT(2, :), X01);
+    L1 = bsxfun(@times, logT(2, :), X);
+    L0 = bsxfun(@times, logT(1, :), not(X));
     Lij = L0 + L1;
     L = sum(Lij, 2);
 else
