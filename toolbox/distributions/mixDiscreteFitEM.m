0723ef698e1eeba62d05e886ef2f223ee8a6ae0a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-23 19:43:40 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@374 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/distributions/mixDiscreteFitEM.m
+++ toolbox/distributions/mixDiscreteFitEM.m
@@ -1,5 +1,6 @@
 function model = mixDiscreteFitEM(X, nmix, distPrior, mixPrior)
 % Fit a mixture of products of discrete distributions via EM. 
+% X must be in 1:C. 
 %
 % X(i, j) is the ith case from the jth distribution
 % nmix - the number of mixture components to use
@@ -8,44 +9,32 @@ function model = mixDiscreteFitEM(X, nmix, distPrior, mixPrior)
 %
 % model.T is nstates-by-ndistributions-by-nmixtures
 %
-[n, d] = size(X); 
+%% Setup
+[n, d]  = size(X); 
 nstates = max(X(:)); 
 if nargin < 3, distPrior = ones(nstates, 1); end
-if nargin < 4, mixPrior  = ones(1, nmix); end
-distPrior = colvec(distPrior); mixPrior = rowvec(mixPrior);
+if nargin < 4, mixPrior  = ones(1, nmix);    end
+distPrior = colvec(distPrior); 
+mixPrior  = rowvec(mixPrior);
+%% Initial M-step using Kmeans
 [mu, assign] = kmeansFit(X, nmix); %#ok
-mixweight = normalize(rowvec(histc(assign, 1:nmix)) + mixPrior);
+mixweight = normalize(rowvec(histc(assign, 1:nmix)) + mixPrior - 1);
 T = zeros(nstates, d, nmix); 
 for k=1:nmix
     counts = histc(X(assign==k, :), 1:nstates);
     T(:, :, k) = normalize(bsxfun(@plus, counts, (distPrior-1)), 1);  
 end
-logRik = zeros(n, nmix);
-for k=1:nmix
-    Lij = zeros(n, d);
-    for j=1:d
-        Lij(:, j) = log(T(X(:, j), j, k));
-    end
-    logRik(:, k) = log(mixweight(k)+eps) + sum(Lij, 2);
-end
-L = logsumexp(logRik, 2); 
-logRik = bsxfun(@minus, logRik, L); 
-Rik = exp(logRik);
-currentLL = (sum(L) + sum(log(mixPrior + eps)))/n; 
-display(currentLL);
-converged = false; 
-it = 0;
-maxiter = 1000; 
-while not(converged)
+%% Loop setup
+currentLL = -inf;
+it        = 0; 
+maxiter   = 1000; 
+logRik    = zeros(n, nmix);
+Lij       = zeros(n, d);
+verbose   = true;
+%% Enter EM loop
+while true
     previousLL = currentLL;
-    counts = zeros(nstates, d, nmix); 
-    for k=1:nmix
-        for c=1:nstates
-            counts(c, :, k) = sum(bsxfun(@times, (X == c), Rik(:, k)), 1);
-        end
-    end
-    T = normalize(T,  1);
-    mixweight = normalize(sum(Rik) + mixPrior - 1); 
+    %% E-step
     for k=1:nmix
         for j=1:d
             Lij(:, j) = log(T(X(:, j), j, k) + eps);
@@ -56,13 +45,26 @@ while not(converged)
     logRik = bsxfun(@minus, logRik, L); 
     Rik = exp(logRik);
     currentLL = (sum(L) + sum(log(mixPrior + eps))) / n; 
-    display(currentLL);
+    %% Check convergence
+    if verbose, display(currentLL); end
     it = it+1; 
     if currentLL < previousLL
-        warning('mixDiscreteFitEM:LLdecrease', 'The log likelihood has decreased!');
+        warning('mixDiscreteFitEM:LLdecrease',   ...
+                'The log likelihood has decreased!');
     end
     converged = convergenceTest(currentLL, previousLL) || it > maxiter; 
+    if converged, break; end
+    %% M-step
+    counts = zeros(nstates, d, nmix); 
+    for k=1:nmix
+        for c=1:nstates
+            counts(c, :, k) = sum(bsxfun(@times, (X == c), Rik(:, k)), 1);
+        end
+    end
+    T = normalize(T,  1);
+    mixweight = normalize(sum(Rik) + mixPrior - 1); 
 end
+%% Return the model
 model.T = T;
 model.mixweight = mixweight;
 model.nstates = nstates;
