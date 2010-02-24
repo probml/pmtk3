43b51adf5d4c64e50b4fd64f2721115eab3da278
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-24 21:14:32 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@392 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/linearRegressionDemos/polyfitNoErrorBars.m
+++ demos/linearRegressionDemos/polyfitNoErrorBars.m
@@ -1,19 +1,18 @@
 %% Linear Regression with Polynomial Expansion
 [xtrain, ytrain, xtest, ytestNoisefree, ytest] = polyDataMake('-sampling','thibaux');
 degs = 1:2;
+
 for i=1:length(degs)
     deg = degs(i);
-    m = Linreg;
-    m.transformer =  ChainTransformer({RescaleTransformer, PolyBasisTransformer(deg,false)});
-    m = fit(m, DataTableXY(xtrain, ytrain));
-    ypredTest = predict(m, xtest);
-    figure;
+    lambda = 1e-8;
+    model = linregFitL2(polyBasis(xtrain, deg), ytrain, lambda);
+    ypredTest = linregPredict(model, polyBasis(xtest, deg)); 
+    figure; 
     scatter(xtrain,ytrain,'b','filled');
     hold on;
     plot(xtest, ypredTest, 'k', 'linewidth', 3);
     hold off
     title(sprintf('degree %d', deg))
-    %set(gca,'ylim',[-10 15]);
     set(gca,'xlim',[-1 21]);
 end
 
