160d1afbe2909c60759bc731e87f684cfbe7878a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-26 23:46:08 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@437 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bayesDemos/mixBetaDemo.m
+++ demos/bayesDemos/mixBetaDemo.m
@@ -1,39 +1,39 @@
-%% Mixture of Beta distributions
 function mixBetaDemo()
+%% Mixture of Beta distributions
 
 N = [20 10];
 alphaPrior = [20 20 ; 30 10];
 M = 2;
 mixprior = [0.5 0.5];
 for z=1:M
-  logmarglik(z) = betaln(alphaPrior(z,1) + N(1), alphaPrior(z,2) + N(2)) - ...
-    betaln(alphaPrior(z,1), alphaPrior(z,2));
+    logmarglik(z) = betaln(alphaPrior(z,1) + N(1), alphaPrior(z,2) + N(2)) - ...
+        betaln(alphaPrior(z,1), alphaPrior(z,2));
 end
 mixpost = exp(normalizeLogspace(logmarglik + log(mixprior)));
 for z=1:M
-  alphaPost(z,:) = alphaPrior(z,:) + N;
+    alphaPost(z,:) = alphaPrior(z,:) + N;
 end
 
 
-grid = 0:0.01:1;
+grid = 0.0001:0.01:0.9999;
 post = evalpdf(grid, mixpost, alphaPost);
 prior = evalpdf(grid, mixprior, alphaPrior);
 figure; hold on
 plot(grid, prior, 'r:', 'linewidth', 3)
 plot(grid, post, 'b-', 'linewidth', 3)
-legend('prior', 'posterior')
+legend({'prior', 'posterior'}, 'Location', 'NorthWest')
 title('mixture of Beta distributions')
 printPmtkFigure('mixBetaDemo')
 
 pbiased = 0;
 for k=1:M
-  pbiased = pbiased + mixpost(k)*(1-betacdf(0.5, alphaPost(k,1), alphaPost(k,2)))
+    model.a = alphaPost(k, 1);
+    model.b = alphaPost(k, 2);
+    pbiased = pbiased + mixpost(k)*(1-betainc(0.5, alphaPost(k, 1), alphaPost(k, 2)));
 end
 
-% Now compute pbiased using just mixture component 1
-pbiasedSimple = 1-betacdf(0.5, alphaPost(1,1), alphaPost(1,2))
-
-
+%% Compute pbiased using just mixture component 1
+pbiasedSimple = 1-betainc(0.5, alphaPost(1,1), alphaPost(1,2))
 
 end
 
@@ -42,7 +42,9 @@ function p = evalpdf(thetas, postZ, alphaPost)
 p = zeros(size(thetas));
 M = numel(postZ);
 for k=1:M
-  p = p + postZ(k)*betapdf(thetas, alphaPost(k,1), alphaPost(k,2));
+    model.a = alphaPost(k, 1); 
+    model.b = alphaPost(k, 2);
+    p = p + postZ(k)*exp(betaLogprob(model, thetas)); 
 end
 end
 
