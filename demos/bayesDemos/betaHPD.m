c6fd9c010457633af27d87b22801ffc730de25b2
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-26 22:43:56 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@429 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bayesDemos/betaHPD.m
+++ demos/bayesDemos/betaHPD.m
@@ -1,32 +1,36 @@
-% Plot CI and HPD for beta posterior
-N1 = 2; N = 10; a = N1+1; b  = (N-N1)+1; alpha = 0.05;
-l = betainv(alpha/2, a, b);
-u = betainv(1-alpha/2, a, b);
+%% Plot CI and HPD for beta posterior
+%PMTKstats betainv
+N1      = 2; 
+N       = 10; 
+model.a = N1+1; 
+model.b = (N-N1)+1; 
+alpha   = 0.05;
+
+l = betainv(alpha/2, model.a, model.b);
+u = betainv(1-alpha/2, model.a, model.b);
 CI = [l,u] 
 
-figure;
-[styles, colors, symbols] = plotColors;
-xs = linspace(0, 1, 40);
-ps = betapdf(xs, a, b);
-plot(xs , ps, 'k-', 'linewidth', 2, 'markersize', 12);
-hold on 
 
+[styles, colors, symbols] = plotColors;
+xs = linspace(0.001, 0.999, 40);
+ps = exp(betaLogprob(model, xs));
+figure; hold on 
 HPD = [0.04, 0.485];
 ints  = {CI, HPD};
 linestyles = {'-', ':'};
 for i=1:length(ints)
-  l = ints{i}(1); u = ints{i}(2);
-  pl = betapdf(l, a, b);
-  pu = betapdf(u, a, b);
+  l = ints{i}(1); 
+  u = ints{i}(2);
+  pl = exp(betaLogprob(model, l));
+  pu = exp(betaLogprob(model, u));
   h=line([l l], [0 pl]); 
-  set(h, 'color', colors(i), 'linestyle', linestyles{i}, 'linewidth', 2);
+  set(h, 'color', colors(i), 'linestyle', linestyles{i}, 'linewidth', 3);
   h = line([l u], [pl pu]); 
   hh(i)=h; 
-  set(h, 'color', colors(i),  'linestyle', linestyles{i},'linewidth', 2);
+  set(h, 'color', colors(i),  'linestyle', linestyles{i},'linewidth', 3);
   h=line([u u], [0 pu]); 
-  set(h, 'color', colors(i), 'linestyle', linestyles{i}, 'linewidth', 2);
+  set(h, 'color', colors(i), 'linestyle', linestyles{i}, 'linewidth', 3);
 end
-
 legend(hh, '95% CI', '95% HPD');
-
+plot(xs , ps, 'k-', 'linewidth', 3);
 printPmtkFigure betaHPD;
