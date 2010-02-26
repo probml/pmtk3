dac0392a0b80c769f3d0295a3df3569e07e89717
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-26 02:05:59 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@411 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- util/publishDemos.m
+++ util/publishDemos.m
@@ -1,5 +1,7 @@
 function publishDemos()
 
+wikiFile = 'C:\pmtk3wiki\Demos.wiki';
+
 cd(fullfile(pmtk3Root(), 'demos'));
 d = dirs(); 
 for i = 1:numel(d)
@@ -8,6 +10,10 @@ end
 
 
 
-
-
+googleRoot = 'http://pmtk3.googlecode.com/svn/trunk/docs/demos';
+wikiText = cell(numel(d), 1); 
+for i=1:numel(d)
+    wikiText{i} = sprintf(' * [%s/%s/index.html %s]',googleRoot, d{i}, d{i});
 end
+
+writeText(wikiText, wikiFile); 
