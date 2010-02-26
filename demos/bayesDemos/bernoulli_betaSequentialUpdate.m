43519c6489e044383e61b66ba7eac5f513ea200f
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-26 20:08:09 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@423 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bayesDemos/bernoulli_betaSequentialUpdate.m
+++ demos/bayesDemos/bernoulli_betaSequentialUpdate.m
@@ -5,37 +5,31 @@
 
 %% Sample
 setSeed(0);
-mu = 0.7; % 70% probability of success
-n = 100;  % number of samples 
-X = sum(rand(n, 1) < repmat(mu, n, 1), 2);
+mu = 0.7;  % 70% probability of success
+n  = 100;  % number of samples 
+X  = rand(n, 1) < mu;
 %% Update & Plot
-figure; hold on;
+figure; hold on; box on;
 [styles, colors, symbols] =  plotColors();
 ns = [0 5 50 100];        
-legendstr = cell(length(ns)+1,1);
 betaPrior = [0.5, 0.5]; % uninformative prior
-for i=1:length(ns)
+xs = linspace(0.001, 0.999, 40); 
+for i=1:numel(ns)
     n = ns(i);
     Xsubset = X(1:n);
-    nsucc = sum(Xsubset, 1);
-    ntrials = size(Xsubset, 1);
-    nfail = ntrials - nsucc;
-    a = betaPrior(1) + nsucc;
-    b = betaPrior(2) + nfail;
-    xs = linspace(0, 1, 50); 
-    logkerna = (a-1).*log(xs);       logkerna(a==1 & xs==0) = 0;
-    logkernb = (b-1).*log(1-xs);     logkernb(b==1 & xs==1) = 0;
-    p = exp(logkerna + logkernb - betaln(a,b));
-    plotArgs = {'linewidth', 2};
-    plot(colvec(xs), colvec(p), styles{i}, 'linewidth', 2);
-    legendstr{i} = sprintf('n=%d', n);
+    nsucc   = sum( Xsubset);
+    nfail   = sum(~Xsubset);
+    model.a = betaPrior(1) + nsucc;
+    model.b = betaPrior(2) + nfail;
+    p = exp(betaLogprob(model, xs));
+    name = sprintf('n=%d', n);
+    plot(xs, p, styles{i}, 'LineWidth', 2, 'DisplayName', name);
 end
-box on;
-xbar = mean(X);
-pmax = 9.95;
-h=line([xbar xbar], [-0.2 pmax]); 
-set(h, 'linewidth', 3, 'Color', 'c');
-legendstr{length(ns)+1} = 'truth';
-legend(legendstr, 'Location', 'NorthWest');
-axis([-0.01,1.01,-0.2,10])
+axis([0, 1, -0.2, 10.5])
+h = verticalLine(mean(X), 'LineStyle'   , '--'    , ...
+                          'LineWidth'  , 3       , ...
+                          'Color'      , 'c'     , ...
+                          'DisplayName', 'truth' );
+uistack(h, 'bottom');
+legend('Location', 'NorthWest');
 printPmtkFigure betaSeqUpdate
