560bf06d29e932ab39114081f67a67e61ddfd55a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-26 23:22:44 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@433 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bayesDemos/shrinkageDemoBaseball.m
+++ demos/bayesDemos/shrinkageDemoBaseball.m
@@ -1,13 +1,12 @@
-
-
+%% Baseball Batting Average Shrinkage Estimates
 % Reproduce example from
 % "Data Analysis Using Stein's Estimator and its Generalizations"
 % Bradley Efron; Carl Morris JASA Vol. 70, No. 350. (Jun., 1975), pp.
 % 311-319.
-
-% 1970 batting averages for 18 major league players. Data from p313 of
+%% Data
+% 1970 batting averages for 18 major league players. 
 % first column = batting average for first 45 at bats
-% 2nd column = batting everage for remainder of season
+% 2nd column   = batting everage for remainder of season
 data = [0.400 0.346;
      0.378 0.298
      0.356 0.276
@@ -26,37 +25,37 @@ data = [0.400 0.346;
      0.200 0.285
      0.178 0.316
      0.156 0.200];
-
-   
+%% Data Transformation
 Y = data(:,1);
 p = data(:,2);
 n = 45;
 x = sqrt(n)*asin(2*Y-1); % arcsin transform
 d = length(x);
 xbar = mean(x);
-V = sum((x-xbar).^2);
-s2 = V/d;
+V    = sum((x-xbar).^2);
+s2   = V/d;
 %B = (d-3)/V;% Efron-Morris shrinkage
 sigma2 = 1; % by construction of the arcsin transform
 B = sigma2/(sigma2 + max(0, s2-sigma2)); % EB
 shrunkTransformed = xbar + (1-B)*(x-xbar); 
 shrunk = 0.5*(sin(shrunkTransformed/sqrt(n))+1); % untransform
 MLE = Y;
-
+%% Plot Shrinkage Estimates
 figure;
-plot(MLE, ones(1,d) ,'o');
+plot(MLE, ones(1, d) ,'o');
 hold on
-plot(shrunk, 0*ones(1,d), 'o');
+plot(shrunk, 0*ones(1, d), 'o');
 for i=1:d
   line([Y(i); shrunk(i)], [1; 0]);
 end
 title('MLE (top) and shrinkage estimates (bottom)')
-printPmtkFigure('shrinkageDemoBaseballParams'); 
-
+printPmtkFigure shrinkageDemoBaseballParams; 
+%% Histograms
 figure;
 ndx = 1:5;
-h = bar([p(ndx)'; MLE(ndx)'; shrunk(ndx)']');
-[im_hatch,colorlist] = applyhatch_pluscolor(gcf,'\-x.', 1);
-legend('true', 'MLE', 'shrunk')
+h = bar([p(ndx)'; 
+MLE(ndx)'; shrunk(ndx)']');
+legend({'true', 'MLE', 'shrunk'})
+[im_hatch, colorlist] = applyhatch_pluscolor(gcf,'\-x.', 1);
 title(sprintf('MSE MLE = %6.4f, MSE shrunk = %6.4f', mse(p,MLE), mse(p,shrunk)));
-printPmtkFigure('shrinkageDemoBaseballPred'); 
+printPmtkFigure shrinkageDemoBaseballPred; 
