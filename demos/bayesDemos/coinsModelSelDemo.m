bb0b8a352d32c41592c7b5a762d073609f4739cd
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-02-26 23:32:34 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@436 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bayesDemos/coinsModelSelDemo.m
+++ demos/bayesDemos/coinsModelSelDemo.m
@@ -1,16 +1,17 @@
-
+%% Coins Model Selection Demo
 
 theta = 0.7; N = 5; alpha = 1;
 alphaH = alpha; alphaT = alpha;
 for i=1:(2^N)
-  flips(i,:) = ind2subv(2*ones(1,N), i); % convert i to  bit vector
-  Nh(i) = length(find(flips(i,:)==1));
-  Nt(i) = length(find(flips(i,:)==2));
-  nh = Nh(i); nt = Nt(i);
-  margLik(i) = exp(betaln(alphaH+nh, alphaT+nt) - betaln(alphaH, alphaT));
-  mle = Nh(i)/N;
-  loglik(i) = Nh(i)*log10(mle) + Nt(i)*log10(1-mle);
-  logBF(i) = betaln(alphaH+nh, alphaT+nt) - betaln(alphaH, alphaH) - N*log(0.5);
+    flips(i,:) = ind2subv(2*ones(1,N), i); % convert i to  bit vector
+    Nh(i) = length(find(flips(i,:)==1));
+    Nt(i) = length(find(flips(i,:)==2));
+    nh = Nh(i);
+    nt = Nt(i);
+    margLik(i) = exp(betaln(alphaH+nh, alphaT+nt) - betaln(alphaH, alphaT));
+    mle = Nh(i)/N;
+    loglik(i) = Nh(i)*log10(mle + eps) + Nt(i)*log10(1-mle + eps);
+    logBF(i) = betaln(alphaH+nh, alphaT+nt) - betaln(alphaH, alphaH) - N*log(0.5);
 end
 
 % sort in order of number of heads
@@ -18,8 +19,7 @@ end
 margLik = margLik(ndx);
 logBF = logBF(ndx);
 loglik = loglik(ndx);
-
-
+%%
 figure;
 hold on
 p0 = (1/2)^N;
@@ -29,17 +29,16 @@ set(gca,'xtick', 1:2^N)
 set(gca,'xticklabel',Nh)
 xlabel('num heads')
 title(sprintf('Marginal likelihood for Beta-Bernoulli model, %s p(D|%s) Be(%s|1,1) d%s', ...
-	      '\int', '\theta', '\theta', '\theta'))
+    '\int', '\theta', '\theta', '\theta'))
 printPmtkFigure('joshCoins4');
-
+%%
 figure;
 plot(exp(logBF), 'o-')
 title('BF(1,0)')
 set(gca,'xtick', 1:2^N)
 set(gca,'xticklabel',Nh)
 printPmtkFigure('joshCoins4BF');
-
-
+%%
 BIC1 = loglik - 1;
 figure;
 plot(BIC1, 'o-');
@@ -47,12 +46,10 @@ title('BIC approximation to log_{10} p(D|M1)')
 set(gca,'xtick', 1:2^N)
 set(gca,'xticklabel',Nh)
 printPmtkFigure('joshCoins4BIC');
-
+%%
 figure;
 plot(log10(margLik), 'o-');
 title('log_{10} p(D|M1)')
 set(gca,'xtick', 1:2^N)
 set(gca,'xticklabel',Nh)
 printPmtkFigure('joshCoins4LML');
-
-
