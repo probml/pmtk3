fab9307c2c30ef46daf0a537c8e2cfe5a772ccd5
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-02 17:28:23 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@444 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/other/fitCv.m
+++ toolbox/other/fitCv.m
@@ -1,46 +1,45 @@
-function [model, bestNdx, mu, se] = fitCv(Ks, fitFn, predictFn, lossFn, X, y,  Nfolds, useSErule)
+function [model, bestNdx, mu, se] = fitCv(params, fitFn, predictFn, lossFn, X, y,  Nfolds, useSErule)
 % Fit a set of models of different complexity and use cross validation to pick the best
 %
 % Inputs:
-% Ks is a matrix of tuning parameters,
-% each row corresponds to a different model 
-%  eg Ks = [lambda1(1) lambda2(1); ...
-%           lambda1(N) lambda2(N)]
+% params is a matrix where each row corresponds to a different tuning parameter 
+%  eg models = [lambda1(1) lambda2(1); ...
+%               lambda1(N) lambda2(N)]
 %  You can use the crossProduct function to create this if necessary
-% model = fitFn(Xtrain, ytrain, K), K = vector of tuning parameters
+% model = fitFn(Xtrain, ytrain, param), param = vector of tuning parameters
 % yhat = predictFn(model, Xtest)
-% L = lossFn(yhat, ytest), should return a vector of errors
+% L = lossFn(yhat, ytest), should return a column vector of errors
 % X is N*D design matrix
 % y is N*1
 % Nfolds is number of CV folds; set to N to get LOOCV
 % useSErule: if true,  pick simplest model within 1 standard error
-%  of best; we assume models (rows of Ks) are ordered from simplest to most
-%  complex!
+%  of best; we assume models (rows of params) are ordered from simplest to most
+%  complex.
 %
 % Outputs
 % model  - best model
 % bestNdx - index of best model
-% mu(i) - mean loss for Ks(i,:)
+% mu(i) - mean loss for params(i,:)
 % se(i) - standard error for mu(i,:)
 
 if nargin < 8, useSErule = false; end
 % if params is 1 row vector, it is a probbaly a set of
 % single tuning params
-if size(Ks,1)==1
+if size(params,1)==1
   %warning('fitCV expects each *row* of Ks to containg tuning params')
-  Ks = Ks(:);
+  params = params(:);
 end
-NK = size(Ks,1);
-mu = zeros(1,NK);
-se = zeros(1,NK);
-for ki=1:NK
-  K = Ks(ki,:);
-  [mu(ki), se(ki)] =  cvEstimate(@(X,y) fitFn(X,y,K), predictFn, lossFn, X, y,  Nfolds);
+NM = size(params,1);
+mu = zeros(1,NM);
+se = zeros(1,NM);
+for m=1:NM
+  param = params(m,:);
+  [mu(m), se(m)] =  cvEstimate(@(X,y) fitFn(X,y,param), predictFn, lossFn, X, y,  Nfolds);
 end    
 if useSErule
   bestNdx = oneStdErrorRule(mu, se);
 else
   bestNdx = argmin(mu);
 end
-Kstar = Ks(bestNdx,:);
-model = fitFn(X, y, Kstar);
+bestParam = params(bestNdx,:);
+model = fitFn(X, y, bestParam);
