256d37cdcf58f0e0a472e25e53631f05bed98a1c
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-04 22:46:50 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@466 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/mlcomp/mlcompReadData.m
+++ toolbox/mlcomp/mlcompReadData.m
@@ -1,39 +1,20 @@
 function [X, y, comments] = mlcompReadData(fpath)
-% Convert an mlcomp data file to matlab format. 
+% Convert an mlcomp data file to matlab format.
 
-startswith = @(str, prefix)strncmp(str, prefix, length(prefix));
-raw = getText(fpath);
-iscomment = cellfun(@(s)startswith(strtrim(s), '#'), raw);
+
+raw = cellfun(@strtrim, getText(fpath), 'UniformOutput', false);
+iscomment = cellfun(@(s)s(1) == '#', raw);
 comments = raw(iscomment);
-raw = raw(~iscomment);
-parser = @(s)tokenize(strrep(strtrim(s), ':', ' '));
-raw = cellfun(parser, raw, 'UniformOutput', false);
-d = (max(cellfun(@numel, raw)) - 1)/2;
-n = numel(raw);
-X = zeros(n, d); 
+raw  = raw(~iscomment);
+vals = cellfun(@(c)str2num(strrep(c, ':', ' ')), raw, 'UniformOutput', false);
+n = numel(vals);
+d = max(cellfun(@(c)c(end-1), vals, 'ErrorHandler', @(varargin)0));
+
+X = zeros(n, d);
 y = zeros(n, 1);
 for i=1:n
-   row = raw{i};
-   y(i) = row(1); 
-   for j=2:2:numel(row)
-       X(i, row(j)) = row(j+1);
-   end
+    row = vals{i};
+    y(i) = row(1);
+    X(i, row(2:2:end)) = row(3:2:end);
 end
-
-end
-
-function toks = tokenize(s)
-    if isempty(s)
-        toks = [];
-        return;
-    end
-    [tok, remaining] = strtok(s, ' ');
-    toks = str2double(tok);
-    while ~isempty(remaining)
-        [tok, remaining] = strtok(remaining, ' '); %#ok  octave has no textscan function
-        if ~isempty(tok)
-            toks = [toks, str2double(tok)]; %#ok
-        end
-    end
-    
 end
