58dce041a95aad5013d9f68fb018683150ac6f3e
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-08 03:57:38 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@480 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/svmDemos/svmRegrDemo.m
+++ demos/svmDemos/svmRegrDemo.m
@@ -37,11 +37,11 @@ for method=1:3
        fname = 'linregL1';
       case 3,
          epsilon = 0.0001; % 0.001;
-         [model, SV] = svmSimpleRegrFit(X, y, kernelFn, epsilon, 1/lambda);
+         [model, SV] = svmQPregFit(X, y, kernelFn, epsilon, 1/lambda);
          w = model.alpha;
          lossStr = sprintf('SVM(%s=%6.4f)', '\epsilon', epsilon);
          fname = 'SVM1';
-         yhat = svmSimpleRegrPredict(model, Xtest);
+         yhat = svmQPregPredict(model, Xtest);
    end
    
    
