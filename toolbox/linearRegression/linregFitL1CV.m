b73458926cfac4575de5ee2cda84503a46022044
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-09 23:18:40 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@491 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/linearRegression/linregFitL1CV.m
@@ -0,0 +1,18 @@
+function [model, lambdaStar, mu, se] = linregFitL1CV (X, y, lambdaRange, standardizeX, nfolds)
+% L1 Linear Regression with lambda chosen via cross validation.
+if nargin < 3, lambdaRange = linspace(0, lassoMaxLambda(X, y), 20)'; end
+if nargin < 4, standardizeX = true;  end
+if nargin < 5, nfolds = 5; end
+
+if standardizeX
+    [X, model.Xmu] = center(X);
+    [X, model.Xstnd] = mkUnitVariance(X);
+end
+
+fitFn = @(X, y, lambda)linregFitL1(X, y, lambda, 'lars', false);
+lossFn = @(y, yhat)mean((y-yhat).^2);
+
+[model, lambdaStar, mu, se] = fitCv...
+   (lambdaRange, fitFn, @linregPredict, lossFn, X, y, nfolds, false, true);
+
+end
