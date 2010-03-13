2b963e1dee8d61b651d165dd3fe98fcab07aea30
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-13 03:01:28 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@529 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/svm/svmlightWriteData.m
+++ toolbox/svm/svmlightWriteData.m
@@ -1,19 +1,19 @@
 function svmlightWriteData(X, y, fname, yformat)
 % Write data in svm_light format
 % X is nexamples-by-nfeatures
-% y is nexamples-by-1 and contains only {-1,0,1}
+% y is nexamples-by-1
 % fname is the file name to which the data will be written.
-    
-    [n, d] = size(X); 
-    data = zeros(n, 2*d+1);
-    data(:, 1) = y;
-    data(:, 2:2:end) = repmat(1:d, n, 1); 
-    data(:, 3:2:end) = X;
-    format = repmat([yformat, repmat(' %d:%f', 1, d), '\n'], 1, n);
-    fid = fopen(fname, 'w');
-    data = data';
-    fprintf(fid, format, data(:)); 
-    fclose(fid);
-    
-   
+
+[n, d] = size(X);
+data = zeros(n, 2*d+1);
+data(:, 1) = y;
+data(:, 2:2:end) = repmat(1:d, n, 1);
+data(:, 3:2:end) = X;
+format = [yformat, repmat(' %d:%f', 1, d), '\n'];
+fid = fopen(fname, 'w');
+data = data';
+fprintf(fid, format, data(:));
+fclose(fid);
+
+
 end
