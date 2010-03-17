984718b58ec6ab237db36f1245f0d886909f4882
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-17 23:04:35 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@555 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/svm/svmlibLinearFit.m
+++ toolbox/svm/svmlibLinearFit.m
@@ -1,7 +1,8 @@
 function model = svmlibLinearFit(X, y, C, options)
 % PMTK interface to svm liblinear
 % Supports only classification with a linear kernel.
-
+% Download liblinear from 
+% http://www.csie.ntu.edu.tw/~cjlin/liblinear/#download
 
 if nargin < 4
     if nunique(y) > 2
