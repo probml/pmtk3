3ec0c80dc7add893eaccb23be5b5217a02caaad9
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-23 17:26:10 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@565 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/distributionDemos/normalGammaPenaltyPlotDemo.m
+++ demos/distributionDemos/normalGammaPenaltyPlotDemo.m
@@ -1,8 +1,6 @@
 function normalGammaPenaltyPlotDemo()
-% Plot contours of penalty(w1) + penalty(w2)
-% where penalty(w) = -log p(w)
+%% Plot contours of penalty(w1) + penalty(w2), where penalty(w) = -log p(w)
 % and p(w) = normalGamma(w | delta, c)
-
 %PMTKauthor Francois Caron
 %PMTKmodified Kevin Murphy
 
@@ -17,8 +15,8 @@ figure; hold on
 for i=1:length(deltas)
   delta = deltas(i);
    pen = @(X) normalGammaNeglogpdf(X, delta, c);
-  Z=pen(X) + pen(Y) - pen(1) - pen(pas);
-  contour(X,Y,Z,[0 0], styles{i}, 'linewidth', 2);
+  Z=pen(X(:)) + pen(Y(:)) - pen(1) - pen(pas);
+  contour(X,Y,reshape(Z, size(X)),[0 0], styles{i}, 'linewidth', 2);
   str{i} = sprintf('%s=%3.2f, c=1', '\delta', delta);
 end
 legend(str)
