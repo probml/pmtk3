285aaa0bf9982c33cc4ecd442af23d02fa6d0644
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-03-24 17:59:56 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@579 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/gaussians/gaussLogprobMissingData.m
+++ toolbox/gaussians/gaussLogprobMissingData.m
@@ -1,21 +1,29 @@
-
 function logp = gaussLogprobMissingData(model, X)
 % Same as gaussLogprob, but supports missing data, represented as
 % NaN values distributed through X. X(i, :) is still the ith case
 % and model has the fields mu and Sigma. 
+missRows = any(isnan(X),2);
+complRows = ~missRows;
+ndxMissRows = find(missRows);
+nMiss = sum(missRows);
 mu = model.mu; Sigma = model.Sigma; 
 mu = rowvec(mu); 
 d = size(Sigma, 2);
 X = reshape(X, [], d);
 n = size(X, 1);
-logp = zeros(n, 1); 
-for i=1:n % for each data case, marginalize out the unknown variables
-    vis = ~isnan(X(i, :));
+logpMiss = zeros(nMiss, 1); 
+for i=1:nMiss % for each data case, marginalize out the unknown variables
+    ndx = ndxMissRows(i);
+    vis = ~isnan(X(ndx, :));
     if isempty(vis), continue; end % if no data, leave logp(i) at 0, since the probability of an empty event is 1 and log(1) = 0. 
-    XiVis = X(i, vis); 
+    XiVis = X(ndx, vis); 
     XiVis = XiVis - mu(vis);
     logZ = (d/2)*log(2*pi) + 0.5*logdet(Sigma(vis, vis));
-    logp(i) = -0.5*sum((XiVis*inv(Sigma(vis, vis))).*XiVis, 2) - logZ; 
+    logpMiss(i) = -0.5*sum((XiVis*inv(Sigma(vis, vis))).*XiVis, 2) - logZ; 
 end
+logpCompl = gaussLogprob(model,X(complRows,:));
 
+logp = NaN(n,1);
+logp(complRows) = logpCompl;
+logp(missRows) = logpMiss;
 end
