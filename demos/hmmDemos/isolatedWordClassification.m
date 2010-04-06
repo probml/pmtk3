1ff5efdcdd3dd58bd778a2176f42cd7d4bce2473
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-06 19:12:30 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@596 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/hmmDemos/isolatedWordClassification.m
+++ demos/hmmDemos/isolatedWordClassification.m
@@ -8,14 +8,14 @@ ytrain = [repmat(4, numel(train4), 1) ; repmat(5, numel(train5), 1)];
 [Xtrain, ytrain] = shuffleRows(Xtrain, ytrain);
 Xtest = test45'; 
 ytest = labels'; 
+[Xtest, ytest] = shuffleRows(Xtest, ytest); 
 %% Initial Guess
 pi0 = [1, 0, 0, 0, 0];
 transmat0 = normalize(diag(ones(nstates, 1)) + ...
             diag(ones(nstates-1, 1), 1), 2);
-%% Fit Function 
+%%        
 fitArgs = {'pi0', pi0, 'transmat0', transmat0};
 fitFn   = @(X)gaussHmmFitEm(X, nstates, fitArgs{:}); 
-%%
 model = generativeClassifierFit(fitFn, Xtrain, ytrain); 
 %%
 logprobFn = @gaussHmmLogprob;
