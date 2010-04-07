a5b0864f2ad37bf91d1f8ab7ccd1f618378c2c62
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-07 00:26:46 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@605 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/hmmTools/hmmComputeTwoSlice.m
+++ toolbox/hmmTools/hmmComputeTwoSlice.m
@@ -3,28 +3,28 @@ function [xi_summed, xi] = hmmComputeTwoSlice(alpha, beta, transmat, obslik)
 % alpha(i,t) computed using forwards
 % beta(i,t) computed using backwards
 % transmat(i,j) = Pr(Q(t) = j | Q(t-1)=i)
-% obslik(i,t) = Pr(Y(t)| Q(t)=i)  
+% obslik(i,t) = Pr(Y(t)| Q(t)=i)
 %
 % OUTPUT:
 % xi(i,j,t)  = p(Q(t)=i, Q(t+1)=j | y(1:T)) , t=2:T
-% xi_summed(i,j) = sum_{t=2}^{T} xi(i,j,t) 
+% xi_summed(i,j) = sum_{t=2}^{T} xi(i,j,t)
 
 [K T] = size(obslik);
 if nargout < 2
-  computeXi = 0;
+    computeXi = 0;
 else
-  computeXi = 1;
-  xi = zeros(K,K,T-1);
+    computeXi = 1;
+    xi = zeros(K,K,T-1);
 end
 
 xi_summed = zeros(size(transmat));
 for t=T-1:-1:1
- b = beta(:,t+1) .* obslik(:,t+1);
- tmpXi = normalize((transmat .* (alpha(:,t) * b')));
- xi_summed = xi_summed + tmpXi;
- if computeXi
-   xi(:,:,t) = tmpXi;
- end
+    b = beta(:,t+1) .* obslik(:,t+1);
+    tmpXi = normalize((transmat .* (alpha(:,t) * b')));
+    xi_summed = xi_summed + tmpXi;
+    if computeXi
+        xi(:,:,t) = tmpXi;
+    end
 end
 
 
--- toolbox/hmmTools/hmmDiscreteFitEm.m
+++ toolbox/hmmTools/hmmDiscreteFitEm.m
@@ -3,53 +3,76 @@ function model = hmmDiscreteFitEm(X, nstates, varargin)
 %% Inputs
 % X        - a cell array of observations;
 %            each observation is 1-by-seqLength.
-%            
+%
 %
 % nstates  - the number of hidden states.
 %
 %% Output
 % model is a struct with fields, pi, A, emission, nstates.
 %%
-[ tol       , ...
-    maxIter   , ...
-    verbose   , ...
-    pi0       , ...
-    transmat0 , ...
-    emission0   ...
+[ tol                 , ...
+    maxIter           , ...
+    verbose           , ...
+    pi0               , ...
+    transmat0         , ...
+    emission0         , ...
+    piPseudoCounts    , ...
+    transPseudoCounts , ...
+    obsPseudoCounts     ...
     ] = process_options(varargin ,...
     'tol'      , 1e-7          ,...
     'maxIter'  , 100           ,...
     'verbose'  , true          ,...
     'pi0'      , []            ,...   % initial guess for starting state dist
     'transmat0', []            ,...   % initial guess for the transmat
-    'emission0', []);                 % initial guess for the emission dists
-X = colvec(X); 
+    'emission0', []            ,...   % initial guess for the emission dists
+    'piPseudoCounts'   , []    ,...
+    'transPseudoCounts', []    ,...
+    'obsPseudoCounts'  , []);
+X = colvec(X);
 if ~iscell(X), X = {X}; end
 nobs = numel(X);
-%% Initialize
 stackedData = cell2mat(X')';
 seqidx      = cumsum([1, cellfun(@(seq)size(seq, 2), X')]);
 seqidx      = seqidx(1:end-1);
+
+if isempty(piPseudoCounts)
+    piPseudoCounts = 2*ones(1, nstates);
+end
+if isempty(transPseudoCounts)
+    transPseudoCounts = ones(nstates, nstates);
+end
+piPseudoCounts = rowvec(piPseudoCounts);
+if diff(size(transPseudoCounts))
+    transPseudoCounts = repmat(rowvec(transPseudoCounts), nstates, 1);
+end
+nobsStates = nunique(stackedData);
+if isempty(obsPseudoCounts)
+    obsPseudoCounts = 2*ones(nobsStates, 1);
+end
+
+
+%% Initialize
 if isempty(transmat0)
-    transmat = normalize(rand(nstates, nstates), 2); % Each row sums to one
+    transmat = normalize(rand(nstates, nstates) + transPseudoCounts -1, 2); % Each row sums to one
 else
     transmat = transmat0;
 end
 if isempty(pi0)
-    startDist = normalize(rand(1, nstates));
+    startDist = normalize(rand(1, nstates) + piPseudoCounts - 1);
 else
     startDist = pi0;
 end
 if isempty(emission0)
-    % 
+    % init each emission dist on a random partition of the data, ignoring
+    % temporal structure.
     emission = cell(nstates, 1);
-    dataPart = randsplit(colvec(stackedData), nstates); 
-    nobsStates = nunique(stackedData);
+    dataPart = randsplit(colvec(stackedData), nstates);
+    
     for i=1:nstates
         data = dataPart{i};
-        randWeights = normalize(rand(length(data), 1)); 
-        pseudoCounts = 3*ones(nobsStates, 1); 
-        emission{i} = discreteFit(data, pseudoCounts, randWeights, nobsStates);
+        randWeights = normalize(rand(length(data), 1));
+        emission{i} = discreteFit(data, obsPseudoCounts, randWeights, nobsStates);
     end
 else
     emission = emission0;
@@ -65,9 +88,9 @@ while true
     [currentLL, startCounts(:), transCounts(:), weights(:)] = deal(0);
     for i=1:nobs
         obs       = colvec(X{i});
-        m.pi      = startDist; m.emission = emission; 
-        m.nstates = nstates  ; m.A        = transmat; 
-        [gamma, loglik, alpha, beta, B] = hmmDiscreteInfer(m, obs); 
+        m.pi      = startDist; m.emission = emission;
+        m.nstates = nstates  ; m.A        = transmat;
+        [gamma, loglik, alpha, beta, B] = hmmDiscreteInfer(m, obs);
         currentLL = currentLL + sum(loglik);
         %% Distribution over starting states
         startCounts = startCounts + gamma(:, 1)';
@@ -80,12 +103,11 @@ while true
         ndx = idx:idx+sz-1;
         weights(ndx, :) = weights(ndx, :) + gamma';
     end
-    startDist = normalize(startCounts);
-    transmat  = normalize(transCounts, 2);
+    startDist = normalize(startCounts + piPseudoCounts - 1);
+    transmat  = normalize(transCounts + transPseudoCounts - 1, 2);
     %% Observation distributions
     for j=1:nstates
-        alpha = 1; % pseduo counts
-        emission{j} = discreteFit(colvec(stackedData), alpha, weights(:, j));
+        emission{j} = discreteFit(colvec(stackedData), obsPseudoCounts, weights(:, j));
     end
     %% Check Convergence
     if verbose, fprintf('%d\t loglik: %g\n', it, currentLL ); end
