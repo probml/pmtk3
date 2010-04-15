b47d7fd36dc2119e262bdaefcaeffd57c97daaaa
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-15 16:40:19 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@642 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- util/pmlMissingCodeReport.m
+++ util/pmlMissingCodeReport.m
@@ -15,7 +15,7 @@ function [missing, pg] = pmlMissingCodeReport(bookSource, includeCodeSol)
 %
 %  *** Also displays an html table ***
 %%
-
+% Don't include these functions in the report
 ignoreList = ['polya-fit'; % In light-speed
               'lm';        % R function (ours is called linearRegressionFrequentist)
               ];
