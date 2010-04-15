717326e1de8a1ecaf137798ab4a0e50c2263de81
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-15 18:42:20 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@649 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- util/pmlOrganizeCode.m
+++ util/pmlOrganizeCode.m
@@ -132,5 +132,7 @@ function chname = processChapterNames(chname)
 % make sure the chapter names are valid directory names
 chname = cellfuncell(@(c)regexprep(c, '[,()\[\]:;&%$#@!`?]', ''), chname);
 chname = cellfuncell(@(c)strrep(strtrim(c), '  ', ' '), chname);
+chname = cellfuncell(@(c)strrep(strtrim(c), 'unfinished', ''), chname);
+chname = cellfuncell(@(c)strrep(strtrim(c), 'Unfinished', ''), chname);
 chname = cellfuncell(@(c)strrep(strtrim(c), ' ', '_'), chname);
 end
