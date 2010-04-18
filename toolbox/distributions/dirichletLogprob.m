fc1115ca383396f24766c8f21ca9fb210522afd3
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-18 00:45:17 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@677 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/distributions/dirichletLogprob.m
+++ toolbox/distributions/dirichletLogprob.m
@@ -1,15 +1,15 @@
 function logp = dirichletLogprob(model, X)
 % logp(i) = log p(X(i, :) | model.alpha)
 %
-% *** X is appended with [X, 1-sum(X, 2)] ***
+% *** X is automatically appended with [X, 1-sum(X, 2)] ***
 %
-% if model.alpha is a scalar it is automatically replicated to the right
+% If model.alpha is a scalar, it is automatically replicated to the right
 % size. 
 %
-% *** Values outside of the simplex are given NaN values, not -Inf ***
-
-
-
+%*** Note, values outside of the simplex are given NaN values, not -Inf ***
+%    This makes it easier to plot, as NaN values are just ignored by the
+%    plot functions. Use logp(isnan(logp)) = -Inf if desired. 
+%%
 X = [X, 1-sum(X, 2)];
 K = size(X, 2); 
 alpha = colvec(model.alpha); 
@@ -20,9 +20,6 @@ logp = log(X)*(alpha-1);
 logp = logp + gammaln(sum(alpha)) - sum(gammaln(alpha)); 
 
 logp(X(: , end) < 0 | sum(X, 2) ~= 1) = NaN;
-
-
-
 
 end
 
