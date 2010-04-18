3a0533bf5eef4b8be4ab5b10d40c3d222ebae83e
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-18 20:06:39 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@682 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/distributions/invWishartSample.m
+++ toolbox/distributions/invWishartSample.m
@@ -1,27 +1,16 @@
 function S = invWishartSample(model, n)
-% Return n samples from the inverse Wishart distribution with parameters
-% model.Sigma, and model.dof.
-%
-% The ith sample is S(:, :, i)
-%
-%%
+% S(:, :, 1:n) ~ IW(model.Sigma, model.dof)
+
 if nargin < 2, n = 1; end
 Sigma = model.Sigma;
 dof   = model.dof;
 d     = size(Sigma, 1);
-
-C = chol(Sigma);
-chi2Model.dof = dof-(0:d-1);
-
-S = zeros(d, d, n);
+C     = chol(Sigma)';
+S     = zeros(d, d, n);
 for i=1:n
-    if (dof <= 81+d)
-        X = randn(dof, d);
-    else
-        X = diag(sqrt(chi2Sample(chi2Model,d)))+setdiag(triu(randn(d)), 0);
-    end
-    [Q, R] = qr(X, 0);
-    M = C' / R;
+    Z = randn(dof, d);
+    [Q, R] = qr(Z, 0);
+    M = C / R;
     S(:, :, i) = M*M';
 end
 end
