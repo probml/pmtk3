eda4bf83a05fedeff5243b291f9436dc65a83abd
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-20 16:41:17 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@687 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ demos/Logistic_regression/softmaxDemo2.m
@@ -0,0 +1,12 @@
+%% Plot the softmax function as a histogram for various temperatures 
+T = [100 10 1 0.1];
+eta = [3 0 1];
+figure();
+nr = 1; 
+nc = numel(T);
+for i=1:numel(T)
+    subplot(nr, nc, i)
+    bar(softmax(eta./T(i))); 
+    title(sprintf('T = %g', T(i)));
+end
+printPmtkFigure('softmaxDemo2'); 
