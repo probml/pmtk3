7fe009c277451ffc64cb5543cae9459e63e6b32a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-20 21:32:53 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@697 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/Linear_regression/gammaPlotDemo.m
+++ demos/Linear_regression/gammaPlotDemo.m
@@ -5,14 +5,19 @@ b = 1;
 bs = b*ones(1,length(as));
 figure;
 [styles, colors, symbols] = plotColors;
+legendStr = cell(1, length(as)); 
 for i=1:length(as)
     a = as(i); b = bs(i);
-    xs = linspace(0, 7, 40);
-    ps = gampdf(xs, a, 1/b);
-    plot(xs , ps, styles{i}, 'linewidth', 2, 'markersize', 12);
+    xs = linspace(0.1, 7, 40);
+    model.a = a; 
+    model.b = b; 
+    ps = exp(gammaLogprob(model, xs));
+    style = [styles{i}, colors(i), symbols(i)];
+    plot(xs , ps, style, 'linewidth', 2.5, 'markersize', 6);
     hold on
     legendStr{i} = sprintf('a=%2.1f,b=%2.1f', a, b);
+    axis tight; 
 end
 legend(legendStr, 'fontsize', 14);
 title('Gamma distributions')
-printPmtkFigure('gammaDist'); 
+printPmtkFigure('gammaDistb1'); 
