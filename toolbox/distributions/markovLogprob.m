2ea53cebd15745a4262512761a491ee3d0c3c4d5
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-21 22:38:44 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@712 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/distributions/markovLogprob.m
@@ -0,0 +1,23 @@
+function logp = markovLogprob(model, X)
+% logp(i)       = log p(X(i, :) | model.pi, model.A)
+% model.pi(j)   = p(S(1) = j)
+% model.A(j, k) = p(S(t) = k | S(t-1) = j)
+%
+% where S(t) denotes the state at time step t.
+%
+% X(m, n) is in 1:length(model.pi), i.e. 1:nstates
+%
+N = size(X, 1);
+logA = log(model.A + eps);
+logPi = colvec(log(model.pi + eps));
+nstates = length(logPi);
+logPrior = logPi(X(:, 1)); 
+
+logp = zeros(N, 1);
+for i=1:N
+    Njk = accumarray([X(i, 1:end-1)', X(i, 2:end)'], 1, [nstates, nstates]);
+    logp(i) = sum(sum(Njk.*logA));
+end
+logp = logp + logPrior; 
+end
+
