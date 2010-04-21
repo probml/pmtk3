338d53676834d38069a5ed93e9dc2f88f1be47b9
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-21 19:42:31 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@708 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ demos/Model_selection_and_regularization/linregPolyVsN.m
@@ -0,0 +1,46 @@
+%% Plot performance of linear regression vs sample size
+setSeed(0);
+degrees = [1, 2, 15];
+for d=1:numel(degrees)
+    deg = degrees(d);
+    lambda = 1e-3;
+    ns = linspace(10, 200, 10);
+    Nns = length(ns);
+    testMse = zeros(1, Nns); 
+    trainMse = zeros(1, Nns);
+    for i=1:length(ns)
+        %% Generate data
+        n=ns(i);
+        [xtrain, ytrain, xtest, ytestNoiseFree, ytest, sigma2] = ...
+            polyDataMake('n', n, 'sampling', 'thibaux');
+        %% Transform data
+        [xtrain, minx, rangex] = rescaleData(xtrain, -1, 1); 
+        xtest = rescaleData(xtest, -1, 1, minx, rangex); 
+        xtrain = addOnes(degexpand(xtrain, deg)); 
+        xtest  = addOnes(degexpand(xtest, deg)); 
+        %% Fit 
+        model = linregFit(xtrain, ytrain, 'lambda', lambda, ...
+            'standardizeX', false);
+        %% Predict
+        yhatTrain = linregPredict(model, xtrain); 
+        trainMse(i) = mean((ytrain - yhatTrain).^2);
+        yhatTest = linregPredict(model, xtest); 
+        testMse(i) = mean((ytest - yhatTest).^2); 
+    end
+    %% Plot
+    figure();
+    hold on
+    ndx = ns;
+    plot(ndx, trainMse, 'bs:', 'linewidth', 2, 'markersize', 12);
+    plot(ndx, testMse, 'rx-', 'linewidth', 2, 'markersize', 12);
+    legend('train', 'test')
+    ylabel('mse')
+    xlabel('size of training set')
+    title(sprintf('truth=degree 2, model = degree %d', deg));
+    set(gca,'ylim',[0 22]);
+    box on;
+    %line([0 max(ns)], [sigma2 sigma2],'color', 'k', 'linewidth', 3);
+    printPmtkFigure(sprintf('polyfitN%d', deg));
+end
+
+
