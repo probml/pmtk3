f07aff8011b46c2099c9f42e221cbb9387accab6
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-21 23:22:37 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@714 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/other/knnClassify.m
+++ toolbox/other/knnClassify.m
@@ -1,45 +1,31 @@
 function [ypred, ypredProb] = knnClassify(Xtrain, ytrain, Xtest, K)
-% function [ypred, ypredProb] = knnClassify(Xtrain, ytrain, Xtest, K)
-% Xtrain(n,:) = n'th example (d-dimensional)
+% K-nearest-neighbour classifier
+% Xtrain(n, :) = n'th example (d-dimensional)
 % ytrain(n) in {1,2,...,C} where C is the number of classes
-% Xtest(m,:)
-% ypred(m) in {1,2..,C} is most likely class (ties broken by picking lowest class)
-% ypredProb(m,:) is the empirical distribution over classes
-
-Ntest = size(Xtest, 1);
+% Xtest(m, :)
+% ypred(m) in {1,2..,C} is most likely class 
+% (ties broken by picking lowest class)
+% ypredProb(m, :) is the empirical distribution over classes
+%%
 Ntrain = size(Xtrain, 1);
 Nclasses = max(ytrain);
-if K>Ntrain
-  fprintf('reducing K = %d to Ntrain = %d\n', K, Ntrain-1);
-  K = Ntrain-1;
+if K > Ntrain
+    fprintf('reducing K = %d to Ntrain = %d\n', K, Ntrain-1);
+    K = Ntrain - 1;
 end
-dst = sqdist(Xtrain', Xtest'); % dst(n,m) = || Xtrain(n,:) - Xtest(m,:) || ^2
+% dst(n, m) = || Xtrain(n, :) - Xtest(m, :) || ^2
+dst = sqDistance(Xtrain, Xtest); 
 if K==1
-
-  [junk, closest] = min(dst,[],1); %#ok
-  ypred = ytrain(closest);
-  ypredProb = oneOfK(ypred, Nclasses); %# delta function
-
-else
-  
-  if 0
-    % loop over test cases
-    for m=1:Ntest
-      [vals, closest] = sort(dst(:,m));
-      labels = ytrain(closest(1:K));
-      votes = hist(labels, 1:Nclasses);
-      ypredProb(m,:) = normalize(votes);
-      [prob, ypred(m)] = max(ypredProb(m,:));
-    end
-  end
-  
-  % vectorize over test cases
-  % for column m, the first K rows are the distances to closest training points
-  [vals, closest] = sort(dst); %#ok
-  labels = ytrain(closest(1:K, :)); % K*M
-  votes = hist(labels, 1:Nclasses); % hist over columns, C*M
-  ypredProb = normalize(votes, 1)'; % M*C
-  [prob, ypred] = max(ypredProb, [], 2); %#ok
+    closest = minidx(dst, [], 1); 
+    ypred = ytrain(closest);
+    ypredProb = oneOfK(ypred, Nclasses); % delta function
+else    
+    % vectorize over test cases for column m, the first K rows are the
+    % distances to closest training points
+    closest       = sortidx(dst); 
+    labels        = ytrain(closest(1:K, :));   % K*M
+    votes         = histc(labels, 1:Nclasses); % hist over columns, C*M
+    ypredProb     = normalize(votes, 1)';      % M*C
+    [prob, ypred] = max(ypredProb, [], 2); 
 end
-
 end
