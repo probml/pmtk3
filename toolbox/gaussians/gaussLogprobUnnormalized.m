58ba38005f2c7c070a6cd72c6a232789e9652e06
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-22 18:25:11 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@730 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/gaussians/gaussLogprobUnnormalized.m
@@ -0,0 +1,14 @@
+function logp = gaussLogprobUnnormalized(model, X)
+% Same as gaussLogprob, but does not normalize logp - used by e.g. mcmc.
+
+
+mu = model.mu; Sigma = model.Sigma;
+d = size(Sigma, 2);
+X = reshape(X, [], d);
+X = bsxfun(@minus, X, rowvec(mu));
+logp = -0.5*sum((X/Sigma).*X, 2);
+
+
+
+
+end
