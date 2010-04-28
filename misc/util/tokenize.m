1d9e71d654cc46b6613f2818b93c81a84b8cc4e6
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-28 21:45:22 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@782 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- misc/util/tokenize.m
+++ misc/util/tokenize.m
@@ -1,8 +1,40 @@
-function tokens = tokenize(str,delimiter)
-% tokenize a string    
-    if(nargin < 2)
-       delimiter = ' ' ; 
-    end
-    tokens = textscan(str,'%s','delimiter',delimiter, 'bufsize', 100000);
-    tokens = tokens{:};
+function tokens = tokenize(str, delimiter)
+% tokenize a string
+% If delmiter is more than one character, it is assumed that you want
+% to tokenize based on multiple delimiters - if not use regexp instead. 
+% 
+% Empty cells are removed
+%% Example
+% str = 'a;man;a;,plan;a ,,,canal;panama! '
+% delimiters = ';, '; % split at ;, or space
+% tokenize(str, delimiters)
+% ans = 
+%     'a;'
+%     'man'
+%     'a'
+%     'plan'
+%     'a'
+%     'canal'
+%     'panama!'
+%
+% Note, Matlab's regexp does a lot of this work automatically if you use
+% 'split' mode, but Octave does not support this, hence the following code.
+
+if(nargin < 2)
+    delimiter = ' ' ;
+end
+delimiter = ['[',delimiter, ']'];
+
+[start, finish] = regexp(str, delimiter);
+if isempty(start)
+    tokens = {str};
+    return
+end
+tokens = cell(numel(start+1), 1);
+tokens{1} = str(1:start(1));
+start = [start, length(str)];
+for i=1:numel(finish)
+    tokens{i+1} = str(finish(i)+1:start(i+1)-1);
+end
+tokens = filterCell(tokens, @(c)~isempty(c));
 end
