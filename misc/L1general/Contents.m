2071a75600258dd91c1b4d77ecbee1144d7753e9
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-28 17:04:30 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@774 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ misc/L1general/Contents.m
@@ -0,0 +1,3 @@
+% Optimization routines for L1 regularized objectives
+%PMTKauthor Mark Schmidt
+%PMTKurl http://pages.cs.wisc.edu/~gfung/GeneralL1/
--- toolbox/Model_selection_and_regularization/larsLibrary/Contents.m
+++ toolbox/Model_selection_and_regularization/larsLibrary/Contents.m
@@ -1,5 +1,5 @@
-
+% Methods for L2 regularized linear regression and Elastic net
 %PMTKauthor Karl Skoglund
 %PMTKurl http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=3897
-%IMM, DTU, kas@imm.dtu.dk
+
 
