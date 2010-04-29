a0730a94291102853c7a4dcba12d7b7d6752ab22
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-04-29 16:41:23 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@808 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- misc/metatools/publishDemos.m
+++ misc/metatools/publishDemos.m
@@ -19,16 +19,18 @@ end
 
 
 
-googleRoot = 'http://pmtk3.googlecode.com/svn/trunk/docs/demos';
+googleRoot = 'http://pmtk3.googlecode.com/svn/trunk/docs/demoOutput';
 wikiText = cell(numel(d), 1);
 for i=1:numel(d)
-    wikiText{i} = sprintf(' * [%s/%s/index.html %s]',googleRoot, d{i}, d{i});
+    if ~dirEmpty(d{i})
+        wikiText{i} = sprintf(' * [%s/%s/index.html %s]',googleRoot, d{i}, d{i});
+    end
 end
-
+wikiText = filterCell(wikiText, @(c)~isempty(c)); 
 writeText(wikiText, wikiFile);
 if svnAutomatically
     system(sprintf('svn ci %s -m "auto-updated by publishDemos"', wikiFile));
-    docdir = fullfile(pmtk3Root(), 'docs', 'demos');
+    docdir = fullfile(pmtk3Root(), 'docs', 'demoOutput');
     system(sprintf('svn ci %s -m "auto-updated by publishDemos"', docdir));
 end
 end
