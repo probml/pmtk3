7355961487954427cfbc331e24f36b357b6422f9
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-03 03:26:16 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@860 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ misc/util/deconstructTest.m
@@ -0,0 +1,26 @@
+function deconstructTest()
+
+
+model.mu = zeros(1, 10); 
+model.Sigma = randpd(10); 
+model.pi = normalize(ones(1, 10)); 
+
+
+tic
+for i=1:1000
+
+[pi, Sigma, mu] = deconstruct(model);
+end
+t = toc
+
+
+tic
+for i=1000
+   pi = model.pi;
+   Sigma = model.Sigma; 
+   mu = model.mu;
+end
+t2 = toc
+
+
+end
