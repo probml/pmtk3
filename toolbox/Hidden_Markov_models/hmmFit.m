26edeacc014ac7abce801971129773b832b78842
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-07 22:01:38 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@906 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Hidden_Markov_models/hmmFit.m
+++ toolbox/Hidden_Markov_models/hmmFit.m
@@ -61,8 +61,8 @@ model.type = type;
     'pi0'                       , []                        , ...
     'trans0'                    , []                        , ...
     'emission0'                 , []                        , ...
-    'piPrior'                   , ones(1, nstates)        , ...
-    'transPrior'                , ones(nstates, nstates)  , ...
+    'piPrior'                   , 2*ones(1, nstates)        , ...
+    'transPrior'                , 2*ones(nstates, nstates)  , ...
     'emissionPrior'             ,[]);
 model.piPrior = rowvec(model.piPrior);
 if diff(size(model.transPrior))
@@ -72,18 +72,20 @@ end
 switch lower(type)
     case 'gauss'
         initFn = @(data)initGauss(data, model);
-        [model, loglikHist] = emAlgo(data, initFn, @estepGauss, ...
+        estepFn = @(model, data)estep(model, data, @estepGaussEmission);
+        [model, loglikHist] = emAlgo(data, initFn, estepFn, ...
                                     @mstepGauss, [], EMargs{:});
     case 'discrete'
         initFn = @(data)initDiscrete(data, model);
-        [model, loglikHist] = emAlgo(data, initFn, @estepDiscrete,...
+        estepFn = @(model, data)estep(model, data, @estepDiscreteEmission);
+        [model, loglikHist] = emAlgo(data, initFn, estepFn,...
                                     @mstepDiscrete, [], EMargs{:});
     otherwise
         error('%s is not a valid output distribution type');
 end
 end
 
-%% Gauss
+%% INIT
 function model = initGauss(data, model)
 %% Initialize Gaussian model
 model.d = size(data{1}, 1);
@@ -105,12 +107,32 @@ if isempty(model.emission)
 end
 end
 
-function [ess, loglik] = estepGauss(model, data)
-%% Compute expected sufficient statistics
+function model = initDiscrete(data, model)
+% Initialize the model
+model.d = 1;
+model.nObsStates = nunique(cell2mat(data')');
+nstates = model.nstates;
+nObsStates = model.nObsStates;
+if isempty(model.emissionPrior)
+    model.emissionPrior = 2*ones(nstates, model.nObsStates);
+end
+if isempty(model.pi)
+    model.pi = normalize(rand(1, nstates) + model.piPrior);
+end
+if isempty(model.A)
+    model.A = normalize(rand(nstates, nstates) + model.transPrior, 2);
+end
+if isempty(model.emission)
+    model.emission = normalize(rand(nstates, nObsStates), 2);
+end
+end
+%% ESTEP
+function [ess, loglik] = estep(model, data, emissionEstep)
+% Compute the expected sufficient statistics. 
 stackedData   = cell2mat(data')';
 seqidx        = cumsum([1, cellfun(@(seq)size(seq, 2), data')]);
 seqidx        = seqidx(1:end-1);
-[nstacked, d] = size(stackedData);
+nstacked      = size(stackedData, 1);
 nstates       = model.nstates;
 startCounts   = zeros(1, nstates);
 transCounts   = zeros(nstates, nstates);
@@ -133,19 +155,37 @@ end
 logprior = log(A(:)+eps)'*(model.transPrior(:)-1) + ...
            log(model.pi(:)+eps)'*(model.piPrior(:)-1);
 loglik = loglik + logprior;        
-wsum = sum(weights, 1);
+ess = structure(weights, startCounts, transCounts); 
+[ess, logEmissionPrior] = emissionEstep(model, ess, stackedData); 
+loglik = loglik + logEmissionPrior;
+end
+
+function [ess, logprior] = estepGaussEmission(model, ess, stackedData)
+% Perform the Gaussian emission component of the estep.
+logprior = 0; % Gauss emission prior not yet implemented
+d = model.d;
+nstates = model.nstates;
+weights = ess.weights;
+wsum = sum(weights, 1); 
 xbar = bsxfun(@rdivide, stackedData'*weights, wsum); %d-by-nstates
 XX   = zeros(d, d, nstates);
 for j=1:nstates
     Xc = bsxfun(@minus, stackedData, xbar(:, j)');
     XX(:, :, j) = bsxfun(@times, Xc, weights(:, j))'*Xc/wsum(j);
 end
-ess.startCounts = startCounts;
-ess.transCounts = transCounts;
 ess.xbar = xbar;
 ess.XX = XX;
+ess.wsum = wsum; 
 end
 
+function [ess, logprior] = estepDiscreteEmission(model, ess, stackedData)
+% Perform the discrete emission component of the estep.
+weights = ess.weights; 
+logprior = log(model.emission(:)+eps)'*(model.emissionPrior(:)-1);
+ess.dataCounts = weights'*bsxfun(@eq, stackedData(:), sparse(1:model.nObsStates));
+ess.wsum = sum(weights, 1)';
+end
+%% MSTEP
 function model = mstepGauss(model, ess)
 %% Maximize
 model.pi = normalize(ess.startCounts + model.piPrior -1);
@@ -161,68 +201,11 @@ end
 model.emission = emission;
 end
 
-%% Discrete
-function model = initDiscrete(data, model)
-% Initialize the model
-model.d = 1;
-model.nObsStates = nunique(cell2mat(data')');
-nstates = model.nstates;
-nObsStates = model.nObsStates;
-if isempty(model.emissionPrior)
-    model.emissionPrior = 2*ones(nstates, model.nObsStates);
-end
-if isempty(model.pi)
-    model.pi = normalize(rand(1, nstates) + model.piPrior);
-end
-if isempty(model.A)
-    model.A = normalize(rand(nstates, nstates) + model.transPrior, 2);
-end
-if isempty(model.emission)
-    model.emission = normalize(rand(nstates, nObsStates), 2);
-end
-end
-
-function [ess, loglik] = estepDiscrete(model, data)
-% Compute expected sufficient statistics
-nobs         = numel(data);
-nObsStates   = model.nObsStates;
-stackedData  = cell2mat(data')';
-seqidx       = cumsum([1, cellfun(@(seq)size(seq, 2), data')]);
-seqidx       = seqidx(1:end-1);
-nStackedData = length(stackedData);
-nstates      = model.nstates;
-A            = model.A;
-startCounts  = zeros(1, nstates);
-transCounts  = zeros(nstates, nstates);
-weights      = zeros(nStackedData, nstates);
-loglik = 0;
-for i=1:nobs
-    obs = colvec(data{i});
-    [gamma, obsLL, alpha, beta, B] = hmmInferState(model, obs);
-    loglik = loglik + obsLL;
-    startCounts = startCounts + gamma(:, 1)';
-    transCounts = transCounts + hmmComputeTwoSlice(alpha, beta, A, B);
-    sz  = size(gamma, 2);
-    idx = seqidx(i);
-    ndx = idx:idx+sz-1;
-    weights(ndx, :) = weights(ndx, :) + gamma';
-end
-logprior = log(A(:)+eps)'*(model.transPrior(:)-1)  + ...
-        log(model.pi(:)+eps)'*(model.piPrior(:)-1) +...
-        log(model.emission(:)+eps)'*(model.emissionPrior(:)-1);
-loglik = loglik + logprior;     
-dataCounts = weights'*bsxfun(@eq, stackedData(:), sparse(1:nObsStates));
-wsum = sum(weights, 1)';
-ess = structure(startCounts, transCounts, dataCounts, wsum);
-end
-
 function model = mstepDiscrete(model, ess)
-% Maximization step
+%% Maximize
 model.pi = normalize(ess.startCounts + model.piPrior -1);
 model.A  = normalize(ess.transCounts + model.transPrior -1, 2);
-
 Epc = model.emissionPrior - 1;
 denom = ess.wsum + sum(Epc, 2);
-model.E = bsxfun(@rdivide, ess.dataCounts + Epc, denom);
-
+model.emission = bsxfun(@rdivide, ess.dataCounts + Epc, denom);
 end
