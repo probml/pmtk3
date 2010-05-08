02de86f4d073a0fe8145a9e898350d941802fbb2
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-08 00:56:43 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@910 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- misc/metatools/whoCallsMe.m
+++ misc/metatools/whoCallsMe.m
@@ -1,48 +1,47 @@
-function [list, m, g, map] = whoCallsMe(fname)
+function [list, m, g, map] = whoCallsMe(fname, recache)
 % Return a list of all of the mfiles on the current path that call
-% the specified fname. 
+% the specified fname.
 %
 % m is a list of all of the (non-built-in) matlab files on your path
 % g is an adjacency matrix s.t. g(i, j) = true iff m{i} calls m{j}.
-% map is a struct mapping mfile names to indices into m and g. 
+% map is a struct mapping mfile names to indices into m and g.
 %
+
+SetDefaultValue(2, 'recache', false); 
 w = which(fname);
 if isempty(w)
-    list = []; g = []; map = struct();  
+    list = []; g = []; map = struct();
     fprintf('%s is not on your path!\n', fname);
     return
 end
 if startswith(w, matlabroot)
-    list = []; g = []; map = struct();  
+    list = []; g = []; map = struct();
     fprintf('%s is a built-in matlab function.\n', fname);
-    return 
+    return
 end
-
-warning off %#ok
 if endswith(fname, '.m'), fname = fname(1:end-2); end
 
-m = cellfuncell(@(s)s(1:end-2),allMfilesOnPath);
-map = enumerate(m);
-g = false(numel(m));
+savefile = fullfile(tempdir(), 'PMTKcallReport.mat');
 
-for i=1:numel(m)
-    from = map.(m{i});
-    toAll = depfunFast(m{i}, false);
-    for j=1:numel(toAll)
-       [path, f] = fileparts(toAll{j});  
-       if isfield(map, f)
-            to = map.(f);
-            g(from, to) = true; 
-       end
-       
+if exist(savefile, 'file') && ~recache;
+    load(savefile);
+else
+    m = cellfuncell(@(s)s(1:end-2),allMfilesOnPath);
+    map = enumerate(m);
+    g = false(numel(m));
+    for i=1:numel(m)
+        from = map.(m{i});
+        toAll = depfunFast(m{i}, false);
+        for j=1:numel(toAll)
+            [path, f] = fileparts(toAll{j});
+            if isfield(map, f)
+                to = map.(f);
+                g(from, to) = true;
+            end        
+        end
     end
+    g = setdiag(g, 0);
+    save(fullfile(tempdir(), 'PMTKcallReport'), 'g', 'm', 'map');
 end
-g = setdiag(g, 0); 
 list = m(g(:, map.(fname)));
-warning on %#ok
-
-
-
-
-
 end
