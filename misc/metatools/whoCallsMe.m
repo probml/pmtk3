ca918b3b8c56c96e00856b384078da431f00a419
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-09 02:24:45 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@911 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- misc/metatools/whoCallsMe.m
+++ misc/metatools/whoCallsMe.m
@@ -1,47 +1,77 @@
-function [list, m, g, map] = whoCallsMe(fname, recache)
+function [list, m, g, map] = whoCallsMe(fname, varargin)
 % Return a list of all of the mfiles on the current path that call
-% the specified fname.
+% the specified fname. This is a simple inverted map of the standard
+% dependency report. 
 %
-% m is a list of all of the (non-built-in) matlab files on your path
-% g is an adjacency matrix s.t. g(i, j) = true iff m{i} calls m{j}.
-% map is a struct mapping mfile names to indices into m and g.
+%% Inputs
+% 
+% fname  - the name of the file, (passed to which)
+% 
+%% Optional named inputs
 %
-
-SetDefaultValue(2, 'recache', false); 
+% recache   -  [false] recache the call graph
+% recursive -  [false] if true, all files that depend, (even indirectly)
+%                      on the specified file are returned. 
+% verbose   -  [true]  
+%% Outputs
+% list      - a list of all of the files that call/depend on fname
+% m         - a list of all user mfiles on the matlab path
+% g         - an adjacency matrix, s.t. g(i, j) iff m{i} calls m{j}
+% map       - map is a struct mapping mfile names to indicies into m and g
+%%
+[recache, recursive, verbose] = process_options(varargin, ...
+    'recache'  , false, ...
+    'recursive', false, ...
+    'verbose',   true);
+%%
 w = which(fname);
+
 if isempty(w)
     list = []; g = []; map = struct();
-    fprintf('%s is not on your path!\n', fname);
+    if verbose
+        fprintf('%s is not on your path!\n', fname);
+    end
     return
 end
-if startswith(w, matlabroot)
+if startswith(w, matlabroot) || startswith(w, 'built-in (')
     list = []; g = []; map = struct();
-    fprintf('%s is a built-in matlab function.\n', fname);
+    if verbose
+        fprintf('%s is a built-in matlab function.\n', fname);
+    end
     return
 end
-if endswith(fname, '.m'), fname = fname(1:end-2); end
-
+fname = argout(2, @fileparts, w); 
 savefile = fullfile(tempdir(), 'PMTKcallReport.mat');
-
-if exist(savefile, 'file') && ~recache;
+savefileR = fullfile(tempdir(), 'PMTKdepReport.mat');
+if recursive && exist(savefileR, 'file') && ~recache;
+    load(savefileR);
+elseif exist(savefile, 'file') && ~recache;
     load(savefile);
 else
-    m = cellfuncell(@(s)s(1:end-2),allMfilesOnPath);
+    if verbose
+        fprintf('recaching - this may take a while\n');
+    end
+    m = cellfuncell(@(s)s(1:end-2), allMfilesOnPath);
     map = enumerate(m);
     g = false(numel(m));
     for i=1:numel(m)
         from = map.(m{i});
-        toAll = depfunFast(m{i}, false);
+        toAll = depfunFast(m{i}, recursive);
         for j=1:numel(toAll)
             [path, f] = fileparts(toAll{j});
             if isfield(map, f)
                 to = map.(f);
                 g(from, to) = true;
-            end        
+            end
         end
     end
     g = setdiag(g, 0);
-    save(fullfile(tempdir(), 'PMTKcallReport'), 'g', 'm', 'map');
+    if recursive
+        save(savefileR, 'g', 'm', 'map');
+    else
+        save(savefile, 'g', 'm', 'map');
+    end
 end
+
 list = m(g(:, map.(fname)));
 end
