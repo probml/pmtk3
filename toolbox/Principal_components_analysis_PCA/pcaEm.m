3a6c667e60c0eb23db3056156de4896d88b82069
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-09 18:33:59 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@914 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Principal_components_analysis_PCA/pcaEm.m
+++ toolbox/Principal_components_analysis_PCA/pcaEm.m
@@ -1,39 +1,30 @@
-function [W, Z, evals, Xrecon, mu, iter] = pcaEm(X, k)
-% X is n*d - rows are examples, columns are features
+function [W, Z, evals, Xrecon, mu, iter] = pcaEm(X, k, varargin)
+%% Principal component analysis using EM
+%
+%% Inputs
+% X      - n*d - rows are examples, columns are features
+%
 % If k is not specified, we use the maximum possible value (rank(X))
-% W is d*k (the basis vectors)
-% Z is  n*k (the principal components)
-% evals 
-% Xrecon is n*d - reconstructed from first K
-% mu is d*1
-
-[n d] = size(X);
-maxiter = 10;
-if nargin < 2
-  k = rank(X);
-end
+%
+% * See emAlgo for additional EM related inputs
+%% Outputs
+% W      -  d*k (the basis vectors)
+% Z      -  n*k (the principal components)
+% evals  -  the eigenvalues
+% Xrecon -  n*d - reconstructed from first K
+% mu     -  d*1
+%%
+n = size(X, 1);
+SetDefaultValue(2, 'k', rank(X)); 
 mu = mean(X);
 X = X - repmat(mu, n, 1);
 X = X'; % each *column* is now a centered data case
-
-W = rand(d,k);
-iter = 1; done = false; mse = inf;
-while ~done
-   Z =  W \ X; % E step
-   %Z1 = inv(W'*W)*W'*X; 
-   %assert(approxeq(Z, Z1))
-   W = (X*Z')/(Z*Z'); % M step
-   %W1 = X*Z'*inv(Z*Z'); % M step
-   %assert(approxeq(W,W1))
-   Xrecon = W*Z;
-   oldmse = mse;
-   mse = mean((Xrecon(:) - X(:)).^2);
-   done = convergenceTest(mse, oldmse, 1e-2);
-   iter = iter + 1;
-   if iter > maxiter, done = true; end
-end
-
-% post process
+%%
+initFn = @(X)init(X, k);
+[model, negMseHist] = emAlgo(X, initFn, @estep, @mstep, [], varargin{:});
+iter = length(negMseHist);
+W = model.W;
+%% post process
 W = orth(W);
 X = X';
 Z = X*W; % rows of Z are cases
@@ -44,8 +35,22 @@ evecs = evecs(:, perm);
 W = W*evecs;
 Z = X*W;
 Xrecon = Z*W' + repmat(mu, n, 1);
+end
 
+function model = init(X, k)
+model.W = rand(size(X, 1), k);
+end
 
+function [ess, negmse] = estep(model, X)
+W = model.W;
+Z = W \ X;
+Xrecon = W*Z;
+negmse = -mean((Xrecon(:) - X(:)).^2);
+ess.Z = Z;
+ess.XZ = (X*Z'); 
+end
 
-
+function model = mstep(model, ess)
+Z = ess.Z;
+model.W = (ess.XZ)/(Z*Z');
 end
