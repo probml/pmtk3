c7d298d64872932ad785f21a5d9e716c59c2493e
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-11 18:39:03 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@929 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/ExpectationMaximization/emAlgoAdaptiveOverRelaxed.m
+++ toolbox/ExpectationMaximization/emAlgoAdaptiveOverRelaxed.m
@@ -11,11 +11,11 @@ function [model, loglikHist] = emAlgoAdaptiveOverRelaxed(model, data, init, este
 
 
 args = varargin;
-[maxIter, convTol, plotfn, verbose, overRelaxFactor] = process_options(...
+[maxIter, convTol, plotfn, verbose, overRelaxFactor, restartNum] = process_options(...
     args, 'maxIter', 100, 'convTol', 1e-3, 'plotfn', [], 'verbose', false, ...
-    'overRelaxFactor', 2);
+    'overRelaxFactor', 2, 'restartNum', 1);
 
-model = init(model, data);
+model = init(model, data, restartNum);
 iter = 1;
 done = false;
 eta = 1;
@@ -23,26 +23,26 @@ eta = 1;
 loglikHist(iter) = ll;
 iter = 2;
 while ~done
-  modelBO = mstep(model, ess);
-  [model, valid] = mstepOR(model, modelBO, eta);
-  if valid
-    [ess, ll] = estep(model, data);
-    valid = (ll > loglikHist(iter-1));
-  end
-  if valid
-    % increase step size
-    eta = eta * overRelaxFactor;
-  else
-    % just took a downhill step...
-    eta = 1;
-    model = modelBO; % regular EM update
-    [ess, ll] = estep(model, data);
-  end
-  loglikHist(iter) = ll;
-  if verbose, fprintf('%d\t loglik: %g\n', iter, ll ); end
-  if ~isempty(plotfn), plotfn(model, data, ess, ll, iter); end
-  done = (iter > maxIter) || ( (iter>1) && convergenceTest(loglikHist(iter), loglikHist(iter-1), convTol));
-  iter = iter + 1;
+    modelBO = mstep(model, ess);
+    [model, valid] = mstepOR(model, modelBO, eta);
+    if valid
+        [ess, ll] = estep(model, data);
+        valid = (ll > loglikHist(iter-1));
+    end
+    if valid
+        % increase step size
+        eta = eta * overRelaxFactor;
+    else
+        % just took a downhill step...
+        eta = 1;
+        model = modelBO; % regular EM update
+        [ess, ll] = estep(model, data);
+    end
+    loglikHist(iter) = ll;
+    if verbose, fprintf('%d\t loglik: %g\n', iter, ll ); end
+    if ~isempty(plotfn), plotfn(model, data, ess, ll, iter); end
+    done = (iter > maxIter) || ( (iter>1) && convergenceTest(loglikHist(iter), loglikHist(iter-1), convTol));
+    iter = iter + 1;
 end
 
 end
