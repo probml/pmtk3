fc4d2f60b1414f025a658805f3a385dfee0e0cd8
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-13 00:19:16 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@954 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Linear_regression/linregCensoredFitEm.m
+++ toolbox/Linear_regression/linregCensoredFitEm.m
@@ -1,50 +1,60 @@
-function [ model, yhat ] = linregCensoredFitEm( x, y, censored, relTol )
-%LINREGCENSOREDEM Fit a censored model by EM
+function [model, yhat, loglikHist] = linregCensoredFitEm(X, y, censored, varargin)
+%% Fit a censored model by EM
 % X: N*D matrix
 % y: N*1 vector
-%  censored(i) = true if censoreds
-%   relTol: relative Tolerance to end the iteration. Defaults to 10^-6
-% 
+%  censored(i) = true if censored
+%
 % yhat(i) = E[z(i)] computed during EM
-
-%author Hannes Bretschneider
-
-if nargin < 4
-    relTol = 10^-6;
-end
-
-n = size(x,1);
-x = [ones(n,1) x];
-w = x\y;
-sigma = std(y-x*w);
-w_diff = inf;
-iter = 0;
-while (w_diff > relTol)
-    iter = iter+1;
-    mu = x*w;
-    [Ez Ez2] = Estep(mu(censored), sigma, y(censored));
-    w_old = w;
-    [w sigma] = Mstep(x, y, censored, Ez, Ez2);
-    w_diff = max(abs(w_old./w-1));
-end
-model = struct('w', w, 'sigma', sigma);
+%PMTKauthor Hannes Bretschneider 
+%PMTKmodified Matt Dunham
+%%
+X = addOnes(X); 
+model.censored = censored; 
+[model, loglikHist] = emAlgo(model, [X, y], @init, @estep, @mstep, ...
+    varargin{:}); 
 yhat = y;
-yhat(censored) = Ez;
+yhat(censored) = model.Ez;
 end
 
-function [Ez Ez2] = Estep(mu, sigma, c)
-H = @(u)normpdf(u, 0, 1)./(1-normcdf(u, 0, 1));
-Ez = mu + sigma*H((c-mu)./sigma);
-Ez2 = mu.^2 + sigma^2 + sigma*(c+mu).*H((c-mu)./sigma);
+function model = init(model, data, restartNum) %#ok
+%% Initialize   
+X = data(:, 1:end-1);
+y = data(:, end); 
+w = X\y;
+model.sigma = std(y-X*w);
+model.w = w;
 end
 
-function [w sigma] = Mstep(x, y, censored, Ez, Ez2)
-n = length(y);
-uncensored = ~censored;
-Y = [y(uncensored); Ez];
-w = x\Y;
-mu = x*w;
+function [ess, sigma] = estep(model, data)
+%% Compute the expected sufficient statistics
+X = data(:, 1:end-1);
+y = data(:, end);     
+w = model.w; 
+sigma = model.sigma; 
+mu    = X*w;
+muCen = mu(model.censored); 
+c     = y(model.censored); 
+H     = @(u)gausspdf(u, 0, 1)./(1-gausscdf(u, 0, 1));
+Ez    = muCen + sigma*H((c-muCen)./sigma);
+Ez2   = muCen.^2 + sigma^2 + sigma*(c+muCen).*H((c-muCen)./sigma);
+ess   = structure(Ez, Ez2, X, y); 
+end
+
+function model = mstep(model, ess)
+%% Maximize
+censored   = model.censored;
+uncensored = ~censored; 
+Ez  = ess.Ez; 
+Ez2 = ess.Ez2;
+X   = ess.X; 
+y   = ess.y; 
+n   = length(y); 
+Y   = [y(uncensored); Ez];
+w   = X\Y;
+mu  = X*w;
 sigma2 = 1/n*(sum((y(uncensored)-mu(uncensored)).^2)+...
-  sum(Ez2-2.*mu(censored).*Ez + mu(censored).^2));
-sigma = sqrt(sigma2);
+             sum(Ez2-2.*mu(censored).*Ez + mu(censored).^2));
+model.w     = w;
+model.sigma = sqrt(sigma2); 
+model.Ez    = Ez; 
 end
