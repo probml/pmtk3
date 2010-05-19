5e179ac9ce72fd6b7fff7932ef5a12b72d414d32
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-19 00:17:50 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1029 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Some_useful_probability_distributions/betaSample.m
+++ toolbox/Some_useful_probability_distributions/betaSample.m
@@ -1,5 +1,15 @@
 function S = betaSample(model, n)
 % Return n samples from a beta distribution with parameters model.a,
 % model.b. 
-S = colvec(randraw('Beta', [model.a, model.b], n));
+%S = colvec(randraw('Beta', [model.a, model.b], n));
+
+if nargin < 2, n = 1; end
+
+sa = randgamma(repmat(model.a, n, 1)); 
+sb = randgamma(repmat(model.b, n, 1));
+S = colvec(sa ./ (sa + sb));
+
+
+
+
 end
