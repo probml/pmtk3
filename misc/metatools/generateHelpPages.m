1dd728eceae058e138c68dac8d8d779d7df89591
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-19 01:06:50 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1038 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- misc/metatools/generateHelpPages.m
+++ misc/metatools/generateHelpPages.m
@@ -1,6 +1,7 @@
 function generateHelpPages()
 %% Generate the help documentation listing files and one line desriptions.
 
+wikiFile = 'C:\pmtk3wiki\helpPages.wiki';
 dest = fullfile(pmtk3Root(), 'docs', 'helpPages'); 
 %% Toolbox
 d = dirs(fullfile(pmtk3Root(), 'toolbox')); 
@@ -13,5 +14,34 @@ end
 generateHelpTable(fullfile(pmtk3Root(), 'misc', 'util'), fullfile(dest, 'util.html'));
 %% Meta Tools
 generateHelpTable(fullfile(pmtk3Root(), 'misc', 'metatools'), fullfile(dest, 'metatools.html'));
+
+
+
+dirEmpty = @(d)isempty(mfiles(d,'topOnly', true));
+googleRoot = 'http://pmtk3.googlecode.com/svn/trunk/docs/helpPages';
+wikiText = cell(numel(d), 1);
+for i=1:numel(d)
+    if ~dirEmpty(d{i})
+        wikiText{i} = sprintf(' * [%s/%s.html %s]',googleRoot, d{i}, d{i});
+    end
+end
+wikiText = filterCell(wikiText, @(c)~isempty(c));
+wikiText =  [{'== Toolboxes =='
+            ''
+            ''
+             };
+            wikiText
+            {
+            ''
+            ''
+            '== Other =='
+            ''
+            ''
+            sprintf(' * [%s/%s.html %s]', googleRoot, 'util', 'util');
+            sprintf(' * [%s/%s.html %s]', googleRoot, 'metatools', 'metatools');
+            }];
+            
+
+writeText(wikiText, wikiFile); 
 
 end
