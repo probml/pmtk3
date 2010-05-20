e5352aabae6815aacd3d66358053cdd354bce08d
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-20 21:04:03 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1130 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/GaussianModels/uniGaussPdf.m
+++ toolbox/GaussianModels/uniGaussPdf.m
@@ -1,6 +1,9 @@
 function p = uniGaussPdf(X, mu, sigma2)
 %% Univariate Gaussian PDF vectorized w.r.t. mu and sigma2
 % p(i) = p(X(i) | mu(i), sigma2(i))
+% Use this function when you want to evaluate each data case under a
+% different distribution, i.e. different mu and sigma2 values, otherwise
+% use gausspdf, which works in the uni and multivariate case. 
 %%
 X      = colvec(X);
 mu     = colvec(mu);
