9633e479c15daa780af6785f3b6092bc861db55e
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-20 20:01:06 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1125 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/GaussianModels/gaussTruncatedSample.m
+++ toolbox/GaussianModels/gaussTruncatedSample.m
@@ -3,8 +3,8 @@ function x = gaussTruncatedSample(model, a, b, N)
 % x is a 1*N row vector
 if nargin < 4, N = 1; end
 mu = model.mu; sigma = model.Sigma;
-model.a = normcdf((a-mu)/sigma, 0, 1); 
-model.b = normcdf((b-mu)/sigma, 0, 1); 
+model.a = gausscdf((a-mu)/sigma, 0, 1); 
+model.b = gausscdf((b-mu)/sigma, 0, 1); 
 u = sampleUniform(model, N);
 x = mu + sigma*gaussinv(u);
 
