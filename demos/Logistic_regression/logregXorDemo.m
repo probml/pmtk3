144517084ecec3457fe0ddd43a95ab8899266f73
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-23 18:28:33 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1204 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/Logistic_regression/logregXorDemo.m
+++ demos/Logistic_regression/logregXorDemo.m
@@ -16,22 +16,25 @@ printPmtkFigure('logregXorLinear')
 %% Basis Expansions
 rbfScale = 1;
 polydeg  = 2;
-%protoTypes = [1 1; 1 5; 5 1; 5 5; -1 -1];
-protoTypes = [-1 -1; 1 1];
+protoTypes = [1 1; 1 5; 5 1; 5 5];
 kernels = {@(X1, X2)kernelRbfSigma(X1, X2, rbfScale)
            @(X1, X2)kernelRbfSigma(X1, protoTypes, rbfScale)
            @(X1, X2)kernelPoly(X1, X2, polydeg)};
 fnames  = {'logregXorRbf', 'logregXorRbfProto', 'logregXorPoly'};
 titles  = {'rbf', 'rbf prototypes', 'poly'};
-
 for i=1:numel(kernels)
     preproc.kernelFn = kernels{i};
+    preproc.standardizeX = false; 
     model = logregFit(X, y, 'lambda', lambda, 'preproc', preproc);
     yhat = logregPredict(model, X);
     errorRate = mean(yhat ~= y);
     fprintf('Error rate using %s features: %2.f%%\n', titles{i}, 100*errorRate);
     predictFcn = @(Xtest)logregPredict(model, Xtest);
     plotDecisionBoundary(X, y, predictFcn);
+    if i==2
+       hold on; 
+       plot(protoTypes(:, 1), protoTypes(:, 2), '*k', 'linewidth', 2, 'markersize', 10)
+    end
     title(titles{i});
     printPmtkFigure(fnames{i})
 end
