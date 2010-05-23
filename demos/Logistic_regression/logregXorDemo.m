b03aa7ad106d823d651eaf5088031b2be7148d5f
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-23 18:21:42 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1203 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/Logistic_regression/logregXorDemo.m
+++ demos/Logistic_regression/logregXorDemo.m
@@ -16,10 +16,13 @@ printPmtkFigure('logregXorLinear')
 %% Basis Expansions
 rbfScale = 1;
 polydeg  = 2;
+%protoTypes = [1 1; 1 5; 5 1; 5 5; -1 -1];
+protoTypes = [-1 -1; 1 1];
 kernels = {@(X1, X2)kernelRbfSigma(X1, X2, rbfScale)
+           @(X1, X2)kernelRbfSigma(X1, protoTypes, rbfScale)
            @(X1, X2)kernelPoly(X1, X2, polydeg)};
-fnames  = {'logregXorRbf', 'logregXorPoly'};
-titles  = {'rbf', 'poly'};
+fnames  = {'logregXorRbf', 'logregXorRbfProto', 'logregXorPoly'};
+titles  = {'rbf', 'rbf prototypes', 'poly'};
 
 for i=1:numel(kernels)
     preproc.kernelFn = kernels{i};
