81ccea2764e7601ddc16f322a364bdd48b17a7ed
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-05-24 18:52:44 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1225 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/Logistic_regression/logregLaplaceGirolami.m
+++ demos/Logistic_regression/logregLaplaceGirolami.m
@@ -1,9 +1,9 @@
 % logregLaplaceDemoGirolami
 % Example of the laplace approximation for logistic regression
 % Based on code written by Mark Girolami
-%PMTKbroken
-clear all
-seed = 0; randn('state', seed); rand('state', seed);
+
+
+setSeed(0);
 
 % We generate data from two Gaussians:
 % x|C=1 ~ gauss([1,5], I)
@@ -26,11 +26,11 @@ Step=0.1;
 W=[reshape(w1,n*n,1) reshape(w2,n*n,1)];
 
 f=W*X';
-Log_Prior = log(mvnpdf(W, zeros(1,D), eye(D).*alpha));
+Log_Prior = log(gausspdf(W, zeros(1,D), eye(D).*alpha));
 Log_Like = W*X'*t - sum(log(1+exp(f)),2); 
 Log_Joint = Log_Like + Log_Prior;
 
-figure(1);clf
+f1 = figure();
 J=2;K=2;
 subplot(J,K,1)
 contour(w1,w2,reshape(-Log_Prior,[n,n]),30);
@@ -41,19 +41,21 @@ title('Log-Likelihood');
 subplot(J,K,3)
 contour(w1,w2,reshape(-Log_Joint,[n,n]),30);
 title('Log-Unnormalised Posterior')
-hold
+hold on
 
 %Identify the parameters w1 & w2 which maximise the posterior (joint)
 [i,j]=max(Log_Joint);
 plot(W(j,1),W(j,2),'.','MarkerSize',40);
 
 %Compute the Laplace Approximation
-[wMAP, C] = logregFitIRLS(t, X, 1/alpha)
-Log_Laplace_Posterior = log(mvnpdf(W, wMAP', C)+eps);
+model = logregBinaryFitL2IRLS(X, t, 1/alpha, false);
+wMAP = model.w;
+C = model.C; 
+Log_Laplace_Posterior = log(gausspdf(W, wMAP', C)+eps);
 
 subplot(J,K,4);
 contour(w1,w2,reshape(-Log_Laplace_Posterior,[n,n]),30);
-hold
+hold on
 plot(W(j,1),W(j,2),'.','MarkerSize',40);
 title('Laplace Approximation to Posterior')
 
@@ -61,7 +63,7 @@ title('Laplace Approximation to Posterior')
 % logregLaplaceDemoPred
 % Plot the predictive distribution for logistic regression
 
-figure(2);clf
+f2 = figure();
 
 % wMAP
 subplot(2,2,1)
@@ -81,7 +83,7 @@ plot(X(find(t==0),1),X(find(t==0),2),'bo');
 predMean = zeros(n*n,1);
 S = 100;
 for i=1:S
-  wsamp = mvnrnd(wMAP', C)';
+  wsamp = gaussSample(struct('mu', wMAP', 'Sigma', C))';
   pred = 1./(1+exp(-grid*wsamp));
   predMean = predMean + pred;
   contour(w1,w2,reshape(pred,[n,n]),[0.5 0.5]);
