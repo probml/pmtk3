13ec4e365e996361167e7375c81eb58994fe6c85
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-02 17:43:43 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1375 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ misc/metatools/dataTools/unzipData.m
@@ -0,0 +1,49 @@
+function unzipData(dataSource, dest, threshold)
+%% Unzip all of the data to another directory, without the zip or svn files
+% If threshold is specified, data is split into two by threshold size in
+% MB, creating "big" and "small" directories. 
+%
+% dataSource is the location of the pmtkData local copy of the svn
+% repository.
+%
+%% Example
+%
+% unzipData('C:\pmtkData', 'C:\users\Matt\Desktop\', 5);
+%%
+
+SetDefaultValue(1, 'dataSource', 'C:\pmtkData'); 
+
+zipFiles = filelist(dataSource, '*.zip', true); 
+
+tocopy = cellfun(@(c)ismember(c, dirs(dataSource)), filenames(zipFiles));
+zipFiles = zipFiles(tocopy); 
+
+destDir = fullfile(dest, 'pmtkData'); 
+if ~(exist(destDir, 'file') == 7)
+    mkdir(destDir); 
+end
+
+if nargin < 3
+    big = fullfile(destDir, 'big');
+    small = fullfile(destDir, 'small'); 
+    mkdir(big);
+    mkdir(small);
+    for z=1:numel(zipFiles)
+        d = dir(zipFiles{z}); 
+        sz = d.bytes/(1024*1024); 
+        if sz <= threshold
+            finalDest = fullfile(destDir, 'small');
+        else
+            finalDest = fullfile(destDir, 'big');
+        end
+        unzip(zipFiles{z}, finalDest); 
+    end
+    
+else
+    for z=1:numel(zipFiles)
+        unzip(zipFiles{z}, destDir); 
+    end
+end
+
+
+end
