4d08d51118328c3a6a17baaffbcd142794959789
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-09 20:30:03 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1414 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/Linear_regression/linearRegressionFrequentist.m
@@ -0,0 +1,61 @@
+function [w, stderr, pval, R2, sigma2, confint, Zscore] = linearRegressionFrequentist(y, XX, names, useLatex)
+% simulate  R's lm function for a simple linear regression model
+% Each row ox X is a training case, excluding the 1 term
+
+[n d] = size(XX);
+
+if nargin < 3 || isempty(names)
+  for i=1:d
+    names{i} = sprintf('x%d: ', i);
+  end
+end
+if nargin <4, useLatex = false; end
+
+X = [ones(n,1) XX];
+w = X\y; % mle
+yhat = X*w;
+rss = sum((y - yhat).^2);
+R2 = 1-rss/sum((y-mean(y)).^2);
+sigma2 = rss/(n-d-1); % unbiased estimate
+Sigma = pinv(X'*X)*sigma2; % covariance matrix of w
+stderr = sqrt(diag(Sigma));
+Zscore = w ./ stderr;
+dof = n-d-1;
+pval = 1-tcdfPMTK(abs(Zscore),dof) + tcdfPMTK(-abs(Zscore),dof);
+
+alpha = 0.95;
+tc = tinvPMTK(1-(1-alpha)/2, dof); 
+confint = [w-tc*stderr w+tc*stderr];
+
+names2 = {'intercept', names{:}};
+fprintf('\nlinear regression n=%d, d=%d, R-squared=%10.5f\n\n', n, d, R2);
+
+
+fprintf('%10s %10s %10s %10s %10s %5s\n', ...
+	'', 'estimate', 'std err', 't value', 'p(>|t|)', '');
+
+for i=1:d+1
+  if pval(i) > 0 & pval(i) < 0.001
+    str = '***';
+  elseif pval(i) >= 0.001 & pval(i) < 0.01
+    str = '**';
+  elseif pval(i) >= 0.01 & pval(i) < 0.05
+    str = '*';
+  elseif pval(i) >= 0.05 & pval(i) < 0.1
+    str = '.';
+  else str = '';
+  end
+  if useLatex
+    fprintf('%10s & %10.5f & %10.5f & %10.5f & %10.5f %s\\\\\n', ...
+      names2{i}, w(i), stderr(i), Zscore(i),  pval(i), str);
+  else
+    fprintf('%10s %10.5f %10.5f %10.5f %10.5f %s\n', ...
+      names2{i}, w(i), stderr(i), Zscore(i),  pval(i), str);
+  end
+  
+end
+
+
+fprintf('\nSignif. codes: *** [0,0.001), ** [0.001, 0.01), * [0.01,0.05) . [0.05,1)\n');
+
+end
