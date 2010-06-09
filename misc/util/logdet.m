d88a5a1658e0e3e5c0ad4448e6386c66cf54cc39
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-09 21:11:41 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1415 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ misc/util/logdet.m
@@ -0,0 +1,15 @@
+function y = logdet(A)
+% Compute log(det(A)) where A is positive-definite
+% This is faster and more stable than using log(det(A)).
+%
+%PMTKauthor Tom Minka
+% (c) Microsoft Corporation. All rights reserved.
+try
+    U = chol(A);
+    y = 2*sum(log(diag(U)));
+catch %#ok
+    y = 0;
+    warning('logdet:posdef', 'Matrix is not positive definite');
+end
+
+end
---
+++ misc/util/randpd.m
@@ -0,0 +1,10 @@
+function M = randpd(d)
+% Create a random positive definite matrix of size d by d 
+
+A = randn(d);
+M = A*A';
+while ~isposdef(M) % check to avoid returning a matrix whose logdet is < eps
+   M = M + diag(0.001*ones(1,d)); 
+end
+
+end
