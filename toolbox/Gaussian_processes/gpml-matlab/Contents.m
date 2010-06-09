8073ab20fcdddb3c351330a62f41a4262c63697b
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-09 20:29:38 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1413 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Gaussian_processes/gpml-matlab/Contents.m
+++
@@ -1,4 +0,0 @@
-%PMTKtitle gpml: Gaussian processes for machine learning
-%PMTKauthor Carl Rasmussen, Chris Williams
-%PMTKdate  July 25, 2007
-%PMTKurl http://www.gaussianprocess.org/gpml/code/matlab/doc/
--- toolbox/Kernel_methods_for_supervised_learning/svmLightWindows/Contents.m
+++
@@ -1,4 +0,0 @@
-%PMTKtitle SVMlight: Windows executable
-%PMTKauthor Thorsten Joachims 
-%PMTKurl http://svmlight.joachims.org/
-%PMTKdate August 14, 2008 (Version 6.0.2)
