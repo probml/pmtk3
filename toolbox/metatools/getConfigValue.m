59427a5b25959d3c77f2cc464b8edbff886fcd15
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-25 20:14:27 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1604 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/metatools/getConfigValue.m
+++ toolbox/metatools/getConfigValue.m
@@ -1,14 +1,40 @@
-function value = getConfigValue(var)
+function value = getConfigValue(var, ignoreLocal)
 %% Return the value of a configuration variable stored in config.txt
-%
+% If a config-local.txt file is found, these value override config.txt's
+% values. 
 %% Example
-% path = getConfigValue('PMTKsupportLink'); 
+% path = getConfigValue('PMTKsupportLink');
 
-[tags, lines] = tagfinder(fullfile(pmtk3Root(), 'config.txt')); 
-S = createStruct(tags, lines); 
+if nargin < 2, ignoreLocal = false; end % if true, ignore config-local.txt
+
+local = fullfile(pmtk3Root(), 'config-local.txt');
+generic = fullfile(pmtk3Root(), 'config.txt');
+
+if exist(local, 'file') && ~ignoreLocal
+    configFile = local;
+    searchTwice = true;
+else
+    configFile = generic;
+    searchTwice = false;
+end
+%%
+[tags, lines] = tagfinder(configFile);
+S = createStruct(tags, lines);
 if isfield(S, var)
-    value = strtrim(S.(var)); 
+    value = strtrim(S.(var));
 else
     value = '';
 end
+%%
+if isempty(value) && searchTwice 
+    % search config.txt if not found in config-local.txt    
+    [tags, lines] = tagfinder(local);
+    S = createStruct(tags, lines);
+    if isfield(S, var)
+        value = strtrim(S.(var));
+    else
+        value = '';
+    end
+end
+
 end
