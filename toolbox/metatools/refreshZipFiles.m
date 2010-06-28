80ea48c3a42066758ec416072a468143ca3663e5
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-28 17:05:55 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1647 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/metatools/refreshZipFiles.m
@@ -0,0 +1,64 @@
+function refreshZipFiles(localSource, refreshList)
+%% Refreh the contents of the individual zip files in each data or pmtkSupport folder
+% If refreshList is not specified, do this for every data or pmtkSupport directory.
+%
+%% Inputs
+% localSource - this is the full path to the local copy of the pmtkData or
+%               pmtkSupport svn repository, OR, the name
+%               'support', or 'data' in which case the appropriate path is
+%               obtained from config.txt or config-local.txt.
+%
+% refreshList - this is a cell array of directory names to be refreshed or
+%               a single string indicating one such directory. If this is
+%               not specified, all directories are updated.
+%% Examples
+%
+% refreshZipFiles() % default is pmtkData
+% refreshZipFiles('data', 'alarmNetwork')
+% refreshZipFiles('C:\googleCode\pmtkData', {'alarmNetwork', 'anscombe'});
+%
+% refreshZipFiles('support', 'fastfit')
+% 
+%% Notes
+% The directories must be part of the svn repository as we use the svn
+% export command.
+% PMTKneedsMatlab
+%%
+excludedDirs = {'meta', 'docs', 'tmp'}; % don't zip these up!
+%%
+SetDefaultValue(1, 'localSource', getConfigValue('PMTKlocalDataPath'));
+switch localSource
+    case 'support'
+        localSource = getConfigValue('PMTKlocalSupportPath');
+    case 'data'
+        localSource = getConfigValue('PMTKlocalDataPath');
+end
+if nargin < 2
+    tozip = dirs(localSource);
+else
+    tozip = cellwrap(refreshList);
+end
+tozip = setdiff(tozip, excludedDirs); 
+%% Check that all of the directories exist
+for i=1:numel(tozip)
+    assert(exist(fullfile(localSource, tozip{i}), 'dir') == 7);
+end
+%%
+tmproot = fullfile(localSource, 'tmp');
+mkdir(tmproot);
+maxLen = max(cellfun(@length, tozip)); 
+for i=1:numel(tozip)
+    z = tozip{i};
+    fprintf('processing %s%s', z, dots(maxLen - length(z)+3)); 
+    p = fullfile(localSource, z);
+    tmp = fullfile(tmproot, z);
+    system(sprintf('svn export %s %s', p, tmp));
+    destZip = fullfile(localSource, z, [z, '.zip']);
+    if exist(destZip, 'file')
+        delete(destZip); % remove the old zip file
+    end
+    zip(destZip, tmp);
+    system(sprintf('rmdir /Q /S %s', tmp));
+end
+system(sprintf('rmdir /Q /S %s', tmproot));
+end
