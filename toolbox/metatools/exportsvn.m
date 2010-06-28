ee1425c0310e5164570c19f8a6cec968d269f3c9
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-28 20:52:49 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1666 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/metatools/exportsvn.m
+++ toolbox/metatools/exportsvn.m
@@ -1,4 +1,4 @@
-function exportsvn(source, dest, exclusions)
+function exportsvn(source, dest, exclusions, createEmpty)
 % Export and zip up a local svn repository ignoring .svn files
 %   SOURCE:  path to the root directory of the svn source
 %   DEST:    path to destination including zip file name
@@ -9,6 +9,7 @@ function exportsvn(source, dest, exclusions)
 % exportsvn('C:\pmtk3', 'C:\users\matt\Desktop\pmtk3.zip', {'docs'})
 
 if nargin < 3, exclusions = {}; end
+if nargin < 4, createEmpty = {}; end
 
 destpath = fileparts(dest);
 system(sprintf('svn export %s %s', source, fullfile(destpath, 'tmp')));
@@ -19,8 +20,15 @@ for i=1:numel(exclusions)
         system(sprintf('rmdir /Q /S %s', ex));
     end
 end
+for i=1:numel(createEmpty)
+   fprintf('adding emtpy directory: %s\n', createEmpty{i}); 
+   mkdir(fullfile(destpath, 'tmp', createEmpty{i}));  
+end
 
 
 zip(dest, fullfile(destpath, 'tmp', '*'));
-system(sprintf('rmdir /Q /S %s', fullfile(destpath, 'tmp')));
+[err, output] = system(sprintf('rmdir /Q /S %s', fullfile(destpath, 'tmp')));
+if err
+    error('svn export failed:%s\n', output); 
+end
 end
