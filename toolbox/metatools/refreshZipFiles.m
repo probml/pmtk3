4d94a059dfbdc12d3a41420a65f481fb44204d11
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-28 17:10:50 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1648 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/metatools/refreshZipFiles.m
+++ toolbox/metatools/refreshZipFiles.m
@@ -24,7 +24,7 @@ function refreshZipFiles(localSource, refreshList)
 % export command.
 % PMTKneedsMatlab
 %%
-excludedDirs = {'meta', 'docs', 'tmp'}; % don't zip these up!
+excludedDirs = tokenize(getConfigValue('PMTKnonSupportDirs'), ',')'; % don't zip these up!
 %%
 SetDefaultValue(1, 'localSource', getConfigValue('PMTKlocalDataPath'));
 switch localSource
