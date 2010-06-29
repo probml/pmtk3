a2139319b599a97d10f22d5627a690d239b95267
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-06-29 18:45:30 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1693 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/ProbDist/invWishart/invWishartLogprob.m
+++ toolbox/ProbDist/invWishart/invWishartLogprob.m
@@ -1,12 +1,25 @@
-function logp = invWishartLogprob(model, S)
+function logp = invWishartLogprob(arg1, arg2, arg3)
 % logp(i) = log p(S(:, :, i) | model)
 % If model.Sigma is scalar, than logp(i) = log p(S(i) | model)
 %
 % model has fields Sigma and dof
 % model.dof must be > d-1
+%
+% logp = invWishartLogprob(model, S); OR
+% logp = invWishartLogprob(Sigma, dof, S);
+%%
 
-Sigma = model.Sigma;
-dof   = model.dof;
+if isstruct(arg1)
+    model = arg1;
+    Sigma = model.Sigma;
+    dof   = model.dof;
+    S     = arg2;
+else
+    Sigma = arg1;
+    dof   = arg2;
+    S     = arg3;
+end
+
 d     = size(Sigma, 1);
 S     = reshape(S, d, d, []);
 n     = size(S, 3);
@@ -14,8 +27,8 @@ logZ  = (dof*d/2)*log(2) + mvtGammaln(d, dof/2) - (dof/2)*logdet(Sigma);
 logp  = zeros(n, 1);
 for i=1:n
     logp(i) = - (dof + d+1) / 2*logdet(S(:, :, i)) ...
-              - 0.5*trace(Sigma / S(:, :, i))      ...
-              - logZ;
+        - 0.5*trace(Sigma / S(:, :, i))      ...
+        - logZ;
 end
 
 
