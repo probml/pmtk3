2d5ab788a2f722056deb2539a90c3a0ee60d3d8f
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-04 16:00:03 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1729 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- bookDemos/Probability/dirichlet3dPlot.m
+++ bookDemos/Probability/dirichlet3dPlot.m
@@ -2,15 +2,26 @@
 % Here we plot the dirichlet distribution as a function of a three
 % dimensional alpha vector, which we can do as the distribtion lives on a 
 % lower dimensional simplex due to the sum to one constraint. 
-%% alpha > 1
+
+alphas = [10 0.1];
+for alpha=alphas(:)'
 xyrange = [0 1 0 1];
-model.alpha = 10;
-npoints = 200; 
+model.alpha = alpha;
+npoints = 100; 
 %% 
 % The dirichletLogprob function returns NaNs for values that lie outside of
 % the simplex, which plot() ignores giving us the nice simplex plot. 
 figure();
-useLog = false;
+% when alpha < 1  plots of exp(logp)  are
+% ugly due to numerical error, but for alpha = 0.1, the plots of
+% p and logp look very similar, except that exp(logp) is more sharply
+% peaked around the corners. So we use logp.
+if 0 % alpha<1
+  useLog = true;
+else
+  useLog = false;
+end
+
 if useLog
   plotSurface(@(X)dirichletLogprob(model, X), xyrange, 'npoints', npoints);
 else
@@ -19,6 +30,7 @@ end
 shading interp;
 camlight right;
 lighting phong;
+%view([-32 50]);
 set(gca, 'xtick', 0:0.5:1, 'ytick', 0:0.5:1);
 if useLog
   zlabel('log(p)');
@@ -26,31 +38,8 @@ else
   zlabel('p')
 end
 title(sprintf('%s=%3.2f', '\alpha', model.alpha))
-printPmtkFigure(sprintf('Dir%d', model.alpha*10))
+printPmtkFigure(sprintf('DirSimplex%d', model.alpha*10))
 
-%% alpha < 1
-% Here we plot the log probability - plots of exp(logp) when alpha < 1 are
-% ugly due to numerical error, but for alpha = 0.1, the plots of
-% p and logp look very similar, except that exp(logp) is more sharply
-% peaked around the corners. 
+end
 
-model.alpha = 0.5;
-npoints = 500; % We need a higher resolution when alpha < 1
-figure();
-if useLog
-  plotSurface(@(X)dirichletLogprob(model, X), xyrange, 'npoints', npoints);
-else
-  plotSurface(@(X)exp(dirichletLogprob(model, X)), xyrange, 'npoints', npoints);
-end
-view([-32 50]);
-shading interp; 
-camlight right;
-lighting phong;
-set(gca, 'xtick', 0:0.5:1, 'ytick', 0:0.5:1);
-if useLog
-  zlabel('log(p)');
-else
-  zlabel('p')
-end
-title(sprintf('%s=%3.2f', '\alpha', model.alpha))
-printPmtkFigure(sprintf('Dir%d', model.alpha*10))
+
