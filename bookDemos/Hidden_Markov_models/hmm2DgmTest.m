3d4f9951cd649f4f55d0866ff7f6d240e51b88c4
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-06 02:03:47 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1740 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- bookDemos/Hidden_Markov_models/hmm2DgmTest.m
+++ bookDemos/Hidden_Markov_models/hmm2DgmTest.m
@@ -16,14 +16,28 @@ X = hmmSample(model, T, 1);
 X = X{1}'; 
 tic
 gamma = hmmInferState(model, X); 
-toc
+t = toc;
+fprintf('fwdbck: %g seconds\n', t); 
 
 tic
 dgm = hmm2Dgm(model, X);
-marginals = dgmInfer(dgm, num2cell(1:dgm.nnodes), 'method', 'libdai'); 
-gammaDgm = zeros(nstates, dgm.nnodes); 
-for t=1:numel(marginals)
-    gammaDgm(:, t) = marginals{t}.T;
+marginalsLD = dgmInfer(dgm, num2cell(1:dgm.nnodes), 'method', 'libdai'); 
+gammaLibDai = zeros(nstates, dgm.nnodes); 
+for t=1:numel(marginalsLD)
+    gammaLibDai(:, t) = marginalsLD{t}.T;
 end
-toc
-assert(approxeq(gamma, gammaDgm)); 
+t = toc;
+fprintf('libdai: %g seconds\n', t); 
+
+tic
+dgm = hmm2Dgm(model, X);
+marginalsJT = dgmInfer(dgm, num2cell(1:dgm.nnodes), 'method', 'jtree'); 
+gammaJtree = zeros(nstates, dgm.nnodes); 
+for t=1:numel(marginalsJT)
+    gammaJtree(:, t) = marginalsJT{t}.T;
+end
+t = toc;
+fprintf('jtree : %g seconds\n', t); 
+
+assert(approxeq(gamma, gammaJtree)); 
+assert(approxeq(gamma, gammaLibDai)); 
