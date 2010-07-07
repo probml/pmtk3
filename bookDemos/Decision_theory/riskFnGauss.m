750dafba718904a9b6b70b303319e35e8e0e7af3
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-07 20:31:50 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1750 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- bookDemos/Decision_theory/riskFnGauss.m
+++ bookDemos/Decision_theory/riskFnGauss.m
@@ -2,9 +2,10 @@
 %
 %%
 ns = [5 20];
+[styles, colors, symbols] = plotColors;
 for ni=1:length(ns)
   n = ns(ni);
-mus = -1.5:0.1:1.5;
+mus = -1.8:0.1:1.8;
 M = length(mus);
 n0 = 1;
 n0B = 5;
@@ -14,22 +15,24 @@ r2 = (pi/(2*n)) * ones(1,M);
 r3 = (mus-mu0).^2;
 r4 = (n+n0)^(-2) * (n + n0^2*(mu0-mus).^2);
 r5 = (n+n0B)^(-2) * (n + n0B^2*(mu0-mus).^2);
+legendStr = {'mle','median','fixed','postmean1', 'postmean5'};
 
-figure; hold on
-plot(mus, r1, 'r-o', 'linewidth', 2);
-plot(mus, r2, 'g-*', 'linewidth', 2);
-plot(mus, r3, 'b-s', 'linewidth', 2);
-plot(mus, r4, 'k-+', 'linewidth', 2);
-plot(mus, r5, 'c-d', 'linewidth', 2);
-
+r = {r1, r2, r3, r4, r5};
+figure; 
+for i=1:5
+plot(mus, r{i}, [styles{i}, colors(i)], 'linewidth', 3);
+ % 'displayname', legendStr{i});
+ hold on
+end
+legend(legendStr,'fontsize', 12, 'Location','NorthWest');
 if ni==1
-  set(gca,'ylim',[0 0.6])
+  set(gca,'ylim',[0 0.5])
 else
-  set(gca,'ylim',[0 0.2])
+  set(gca,'ylim',[0 0.18])
 end
 
 %grid on
-legend('mle','median','fixed','postmean5', 'postmean1')
+
 
 %%text(0.7, 0.4, 'fixed', 'fontsize', 14, 'color', 'b')
 %%text(1.2, 0.3, 'postmean', 'fontsize', 14, 'color', 'k')
