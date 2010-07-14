704f010d88baa90ed795233af9c90354acee243e
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-14 23:12:16 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1786 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/Graphical_models/dgm/dgmLogZ.m
@@ -0,0 +1,23 @@
+function logZ = dgmLogZ(dgm, varargin)
+%% Estimate the log of the partition sum
+% See dgmInferNodes for optional args
+%%
+[clamped, softev, localev] = process_options(varargin, ...
+    'clamped', [], ...
+    'softev' , [], ...
+    'localev', []);
+
+localFacs = dgmEv2LocalFacs(dgm, localev, softev);
+G = dgm.G;
+
+if isfield(dgm, 'jtree')
+    jtree = jtreeSliceCliques(dgm.jtree, clamped);
+else
+    doSlice = true;
+    factors = cpds2Factors(dgm.CPDs, G, dgm.CPDpointers);
+    factors = addEvidenceToFactors(factors, clamped, doSlice);
+    jtree   = jtreeInit(factorGraphCreate(factors, G));
+end
+jtree = jtreeAddFactors(jtree, localFacs);
+[jtree, logZ] = jtreeCalibrate(jtree);
+end
