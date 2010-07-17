12a8e337948e5f2982e544d2b9afd18aef3277b7
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-17 21:44:17 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1833 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Graphical_models/inference/junctionTree/jtreeInit.m
+++ toolbox/Graphical_models/inference/junctionTree/jtreeInit.m
@@ -4,25 +4,30 @@ function jtree = jtreeInit(fg, varargin)
 % Optionally include cliqueConstraints, a cell array of sets of variable
 % indices if you want to guarantee that cliques containing these sets are
 % created.
-%%
-cc = process_options(varargin, 'cliqueConstraints', {}); 
+%% setup
+cc       = process_options(varargin, 'cliqueConstraints', {}); 
 factors  = fg.Tfac(:);
 nfactors = numel(factors);
 nstates  = cellfun(@(t)t.sizes(end), factors);
 G        = moralizeGraph(fg.G);
 nvars    = size(G, 1);
-cc       = cellwrap(cc); 
+%% add in clique constraints
+cc = cellwrap(cc); 
 for i=1:numel(cc)
     c = cc{i};
     G(c, c) = 1;
 end
 G = setdiag(G, 0);
-elimOrder = minweightElimOrder(G, nstates);
-G = mkChordal(G, elimOrder);
-[pElimOrder, chordal, clqs] = maxCardinalitySearch(G); % clqs{i} is the i'th maximal clique, ordered by RIP
-cliqueTreeUndir = mcsCliques2Jtree(clqs);
+%% build clique tree
+% clqs{i} is the i'th maximal clique, ordered by RIP
+elimOrder                   = minweightElimOrder(G, nstates);
+G                           = mkChordal(G, elimOrder);
+[pElimOrder, chordal, clqs] = maxCardinalitySearch(G); 
+cliqueTreeUndir             = mcsCliques2Jtree(clqs);
+%% create clique lookup table
+% cliqueLookup(i, c) = true if GM node i is in clqs{c}
 ncliques = numel(clqs);
-cliqueLookup = false(nvars, ncliques);                 % cliqueLookup(i, c) = true if GM node i is in clqs{c}
+cliqueLookup = false(nvars, ncliques);                 
 for c=1:ncliques
     cliqueLookup(clqs{c}, c) = true;
 end
@@ -31,7 +36,7 @@ initClqSizes = cellfun('length', clqs);
 factorLookup = false(nfactors, ncliques);
 for f=1:nfactors
     candidateCliques = find(all(cliqueLookup(factors{f}.domain, :), 1));
-    smallest = minidx(initClqSizes(candidateCliques));
+    smallest         = minidx(initClqSizes(candidateCliques));
     factorLookup(f, candidateCliques(smallest)) = true;
 end
 cliques = cell(ncliques, 1); % tabular potentials
@@ -41,11 +46,11 @@ for c=1:ncliques
     tf         = [{T}; factors(factorLookup(:, c))];
     cliques{c} = tabularFactorMultiply(tf);
 end
-%% Determine message schedule
-root = nvars;
-rootCliques = find(cliqueLookup(root, :));
-rootClq = rootCliques(minidx(initClqSizes(rootCliques)));
-[preOrder, postOrder, pred] = dfsearch(cliqueTreeUndir, rootClq, false);
+%% determine message schedule
+root                        = nvars; % last one
+rootCliques                 = find(cliqueLookup(root, :));
+rootClqNdx                  = rootCliques(minidx(initClqSizes(rootCliques)));
+[preOrder, postOrder, pred] = dfsearch(cliqueTreeUndir, rootClqNdx, false);
 % Make a directed version of cliqueTreeUndir
 cliqueTree = zeros(ncliques, ncliques);
 for i=1:length(pred)
@@ -61,7 +66,7 @@ preOrderChildren = cell(1, length(preOrder));
 for i = preOrder
     preOrderChildren{i} = children(cliqueTree, i);
 end
-%%
+%% package 
 jtree = structure(cliques, preOrder, postOrder, ...
     preOrderChildren, postOrderParents, cliqueLookup, cliqueTree);
 end
