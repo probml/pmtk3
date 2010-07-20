b8034684286d67679a2c83b75e59b7d0506b6074
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-20 06:58:12 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1852 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Hidden_Markov_models/hmmFitFullyObs.m
+++ toolbox/Hidden_Markov_models/hmmFitFullyObs.m
@@ -1,5 +1,5 @@
 function model =  hmmFitFullyObs(Z, Y, type, varargin)
-%% Fit a fully observed HMM via mle
+%% Fit a fully observed HMM via mle/map
 %
 % Z       - a cell array of observations of the discrete Markov backbone;
 %           each observation j is 1-by-seqLength(j).
@@ -9,29 +9,43 @@ function model =  hmmFitFullyObs(Z, Y, type, varargin)
 %
 % type     - one of {'gauss', 'discrete'}
 %
-% TODO   (add support for priors)
+% See hmmFit for details on priors
 %%
-
 Z        = cellwrap(Z); 
 Y        = cellwrap(Y); 
 Zstacked = cell2mat(Z')'; % sum(seqLength)-by-1
 Ystacked = cell2mat(Y')'; % sum(seqLength)-by-d
 nstatesZ = max(Zstacked); 
-model.A  = normalize(accumarray([Zstacked(1:end-1), Zstacked(2:end)], 1), 2); 
 
+[piPrior, transPrior, emissionPrior] = process_options(varargin , ...
+    'piPrior'                   , 2*ones(1, nstatesZ)           , ...
+    'transPrior'                , 2*ones(nstatesZ, nstatesZ)    , ...
+    'emissionPrior'             , []);
+
+if diff(size(transPrior))
+    transPrior = repmat(rowvec(transPrior), nstatesZ, 1);
+end
+A        = accumarray([Zstacked(1:end-1), Zstacked(2:end)], 1); 
+model.A  = normalize(A + transPrior-1, 2); 
 seqidx   = cumsum([1, cellfun(@(seq)size(seq, 2), Z')]);
-model.pi = normalize(histc(Zstacked(seqidx(1:end-1)), 1:nstatesZ))';
-      
+pi       = histc(Zstacked(seqidx(1:end-1)), 1:nstatesZ)';
+model.pi = normalize(pi + rowvec(piPrior) - 1);       
 switch lower(type)
     case 'gauss'
         
         d = size(Ystacked, 2); 
+        if isempty(emissionPrior)
+            emissionPrior.mu    = zeros(1, d);
+            emissionPrior.Sigma = 0.1*eye(d);
+            emissionPrior.k     = 0.01;
+            emissionPrior.dof   = d + 1; 
+        end
         mu = zeros(d, nstatesZ);
         Sigma = zeros(d, d, nstatesZ); 
         for s=1:nstatesZ
-           data           = Ystacked(Zstacked == s, :); 
-           mu(:, s)       = mean(data, 1)'; 
-           Sigma(:, :, s) = cov(data); 
+           m              = gaussFit(Ystacked(Zstacked == s, :), emissionPrior); 
+           mu(:, s)       = m.mu(:);  
+           Sigma(:, :, s) = m.Sigma; 
         end
         model.emission = condGaussCpdCreate(mu, Sigma); 
         model.d = d; 
@@ -39,12 +53,15 @@ switch lower(type)
     case 'discrete'
         
         nstatesY = max(Ystacked); 
+        if isempty(emissionPrior)
+            emissionPrior = 2*ones(nstatesZ, nstatesY);
+        end
         T = zeros(nstatesZ, nstatesY); 
         for s=1:nstatesZ
             data = Ystacked(Zstacked == s);
             T(s, :) = histc(data, 1:nstatesY); 
         end
-        T = normalize(T, 2); 
+        T = normalize(T + emissionPrior - 1, 2);  
         model.emission = tabularCpdCreate(T); 
         model.d = 1; 
         
--- toolbox/ProbDist/gauss/gaussFit.m
+++ toolbox/ProbDist/gauss/gaussFit.m
@@ -1,5 +1,33 @@
-function model = gaussFit(X)
-% Fit a Gaussian via MLE
-model.mu = mean(X);
-model.Sigma = cov(X);
+function model = gaussFit(X, prior)
+% Fit a Gaussian via mle / map
+% 
+% prior (optional) - parameters of a Gauss-inverseWishart distribution, 
+%                    namely mu, Sigma, dof, k 
+%
+%
+%%
+if nargin < 2 || isempty(prior) % mle
+    mu = mean(X);
+    Sigma = cov(X);
+else % map
+    
+    [N, D] = size(X);
+    xbar   = mean(X)';
+    XX     = cov(X);
+    kappa0 = prior.k;
+    m0     = prior.mu(:);
+    nu0    = prior.dof;
+    S0     = prior.Sigma;
+    mu     = (N*xbar + kappa0*m0)./(N + kappa0);
+    a      = (kappa0*N)./(kappa0 + N);
+    b      = nu0 + N + D + 2;
+    Sprior = (xbar-m0)*(xbar-m0)';
+    Sigma  = (S0 + XX + a*Sprior)./b;
+    
+    model.prior = prior; 
+    
+end
+model.mu = mu;
+model.Sigma = Sigma; 
+model.modelType = 'gauss';
 end
