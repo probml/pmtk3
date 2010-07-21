27b24c3b055511f43c15db6bd54c898938f0908c
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-21 15:37:38 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1865 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- bookDemos/Graphical_models/mrfImageDenoising.m
+++ bookDemos/Graphical_models/mrfImageDenoising.m
@@ -3,6 +3,7 @@
 %% Generate Data
 %input matrix consisting of letter A. The body of letter A is made of 1's
 %while the background is made of -1's.
+% We use a small 8x8 image so we can use exact inference
 setSeed(0);
 
 if imagesToolboxInstalled
@@ -12,14 +13,9 @@ if imagesToolboxInstalled
     [M, N] = size(img);
     img    = double(img);
     m      = mean(img(:));
-    img2   = +1*(img>m) + -1*(img<m); % -1 or +1
-    figure; 
-    imagesc(img2); 
-    title('original image'); 
-    y      = img2 + sigma*randn(size(img2)); %y = noisy signal
-    figure; imagesc(y); title('noisy copy'); 
-else
-    
+    imgOrig = img;
+    img   = +1*(img>m) + -1*(img<m); % -1 or +1
+else 
     img =[
         1     1    -1     1     1    -1     1     1
         1     1    -1     1     1    -1     1     1
@@ -30,17 +26,16 @@ else
         1     1    -1     1     1    -1     1     1
         1     1    -1     1     1    -1     1     1
         ];
-    figure;
-    imagesc(img);
-    title('original image');
-    sigma = 0.5;
-    [M, N] = size(img);
-    y = img + sigma*randn(M, N);
-    figure;
-    imagesc(y);
-    title('noisy copy');
-    
 end
+figure;
+imagesc(img); colormap('gray'); title('original image');
+sigma = 0.5;
+[M, N] = size(img);
+y = img + sigma*randn(M, N);
+figure;
+imagesc(y);colormap('gray'); title('noisy copy');
+
+
 %% Create the model
 sigma2    = sigma.^2; % we assume we know this
 localCPD  = condGaussCpdCreate( [-1 +1], [sigma2, sigma2]);
@@ -53,9 +48,12 @@ model     = mrfCreate(G, 'nodePots', nodePot, 'edgePots', edgePot,...
 
 map = mrfMap(model, 'localev', y(:)); 
 
-%nodeBels  = mrfInferNodes(model, 'localev', y(:));
-%maxMarginals = maxidx(tfMarg2Mat(nodeBels), [], 1) - 1;
+
 figure;
-%imagesc(reshape(maxMarginals, M, N));
-imagesc(reshape(map, M, N));
-title('reconstructed image');
+imagesc(reshape(map, M, N));colormap('gray'); title('MAP estimate');
+
+nodeBels  = mrfInferNodes(model, 'localev', y(:));
+maxMarginals = maxidx(tfMarg2Mat(nodeBels), [], 1) - 1;
+figure;
+imagesc(reshape(maxMarginals, M, N));
+colormap('gray'); title('marginal MAP estimate');
