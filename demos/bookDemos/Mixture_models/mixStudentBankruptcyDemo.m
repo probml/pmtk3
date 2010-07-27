f2b23146ae9edf47bd4b8deaf2eb7f2f76896489
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-27 23:52:00 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1948 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bookDemos/Mixture_models/mixStudentBankruptcyDemo.m
+++ demos/bookDemos/Mixture_models/mixStudentBankruptcyDemo.m
@@ -1,5 +1,8 @@
-%% Fit a mixture of Student T distributions to the bankruptcy data
-%
+%% Fit a mixture of Gaussians and mixture of Students to some data
+% and use the resulting models to classify
+% This example is based on fig 3.3 of Kenneth Lo's PhD thesis,
+% "Statistical methods for high throughput genomics", UBC 2009
+%PMTKauthor Hannes Bretschneider
 %%
 function mixStudentBankruptcyDemo()
 
@@ -13,24 +16,16 @@ K = 2;
 
 
 [model] = mixStudentFitEm(X, K);
-model.dof
-if 0
-for c=1:K
-fprintf( 'c = %d\n', c);
-	model.mu(:,c)
-	model.Sigma(:,:,c)
-	model.dof(c)
-end
-end
-
 [zhat] = mixStudentInfer(model, X);
 figure;
 process(model, zhat, X, Y, 'student');
+printPmtkFigure('robustMixGaussBankruptcy')
 
 [model] = mixGaussFitEm(X, K);
 [zhat] = mixGaussInfer(model, X);
 figure;
 process(model, zhat, X, Y, 'gauss');
+printPmtkFigure('robustMixStudentBankruptcy')
 
 end
 
@@ -48,11 +43,11 @@ idx1 = find(classes == 1);
 idx2 = find(classes == 2);
 error1 = sum(Y(idx1) ~= 1) + sum(Y(idx2) ~= 0);
 error2 = sum(Y(idx1) ~= 0) + sum(Y(idx2) ~= 1);
-error = error1;
+nerror = error1;
 if (error2 < error1)
 	% swap the label of classes == 1 and classes == 2
 	classes = classes + (classes == 1) - (classes == 2);
-	error = error2;
+	nerror = error2;
 end
 
 % indices of true and false positive/ negatives
@@ -66,8 +61,8 @@ h1 = plot(X(idxbankrupt1, 1), X(idxbankrupt1,2), 'bo');
 plot(X(idxbankrupt2, 1), X(idxbankrupt2,2), 'ro');
 h2 = plot(X(idxsolvent1, 1), X(idxsolvent1,2), 'b^');
 plot(X(idxsolvent2, 1), X(idxsolvent2,2), 'r^');
-title(sprintf('Bankruptcy Data using %s (red=error)', name ));
+title(sprintf('%d errors using %s (red=error)', nerror, name ));
 legend([h1 h2], 'Bankrupt', 'Solvent', 'location', 'southeast');
-fprintf('Num Errors using %s: %d\n' , name, error);
+fprintf('Num Errors using %s: %d\n' , name, nerror);
 
 end
