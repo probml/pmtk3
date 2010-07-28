75b10a3101dec81541348468ecbf0fc30b76503c
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-28 05:44:20 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1952 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/Graphical_models/mrf/mrfToMrf2.m
+++ toolbox/Graphical_models/mrf/mrfToMrf2.m
@@ -3,5 +3,5 @@ function mrf2 = mrfToMrf2(mrf, varargin)
 % for use by Mark Schmidt's UGM library
 % See mrf2Create for additional optional args
 %%
-mrf2 = factorGraphToMrf2(mrfToFactorGraph(mrf)); 
+mrf2 = factorGraphToMrf2(mrfToFactorGraph(mrf), varargin{:}); 
 end
