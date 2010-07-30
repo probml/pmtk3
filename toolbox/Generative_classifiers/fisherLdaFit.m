48c8ae2f3ceb397c45d75e20bcd59afccb0329a8
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-30 16:43:19 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1976 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/Generative_classifiers/fisherLdaFit.m
@@ -0,0 +1,17 @@
+function [W, Z]  = fisherLdaFit(Xtrain, ytrain, K)
+% Optimal linear projection from D dimensions to C-1
+% Xtrain is N*D, ytrain(i) is in {1,2,...,C}
+% W is D*K, where K <= C-1
+% Z is the project of Xtrain onto W, i.e. Xtrain*W
+% PMTKsupervisedModel fisherLda
+%%
+C = max(ytrain);
+if nargin < 3, K = C-1; end
+muC = partitionedMean(Xtrain, ytrain);
+Sw = (Xtrain  - muC(ytrain, :))'*(Xtrain  - muC(ytrain, :));
+muOverall = mean(Xtrain, 1);
+Sb = (ones(C, 1)*muOverall - muC)'*(ones(C, 1)*muOverall - muC);
+[W, D] = eig(Sw\Sb);
+W = W(:, 1:K);
+Z = Xtrain*W;
+end
