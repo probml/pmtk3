679b8550a5bf0d37fa09a5d4013586989746fc9a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-07-30 16:40:56 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@1974 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/ProbDist/discrete/discreteSample.m
@@ -0,0 +1,33 @@
+function S = discreteSample(arg1, n)
+%% Sample from a discrete distribution
+%
+% S = discreteSample(model, n); OR  
+% S = discreteSample(T, n);
+% 
+% T is of size K-by-d where K is the number of states and d is the number
+% of distributions; n is the number of samples. Each *column* of T sums to
+% one. 
+%
+% model, if specified, must have the field T
+%
+% S is of size n-by-d
+%%
+
+if isstruct(arg1)
+    model = arg1;
+    T = model.T;
+    d = model.d;
+else
+    T = arg1;
+    [K, d] = size(T); 
+end
+
+if nargin < 2
+    n = 1;
+end
+
+S = zeros(n, d); 
+for i=1:d
+   S(:, i) = sampleDiscrete(T(:, i), n, 1);
+end
+end
