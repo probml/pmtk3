b82c7543e154c51a3b9498b560a49881e7a35732
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-11 21:35:27 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2096 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- localUtil/supportTools/scrapePmtkSupport.m
+++ localUtil/supportTools/scrapePmtkSupport.m
@@ -1,15 +1,15 @@
-function dataSets = scrapePmtkSupport()
+function packages = scrapePmtkSupport()
 %% Scrape pmtkSupport.googleCode.com to get the names of all the support packages there
 excludedDirs = tokenize(getConfigValue('PMTKmetaDirs'), ',')';
 
 url = 'http://pmtksupport.googlecode.com/svn/trunk/';
 
 raw = tokenize(urlread(url), '\n');
-dataSets = filterCell(raw, @(c)startswith(c, '<li>'));
-dataSets(1) = []; % remove '..'
+packages = filterCell(raw, @(c)startswith(c, '<li>'));
+packages(1) = []; % remove '..'
 start = '<li><a href="';
-dataSets = cellfuncell(@(c)c(numel(start)+1:end), dataSets); 
-dataSets = cellfuncell(@(c)strtok(c, '/'), dataSets);
-dataSets = filterCell(dataSets, @(c)~isSubstring('.', c)); 
-dataSets = setdiff(dataSets, excludedDirs)'; 
+packages = cellfuncell(@(c)c(numel(start)+1:end), packages); 
+packages = cellfuncell(@(c)strtok(c, '/'), packages);
+packages = filterCell(packages, @(c)~isSubstring('>', c)); 
+packages = setdiff(packages, excludedDirs)'; 
 end
