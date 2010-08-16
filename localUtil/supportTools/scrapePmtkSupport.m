124da9db378a80abc375e88adfa8974a568b5011
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-16 17:38:12 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2138 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- localUtil/supportTools/scrapePmtkSupport.m
+++ localUtil/supportTools/scrapePmtkSupport.m
@@ -1,6 +1,6 @@
 function packages = scrapePmtkSupport()
 %% Scrape pmtkSupport.googleCode.com to get the names of all the support packages there
-excludedDirs = tokenize(getConfigValue('PMTKmetaDirs'), ',')';
+excludedDirs = strtrim(tokenize(getConfigValue('PMTKmetaDirs'), ',')');
 
 url = 'http://pmtksupport.googlecode.com/svn/trunk/';
 
@@ -10,6 +10,11 @@ packages(1) = []; % remove '..'
 start = '<li><a href="';
 packages = cellfuncell(@(c)c(numel(start)+1:end), packages); 
 packages = cellfuncell(@(c)strtok(c, '/'), packages);
-packages = filterCell(packages, @(c)~isSubstring('>', c)); 
+packages = filterCell(packages, @(c)~isSubstring('>', c));
+for i=1:numel(packages)
+   if startswith(packages{i}, '="')
+       packages{i} = strtrim(packages{i}(3:end)); 
+   end
+end
 packages = setdiff(packages, excludedDirs)'; 
 end
