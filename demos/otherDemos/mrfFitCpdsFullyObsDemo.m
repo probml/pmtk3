a2e9facbe2361f145c31c94a852285e15cae0a7c
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-17 01:21:27 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2153 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/otherDemos/mrfFitCpdsFullyObsDemo.m
+++ demos/otherDemos/mrfFitCpdsFullyObsDemo.m
@@ -1,6 +1,7 @@
 %% Fit the local CPDs of an mrf given an image / noisy image pair
 %
 %%
+cmap = 'bone';
 imgs = loadData('tinyImages'); 
 img = double(imgs.matlabIconGray);
 [M, N] = size(img); 
@@ -9,32 +10,26 @@ img = reshape(quantizePMTK(img(:), 'levels', ns), M, N);
 img = canonizeLabels(img); 
 nstates = max(img(:)); 
 figure;
-imagesc(img); colormap('bone'); title('original image');
+imagesc(img); 
+colormap(cmap); 
+title('original image');
 localCPD  = condGaussCpdCreate( nstates*ones(1, nstates), ones(1, 1, nstates)); 
 
 sigma = 0.1; 
 yTrain = img./nstates + sigma*randn(M, N);
 yTest  = img./nstates + sigma*randn(M, N);
-figure; imagesc(yTrain);colormap('bone'); title('noisy copy (yTrain)');
-figure; imagesc(yTest);colormap('bone'); title('noisy copy (yTest)');
+figure; imagesc(yTrain);
+colormap(cmap); 
+title('noisy copy (yTrain)');
+figure; imagesc(yTest); 
+colormap(cmap); 
+title('noisy copy (yTest)');
 localCPD = localCPD.fitFn(localCPD, img(:), yTrain(:));
 
 edgePot = exp(bsxfun(@(a, b)-abs(a-b), 1:nstates, (1:nstates)')); % will be replicated
 
 figure; imagesc(edgePot); colormap('default'); title('tied edge potential');
-
-if 1
-    nodePot = normalize(rand(1, nstates));
-else
-    yGuess = floor(rescaleData(yTrain, 1, nstates));
-    nodePot = cell(numel(img), 1);
-    for i=1:numel(img)
-        pot = zeros(1, nstates);
-        guess = yGuess(i);
-        pot(guess) = 1;
-        nodePot{i} = tabularFactorCreate(pot, i);
-    end
-end
+nodePot = normalize(rand(1, nstates));
 G         = mkGrid(M, N);
 infEngine = 'libdai';
 opts = {'TRWBP', '[updates=SEQFIX,tol=1e-9,maxiter=10000,logdomain=0,nrtrees=0]'};
@@ -44,7 +39,7 @@ mrf     = mrfCreate(G, 'nodePots', nodePot, 'edgePots', edgePot,...
 
 nodes = mrfInferNodes(mrf, 'localev', rowvec(yTest)); 
 maxMarginals = maxidx(tfMarg2Mat(nodes), [], 1);
-figure; imagesc(reshape(maxMarginals, M, N)); colormap('bone'); 
+figure; imagesc(reshape(maxMarginals, M, N)); colormap(cmap); 
 title('reconstructed image'); 
 placeFigures;
 
