baf442d22204ea6ca519002b5b78f201d0912640
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-18 22:23:40 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2182 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/Kernel_methods_for_supervised_learning/rvmPredict.m
@@ -0,0 +1,18 @@
+function yhat = rvmPredict(model, Xtest)
+
+
+switch model.outputType
+    
+    case 'binary'
+        yhat = logregPredictBayes(model, Xtest, 'vb'); 
+        
+    case 'multiclass'
+        
+        binaryPostFn = @(model, Xtest)argout(2, @logregPredictBayes, model, Xtest, 'vb'); 
+        yhat = oneVsRestClassifPredict(model, Xtest, binaryPostFn); 
+        yhat = setSupport(yhat, model.ySupport); 
+        
+    case 'regression'
+        yhat = linregPredictBayes(model, Xtest); 
+end
+end
