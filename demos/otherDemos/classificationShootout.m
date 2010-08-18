dd69d4e7823802a9331bccdeabb8106a94201ca0
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-18 23:07:58 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2184 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ demos/otherDemos/classificationShootout.m
@@ -0,0 +1,176 @@
+function results = classificationShootout()
+%% Compare different classification algorithms on a number of data sets
+%
+%%
+setSeed(0);
+split = 0.6; % 60% training data, 40% testing
+dataSets = setupData();
+nDataSets = numel(dataSets);
+
+methods = {'SVM', 'RVM', 'SMLR', 'RMLR'};
+nMethods = numel(methods);
+results = cell(nDataSets, nMethods);
+
+for i=1:nDataSets
+    gamma = pickGamma(dataSets{i});
+    for j=1:nMethods
+        fprintf('%s:%s\n', dataSets{i}.name, methods{j}); 
+        results{i, j} = evaluateMethod(methods{j}, dataSets{i}, gamma, split);
+    end
+end
+
+displayResults(results);
+
+end
+
+function gamma = pickGamma(data)
+% Pick a value for gamma, which all of the methods will use, by cv on an svm
+gammaRange = logspace(-6, 2, 200);
+fitFn = @(X, y, gamma)svmFit(X, y, 'kernel', 'rbf', 'kernelParam', gamma);
+[model, gamma] = fitCv(gammaRange, fitFn, @svmPredict, @(a, b)mean(a~=b), data.X, data.y);
+end
+
+function results = evaluateMethod(method, dataSet, gamma, split)
+%% Evaluate the performance of a method on a given data set.
+
+X      = dataSet.X;
+y      = dataSet.y;
+N      = size(X, 1);
+nTrain = floor(split*N);
+nTest  = N - nTrain;
+Xtrain = X(1:nTrain, :);
+Xtest  = X(nTrain+1:end, :);
+yTrain = y(1:nTrain);
+yTest  = y(nTrain+1:end);
+
+lambdaRange = logspace(-7, 0, 30);
+
+switch method
+    case 'SVM'
+        
+        fitFn = @(X, y, lambda)...
+            svmFit(X, y, 'C', 1./lambda, 'kernelParam', gamma, 'kernel', 'rbf');
+        predictFn = @svmPredict;
+        doCv = true;
+        
+    case 'RVM'
+        
+        model  = rvmFit(Xtrain, yTrain, gamma);
+        yhat   = rvmPredict(model, Xtest);
+        lambda = 0;
+        nerrs  = sum(yhat ~= yTest);
+        doCv   = false;
+        
+    case 'SMLR'
+        
+        fitFn = @(X, y, lambda)logregFit(X, y, ...
+            'lambda' , lambda, ...
+            'regType', 'L1',...
+            'preproc', preprocessorCreate('kernelFn',...
+            @(X1, X2)kernelRbfGamma(X1, X2, gamma)));
+        predictFn = @logregPredict;
+        doCv = true;
+        
+    case 'RMLR'
+        
+        fitFn = @(X, y, lambda)logregFit(X, y, ...
+            'lambda' , lambda, ...
+            'regType', 'L2',...
+            'preproc', preprocessorCreate('kernelFn',...
+            @(X1, X2)kernelRbfGamma(X1, X2, gamma)));
+        predictFn = @logregPredict;
+        doCv = true;
+        
+end
+
+if doCv
+    lossFn = @(yTest, yHat)mean(yHat ~= yTest);
+    nfolds = 5;
+    [model, lambda]  = fitCv(lambdaRange, fitFn, predictFn, lossFn, Xtrain, yTrain, nfolds);
+    yHat   = predictFn(model, Xtest);
+    nerrs  = sum(yHat ~= yTest);
+end
+
+results.method      = method;
+results.dataSetName = dataSet.name;
+results.nClasses    = dataSet.nClasses;
+results.nFeatures   = dataSet.nFeatures;
+results.nTrain      = nTrain;
+results.nTest       = nTest;
+results.nerrs       = nerrs;
+results.gamma       = gamma;
+results.lambda      = lambda;
+
+end
+
+function displayResults(results)
+%% Display the results in a table
+
+
+end
+
+
+
+function dataSets = setupData()
+%% Load various dataSets, and standardize the format
+
+%% Crabs
+loadData('crabs');
+X = [Xtrain; Xtest];
+y = [ytrain; ytest];
+[X, y] = shuffleRows(X, y);
+dataSets{1}.X = X;
+dataSets{1}.y = y;
+dataSets{1}.name = 'Crabs';
+dataSets{1}.nClasses  = 2;
+dataSets{1}.nFeatures = 5;
+%% fisherIris
+loadData('fisherIris');
+X = meas;
+y = canonizeLabels(species);
+[X, y] = shuffleRows(X, y);
+dataSets{2}.X = X;
+dataSets{2}.y = y;
+dataSets{2}.name = 'Iris';
+dataSets{2}.nClasses  = 3;
+dataSets{2}.nFeatures = 4;
+%% Fglass
+loadData('fglass');
+X = [Xtrain; Xtest];
+y = [ytrain; ytest];
+[X, y] = shuffleRows(X, y);
+dataSets{3}.X = X;
+dataSets{3}.y = y;
+dataSets{3}.name = 'Fglass';
+dataSets{3}.nClasses  = 6;
+dataSets{3}.nFeatures = 9;
+%% yeastUci
+loadData('yeastUci');
+y = canonizeLabels(y);
+[X, y] = shuffleRows(X, y);
+dataSets{4}.X = X;
+dataSets{4}.y = y;
+dataSets{4}.name = 'Yeast';
+dataSets{4}.nClasses  = 10;
+dataSets{4}.nFeatures = 8;
+%% ionosphere
+loadData('ionosphere')
+X = uci_ionosphere(:, 1:34);
+y = uci_ionosphere(:, 35);
+[X, y] = shuffleRows(X, y);
+dataSets{5}.X = X;
+dataSets{5}.y = y;
+dataSets{5}.name = 'Ionosophere';
+dataSets{5}.nClasses  = 2;
+dataSets{5}.nFeatures = 34;
+%% bankruptcy
+loadData('bankruptcy'); 
+X = data(:, 2:end); 
+y = data(:, 1); 
+[X, y] = shuffleRows(X, y); 
+dataSets{6}.X = X;
+dataSets{6}.y = y;
+dataSets{6}.name = 'Bankruptcy';
+dataSets{6}.nClasses  = 2;
+dataSets{6}.nFeatures = 2;
+end
--- toolbox/Kernel_methods_for_supervised_learning/svmFit.m
+++ toolbox/Kernel_methods_for_supervised_learning/svmFit.m
@@ -110,7 +110,7 @@ if isempty(fitFn)
 elseif ischar(fitFn)
     fitFn = str2func(fitFn);
 end
-fitFn
+
 %% Preprocess Data
 if standardizeX
     X = mkUnitVariance(centerCols(X));
