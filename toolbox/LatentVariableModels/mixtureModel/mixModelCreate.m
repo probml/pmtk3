7a49cd2cc503a537ce2e10f70e1f4a6fc986564a
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-27 16:33:45 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2345 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ toolbox/LatentVariableModels/mixtureModel/mixModelCreate.m
@@ -0,0 +1,13 @@
+function model = mixModelCreate(cpd, type, nmix, mixWeight, mixPrior)
+%% Create a mixture models
+% cpd is a conditional probability distribution, see e.g.
+% condGaussCpdCreate, condDiscreteProdCpdCreate, condStudentCpdCreate.
+% type is one of 'gauss', 'student', 'discrete'
+% 
+%% 
+if nargin < 5
+    mixPrior = ones(1, nmix); 
+end
+model = structure(cpd, type, nmix, mixWeight, mixPrior);
+model.mixPriorFn  = @(m)log(m.mixWeight(:))'*(m.mixPrior(:)-1);
+end
