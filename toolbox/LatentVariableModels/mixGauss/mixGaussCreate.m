dfd8ca7ec12bc0c73ec4d94e60c42431ef0b4654
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-27 19:12:56 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2358 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/LatentVariableModels/mixGauss/mixGaussCreate.m
+++
@@ -1,7 +0,0 @@
-function model = mixGaussCreate(mu, Sigma, mixweight, K)
-%% Create a mixture of Gaussian's model
-% See also mixGaussFit
-model = structure(mu, Sigma, mixweight, K); 
-model.modelType = 'mixGauss';
-
-end
