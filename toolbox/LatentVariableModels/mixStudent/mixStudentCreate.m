2c5b0966ababd365a23dc2aef0da5465f7413c29
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-27 21:49:51 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2367 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/LatentVariableModels/mixStudent/mixStudentCreate.m
+++
@@ -1,7 +0,0 @@
-function model = mixStudentCreate(mu, Sigma, mixweight, dof, K)
-%% Create a student T mixture model
-% See also mixStudentFit
-%%
-model = structure(mu, Sigma, mixweight, dof, K); 
-model.modelType = 'mixStudent';
-end
--- toolbox/LatentVariableModels/mixStudent/mixStudentLogprob.m
+++
@@ -1,7 +0,0 @@
-function [logp] = mixStudentLogprob(model, X)
-% logp(i) = log p(X(i,:) | model)
-% logPz(i,k) = log p(z=k, X(i,:), model) unnormalized
-[z, pz, logp] = mixStudentInfer(model, X); %#ok
-
-
-end
