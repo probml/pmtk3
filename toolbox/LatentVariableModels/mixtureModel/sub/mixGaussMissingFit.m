0327ced6282fa39a7308d8e005dfbdf6c6468cf8
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-27 22:07:04 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2370 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- toolbox/LatentVariableModels/mixtureModel/sub/mixGaussMissingFit.m
+++
@@ -1,6 +0,0 @@
-function [model, loglikHist] = mixGaussMissingFit(data, K, varargin)
-% Fit a mixture of Gaussians where the data may have NaN entries
-% Set doMAP = 1 to do MAP estimation (default)
-% Set diagCov = 1 to use and diagonal covariances (does not currently save
-% space)
-[model, loglikHist] = mixGaussMissingFitEm(data, K, varargin{:});
