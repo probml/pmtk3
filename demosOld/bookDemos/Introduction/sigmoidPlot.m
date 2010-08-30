fff2597d000707be165ad32a7b9c78d7e0c908d5
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-30 19:41:36 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2402 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demosOld/bookDemos/Introduction/sigmoidPlot.m
+++
@@ -1,8 +0,0 @@
-%% Plot the sigmoid function
-%
-%%
-xs = -10:0.1:10;
-p = 1./(1+exp(-xs));
-figure;
-plot(xs, p, '-', 'linewidth', 3)
-printPmtkFigure('sigmoidPlot')
--- demosOld/otherDemos/hmmStudentTest.m
+++
@@ -1,9 +0,0 @@
-%% Simple test of hmmFitEm with Student observations
-%
-%%
-setSeed(0); 
-loadData('data45');
-data = [train4'; train5'];
-d = 13;
-model = hmmFitEm(data, 2, 'student', 'verbose', true);
-
