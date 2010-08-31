39574cff47988aad7042953b0a6a2fb08a9532e3
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-08-31 03:31:14 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2423 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bookDemos/Discriminative_models_for_regression_and_classification/bayesLinRegDemo2d.m
+++ demos/bookDemos/Discriminative_models_for_regression_and_classification/bayesLinRegDemo2d.m
@@ -12,26 +12,23 @@ noiseSD = 0.2;          % Standard deviation of Gaussian noise, applied to actua
 priorPrecision = 2.0;   % Fix the prior precision, alpha. We will use a zero-mean isotropic Gaussian.
 likelihoodSD = noiseSD; % Assume the likelihood precision, beta, is known.
 likelihoodPrecision = 1/(likelihoodSD)^2; 
-
 %% Generate the training points
 xtrain = -1 + 2*rand(trainingPoints,1);
 model = struct('mu', 0, 'Sigma', noiseSD);
 noise = gaussSample(model, trainingPoints);
 ytrain = a0 + a1*xtrain + noise;
-
+%% Plot
 % Number of successive data points for which likelihood distributions will
 % be graphed. The prior and the last data point are always plotted so 0 <=
 % iter <= trainingPoints - 1.
 iter = 2; 
-
-%% Plot the prior distribution over w0, w1
+% Plot the prior distribution over w0, w1
 subplot2(iter+2,3,1,2);
 priorMean = [0;0];
 priorSigma = eye(2)./priorPrecision; %Covariance Matrix
 priorPDF = @(W)gaussProb(W,priorMean',priorSigma);
 contourPlot(priorPDF,[]);
-
-%% Plot sample lines whose parameters are drawn from the prior distribution.
+% Plot sample lines whose parameters are drawn from the prior distribution.
 subplot2(iter+2,3,1,3);
 plotSampleLines(priorMean',priorSigma,6,[])
 
@@ -52,19 +49,16 @@ for i=1:iter
   subplot2(2+iter,3,i+1,3);
   plotSampleLines(mu,sigma,6,[xtrain(1:i),ytrain(1:i)]);  
 end
-
-%% Plot likelihood for the last point alone
+% Plot likelihood for the last point alone
 last = trainingPoints;
 subplot2(2+iter,3,iter+2,1);
 likelyhoodLast = @(W) uniGaussPdf(xtrain(last),W*[1;xtrain(last)],likelihoodSD.^2);
 contourPlot(likelyhoodLast,[a0,a1]);
-
-%% Plot the posterior over all of the training data. 
+% Plot the posterior over all of the training data. 
 subplot2(2+iter,3,iter+2,2);
 [postW,mu,sigma] = update([ones(trainingPoints,1),xtrain],ytrain,likelihoodPrecision,priorMean,priorSigma);
 contourPlot(postW,[a0,a1]);
-
-%% Plot sample lines whose parameters are drawn from the posterior. 
+% Plot sample lines whose parameters are drawn from the posterior. 
 subplot2(2+iter,3,iter+2,3);
 plotSampleLines(mu',sigma,6,[xtrain,ytrain]);
 
@@ -80,12 +74,12 @@ title('data space');
 
 printPmtkFigure bayesLinRegPlot2d
 end
-%%
+
+function plotSampleLines(mu, sigma, numberOfLines,dataPoints)
 % Plot the specified number of lines of the form y = w0 + w1*x in [-1,1]x[-1,1] by
 % drawing w0, w1 from a bivariate normal distribution with specified values
 % for mu = mean and sigma = covariance Matrix. Also plot the data points as
 % blue circles. 
-function plotSampleLines(mu, sigma, numberOfLines,dataPoints)
 for i = 1:numberOfLines
     model = struct('mu', mu, 'Sigma', sigma);
     w = gaussSample(model);
@@ -103,7 +97,6 @@ if(size(dataPoints,2) == 2)
     plot(dataPoints(:,1),dataPoints(:,2),'ob');    
 end
 
-%%%%%%%%%%%%%%
 
 %Generates a colour filled contour plot of the bivariate function, 'func'
 %over the domain [-1,1]x[-1,1], plotting it to the current figure. Also plots
@@ -130,7 +123,7 @@ if(length(trueValue) == 2)
     plot(trueValue(1),trueValue(2),'+w');
 end
 end
-%%
+%
 % Given the mean = priorMu and covarianceMatrix = priorSigma of a prior
 % Gaussian distribution over regression parameters; observed data, xtrain
 % and ytrain; and the likelihood precision, generate the posterior
