06646cb26742e314436341a13c8dd0a1c59f4d1c
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-09-01 01:03:53 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2510 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- localUtil/reports/publishTutorial.m
+++ localUtil/reports/publishTutorial.m
@@ -1,8 +1,24 @@
-% Publish the pmtk tutorial.
+function publishTutorial()
+%% Publish the pmtk tutorial.
 % Remember to do an SVN checkin after running.
 % The wiki pages at http://code.google.com/p/pmtk3/wiki/Tutorial
 % are manually created - they link to the matlab-generated
 % html pages.
+%%
+recursive = false; 
+exclusions = {'html', 'attic'};  % no need to exclude if recursive is false
+src = fullfile(pmtk3Root(), 'docs', 'tutorial');
+tutFiles = filelist(src, '*.m', recursive); 
+if recursive
+   for i=1:numel(exclusions)
+      tutFiles = setdiff(tutFiles, fullfile(src, exclusions{i}), '*.m', recursive); 
+   end
+end
 
-pmtkPublish('tutFoundations')
-pmtkPublish('tutSupervised')
+for i=1:numel(tutFiles)
+   pmtkPublish(tutFiles{i});  
+end
+
+
+
+end
