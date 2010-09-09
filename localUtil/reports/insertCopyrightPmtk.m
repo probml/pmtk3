e8c0b6e3196d443f8164699eb968ca9db2ed5266
mattbdunham <mattbdunham@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-09-09 06:22:55 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2549 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- localUtil/reports/insertCopyrightPmtk.m
+++ localUtil/reports/insertCopyrightPmtk.m
@@ -2,15 +2,16 @@ function insertCopyrightPmtk(fname)
 %% Insert a copyright notice into every pmtk3 .m file
 % or into just the specified file. 
 %%
-
-% This file is from pmtk3.googlecode.com
-
+exclusions = {'docs'};
 text = 'This file is from pmtk3.googlecode.com';
 
 if nargin > 0
     insertCopyright(text, fname); 
 else
     flist = filelist(pmtk3Root(), '*.m', true);
+    for i=1:numel(exclusions)
+       flist = setdiff(flist, filelist(fullfile(pmtk3Root(), exclusions{i}), '*.m', true));  
+    end
     for i=1:numel(flist)
         fprintf('inserting into %s\n', flist{i}); 
         insertCopyright(text, flist{i}); 
