968a721ad0e810aeca6a6cf13e8c460a6fd5dc91
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-10-16 04:17:37 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2584 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bookDemos/Linear_algebra/svdImageDemo.m
+++ demos/bookDemos/Linear_algebra/svdImageDemo.m
@@ -4,7 +4,8 @@
 
 % This file is from pmtk3.googlecode.com
 
-loadData('clown'); 
+%loadData('clown'); 
+load('clown'); % builtin to matlab
 r = rank(X);
 disp(r); 
 [U, S, V] = svd(X, 0);
@@ -29,14 +30,14 @@ figure();
 plot(log(sigma(1:K)), 'r-', 'linewidth', 3)
 ylabel(sprintf('log(%s_i)','\sigma'));
 xlabel('i')
-printPmtkFigure svdImageDemoClownSigma;
+%printPmtkFigure svdImageDemoClownSigma;
 %% Scramble the data and replot (Hastie01 p491)
 N = numel(X);
 perm = randperm(N);
 X2 = reshape(X(perm), size(X));
 [U, S2, V] = svd(X2, 0);
 sigma2 = diag(S2);
-figure(); hold on
+hold on
 plot(log(sigma2(1:K)), 'g:', 'linewidth', 3)
 title('randomized');
 printPmtkFigure svdImageDemoClownSigmaScrambled;
