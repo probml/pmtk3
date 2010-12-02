4f3c467131d1b6602b2c9b597311b0256dbd0a50
murphyk2 <murphyk2@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2010-12-02 06:04:05 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2661 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/bookDemos/Bayesian_statistics/bayesChangeOfVar.m
+++ demos/bookDemos/Bayesian_statistics/bayesChangeOfVar.m
@@ -1,3 +1,5 @@
+% MC on change of variables and empirical distribution
+
 function bayesChangeOfVar()
 
 xseq = [0:0.1:10];
@@ -9,21 +11,24 @@ mu = 6; sigma = 1;
 
 N = 10^6;
 x = normrnd(mu,sigma,N,1);
-[xheight,xpoints] = hist(x,50,1);
-[ypoints,yheight] = hist(ginv(x),50,1);
-bar(xpoints,xheight*2,'r');
+% [xheight,xpoints] = hist(x,50); octave
+% [ypoints,yheight] = hist(ginv(x),50); octave
+[xheight,xpoints] = hist(x,50); % matlab
+[ypoints,yheight] = hist(ginv(x),50); % matlab
+
+bar(xpoints,xheight*2/N,'r');
 hold on;
 plot(xseq,ginv(xseq),'b','linewidth',3);
-barh(yheight,ypoints*100,'g');
+barh(yheight,ypoints*100/N,'g');
 line([mu,mu],[0,ginv(mu)],'color','c','linewidth',3);
 line([0,mu],[ginv(mu),ginv(mu)],'color','c','linewidth',3);
 
 text(9,1/10,'p_X');
 text(1,2/10,'p_Y');
-text(9,ginv(9) - 1/10,'g^{-1}');
+text(9,ginv(9) - 1/10,'g');
 hold off;
 
+
+printPmtkFigure('bayesChangeOfVar')
+
 end
-
-
-
