5a62eaf99a9b38bca2ec4f7e765f3f603ac8b471
murphyk2@gmail.com <murphyk2@gmail.com@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2012-01-04 05:58:53 +0000

    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2792 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- projects/markovLanguageExercise/stream2text.m
+++
@@ -1,6 +0,0 @@
-function text = stream2text(stream)
-  % converts an array of integers representing letters and spaces, to a
-  % charachter array
-  table = ['a':'z' ' '];
-  text = table(stream);
-  
