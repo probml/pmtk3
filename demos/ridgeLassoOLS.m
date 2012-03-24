e2c05efb7fd1c21e526eb18b7ef72c0ba2acff00
mattbdunham@gmail.com <mattbdunham@gmail.com@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2012-03-24 21:49:07 +0000

    simple plot comparing ridge lasso and OLS
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2802 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

---
+++ demos/ridgeLassoOLS.m
@@ -0,0 +1,41 @@
+function ridgeLassoOLSimageDemo
+
+X1 = [-2 2]; 
+Y1 = [-1 1]; 
+plot(X1, Y1, '-r', 'linewidth', 6);
+hold on; 
+X2 = X1; 
+Y2 = [-0.5 0.5];
+plot(X2, Y2, ':g', 'linewidth', 6); 
+
+X3a = [-2 -1]; 
+Y3a = [-0.5 0]; 
+
+X3b = [-1 1]; 
+Y3b = [0 0]; 
+
+X3c = [1 2]; 
+Y3c = [0 0.5]; 
+
+plot(X3a, Y3a, '--k', 'linewidth', 6); 
+
+legend('1', '2', '3', 'location', 'northwest'); 
+
+plot(X3b, Y3b, '--k', 'linewidth', 6); 
+plot(X3c, Y3c, '--k', 'linewidth', 6); 
+
+
+axis([-2 2 -1 1]); 
+
+xlabel('c_k', 'fontsize', 36); 
+ylabel('w_k', 'fontsize', 36); 
+set(gca, 'fontsize', 24); 
+grid on; 
+
+printPmtkFigure ridgeLassoOLS
+
+
+
+
+
+end
