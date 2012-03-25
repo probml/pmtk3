8d1284c05dde84d7683eda08bbe976c63e8183ce
murphyk2@gmail.com <murphyk2@gmail.com@b6abd7f4-f95b-11de-aa3c-59de0406b4f5>
2012-03-25 20:24:22 +0000

    Modified pareto plot
    git-svn-id: https://pmtk3.googlecode.com/svn/trunk@2806 b6abd7f4-f95b-11de-aa3c-59de0406b4f5

--- demos/paretoPlot.m
+++ demos/paretoPlot.m
@@ -5,29 +5,34 @@
 % This file is from pmtk3.googlecode.com
 
 figure; hold on;
-k = [0.5 1 2 3];
+ks = [1 1 2];
+ms = [0.5 1 1];
 [styles, colors] = plotColors();
-xs = 0.9:0.05:5;
-model.m = 1;
-nk = length(k); 
+xs = 0:0.01:5;
+nk = length(ks); 
 legendStr = cell(1, nk); 
 for i=1:nk
-  model.K = k(i); 
+  model.K = ks(i);
+  model.m = ms(i);
   p = exp(paretoLogprob(model, xs));
   plot(xs, p, styles{i}, 'color', colors(i), 'linewidth', 3);
-  legendStr{i} = sprintf('%s=%2.1f', 'k', k(i));
+  legendStr{i} = sprintf('m=%2.1f, k=%2.1f', ms(i), ks(i));
 end
 legend(legendStr)
-title('Pa(m=1, k)')
-set(gca, 'xlim', [1 5]);
+title('Pareto distribution', 'fontsize', 15)
+%set(gca, 'xlim', [1 5]);
+set(gca, 'ylim', [-0.1 2.1])
 printPmtkFigure pareto-pdf;
+
+%{
 %% Plot on a log log scale
 copyobj(gca, figure());
 set(gca, 'xscale', 'log', 'yscale', 'log');
-axis(exp([0 1.5 -6 2]));
-set(gca, 'XTick', 1:0.5:5);
-set(gca, 'YTick', [0 0.5 1 2 3]);
+%figure;
+%axis(exp([0 1.5 -6 2]));
+%set(gca, 'XTick', 1:0.5:5);
+%set(gca, 'YTick', [0 0.5 1 2 3]);
 legend(legendStr)
-title('Pa(m=1, k) - log scale - ')
+title('Pareto(m=1, k) on log scale', 'fontsize', 15)
 printPmtkFigure pareto-log-pdf;
-%%
+%}
