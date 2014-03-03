85b22c6608f3f8be2c587bfe4bedaac6f776e558
tianjun <tianjun.cpp@gmail.com>
2014-03-03 11:34:15 +0800

    add function gengerate_rst() to genetate chX.rst

--- python/utils.py
+++ python/utils.py
@@ -2,6 +2,7 @@
 
 import os
 import scipy.io as sio
+import glob
 
 PYTHON_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(os.path.dirname(PYTHON_DIR), 'pmtkdataCopy')
@@ -18,3 +19,19 @@ def load_mat(matName):
     except FileNotFoundError:
         raise
     return data
+
+
+def generate_rst():
+    """generate chX.rst in current working directory"""
+    scripts = glob.glob('*.py')
+    chapter = os.path.basename(os.getcwd())
+    rst_file = chapter + '.rst'
+    with open(rst_file, 'w') as f:
+        f.write(chapter)
+        f.write('\n========================================\n')
+        for script in scripts:
+            f.write('\n' + script[:-3])
+            f.write('\n----------------------------------------\n')
+            for img in glob.glob(script[:-3] + '*.png'):
+                f.write(".. image::" + img + "\n")
+            f.write(".. literalinclude:: " + script + "\n")
