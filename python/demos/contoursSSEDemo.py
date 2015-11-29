907dbe7370b242aaf188ce2382f68ad820ab1911
srvasude <srvasude@google.com>
2015-11-28 21:21:52 -0800

    cleanup contoursSSe and linregResiduals

--- python/demos/contoursSSEDemo.py
+++ python/demos/contoursSSEDemo.py
@@ -8,7 +8,7 @@
 #           Error surface for linear regression model.
 #
 #       Last Modified:
-#           2015-11-21
+#           2015-11-28
 
 import matplotlib.pyplot as pl
 from mpl_toolkits.mplot3d import Axes3D
@@ -16,13 +16,9 @@ import numpy as np
 from utils import poly_data_make
 
 def contoursSSEDemo():
-  np.random.seed(0)
   N = 21
-  x = np.random.randn(N,1)
-  wtrue = [1, 1]
-  sigma = 1
-  y = wtrue[0] + wtrue[1]*x + sigma * np.random.randn(N, 1)
-  X = np.concatenate((np.ones((N,1)), x.reshape(N,1)), axis=1)  
+  x,y,_,_,_,_ = poly_data_make(sampling='thibaux', n=N)
+  X = np.concatenate((np.ones((N,1)), x.reshape(N,1)), axis=1)
 
   return X,y
 
@@ -30,10 +26,13 @@ if __name__ == '__main__':
   X,y  = contoursSSEDemo()
   N = len(y)
   w = np.linalg.lstsq(X, y)[0]
-  v = np.arange(-1, 3, .1)
+  #print sum((np.dot(X,w)-y)**2)
+  v = np.arange(-6, 6, .1)
   W0, W1 = np.meshgrid(v, v)
+  #print W0, W1
   
-  SS = np.array([sum((w0*X[:,0] + w1*X[:,1].reshape(N,1) - y)**2)[0] for w0, w1 in zip(np.ravel(W0), np.ravel(W1))])
+  SS = np.array([sum((w0*X[:,0] + w1*X[:,1] - y)**2) for w0, w1 in zip(np.ravel(W0), np.ravel(W1))])
+  #print SS
   SS = SS.reshape(W0.shape)
   
   fig = pl.figure()
@@ -45,7 +44,7 @@ if __name__ == '__main__':
   fig,ax = pl.subplots()
   ax.set_title('Sum of squares error contours for linear regression')
   CS = pl.contour(W0, W1, SS)
-  pl.plot([.9492],[1.2565],'x')  
+  pl.plot([-4.351],[0.5377],'x')  
 
   pl.savefig('linregContoursSSE.png')
   pl.show()
--- python/demos/linregResiduals.py
+++ python/demos/linregResiduals.py
@@ -8,12 +8,11 @@
 #         Linear regression on data with residuals.
 #
 #       Last Modified:
-#           2015-11-21
+#           2015-11-28
 
 import matplotlib.pyplot as pl
 import numpy as np
 from utils import poly_data_make
-
 
 N = 21
 x, y, _, _, _, _ = poly_data_make(sampling='thibaux', n=N)
@@ -21,15 +20,15 @@ X = np.concatenate((np.ones((N,1)), x.reshape(N,1)), axis=1)
 w = np.linalg.lstsq(X, y)[0]
 y_estim = np.dot(X,w)
 
-pl.plot(X[:,1],y,'o')
-pl.plot(X[:,1],y_estim,'-')
+pl.plot(X[:,1], y, 'o')
+pl.plot(X[:,1], y_estim, '-')
 pl.savefig('linregResidualsNoBars.png')
 pl.show()
 
 for x0, y0, y_hat in zip(X[:,1], y, y_estim):
-  pl.plot([x0, x0],[y0,y_hat],'k-')
-pl.plot(X[:,1],y,'o')
-pl.plot(X[:,1],y_estim,'-')
+  pl.plot([x0, x0],[y0, y_hat],'k-')
+pl.plot(X[:,1], y, 'o')
+pl.plot(X[:,1], y_estim, '-')
 
 pl.savefig('linregResidualsBars.png')
 pl.show()
