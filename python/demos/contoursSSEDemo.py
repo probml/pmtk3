bd947c124f2768d0edbf797aaef1587cd8083c8e
srvasude <srvasude@google.com>
2015-11-28 18:22:04 -0800

    Used seed of 0 so that diagram most matches what is in the book

--- python/demos/contoursSSEDemo.py
+++ python/demos/contoursSSEDemo.py
@@ -13,26 +13,30 @@
 import matplotlib.pyplot as pl
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from utils import preprocessor_create
+from utils import poly_data_make
+
+
 
 def contoursSSEDemo():
-  np.random.seed(123)
-  n = 20
-  x = np.random.randn(n, 1)
-  wtrue = np.array([1, 1])
+  np.random.seed(0)
+  N = 21
+  x = np.random.randn(N,1)
+  wtrue = [1, 1]
   sigma = 1
-  y = wtrue[0] + wtrue[1]*x + sigma * np.random.randn(n, 1)
-  X = np.concatenate((np.ones((n, 1)), x.copy()), axis=1)  
+  y = wtrue[0] + wtrue[1]*x + sigma * np.random.randn(N, 1)
+  X = np.concatenate((np.ones((N,1)), x.reshape(N,1)), axis=1)  
 
   return X,y
 
 if __name__ == '__main__':
   X,y  = contoursSSEDemo()
-  n = len(y)
+  N = len(y)
   w = np.linalg.lstsq(X, y)[0]
   v = np.arange(-1, 3, .1)
   W0, W1 = np.meshgrid(v, v)
   
-  SS = np.array([sum((w0*X[:,0] + w1*X[:,1].reshape(n,1) - y)**2)[0] for w0, w1 in zip(np.ravel(W0), np.ravel(W1))])
+  SS = np.array([sum((w0*X[:,0] + w1*X[:,1].reshape(N,1) - y)**2)[0] for w0, w1 in zip(np.ravel(W0), np.ravel(W1))])
   SS = SS.reshape(W0.shape)
   
   fig = pl.figure()
@@ -44,7 +48,7 @@ if __name__ == '__main__':
   fig,ax = pl.subplots()
   ax.set_title('Sum of squares error contours for linear regression')
   CS = pl.contour(W0, W1, SS)
-  pl.plot([.73180031],[.88656731],'x')
-  
+  pl.plot([.9492],[1.2565],'x')  
+
   pl.savefig('linregContoursSSE.png')
   pl.show()
