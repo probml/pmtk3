45c214686ee32aad869f9ff2ec13e2b926218ef9
jfainberg <jfainberg@users.noreply.github.com>
2015-12-21 15:19:10 +0000

    xs starts at 0; bar() by default starts at 1

--- demos/poissonPlotDemo.m
+++ demos/poissonPlotDemo.m
@@ -10,7 +10,7 @@ for i=1:4
   figure;
   xs = 0:25;
   mu = exp(poissonLogprob(struct('lambda', lambdas(i)), xs));
-  h = bar(mu);
+  h = bar(xs, mu);
   title(sprintf('Poi(%s=%5.3f)', '\lambda', lambdas(i)))
   printPmtkFigure(sprintf('poissonDemo%d', i))
 end
