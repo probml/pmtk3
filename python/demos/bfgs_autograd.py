Fixed bug in loss function of linear regression (off by factor of 0.5), added finite difference check to gradient computation (thanks to Dougal Maclaurin).

python/demos/bfgs_autograd.py
@@ -4,7 +4,7 @@ from scipy.optimize import minimize
 
 def squared_loss(y_pred, y):
     N = y.shape[0]
-    return sum(np.square(y - y_pred))/N
+    return 0.5*sum(np.square(y - y_pred))/N
 
 def gradient(params, X, y):
     obj_fun = lambda params: squared_loss(np.dot(X, params),  y) 

python/demos/linreg_1d_batch_demo.py
@@ -2,7 +2,7 @@
 
 import autograd
 import autograd.numpy as np
-#import numpy as np
+import autograd.util 
 import matplotlib.pyplot as plt
 
 from utils.optim_util import bfgs_fit, MinimizeLogger, plot_loss_trace, plot_param_trace
@@ -19,27 +19,34 @@ def main():
     params_init = np.zeros(D)
     logger = MinimizeLogger(LinregModel.objective, (Xtrain, ytrain), print_freq=10)
     
+    # Check that OLS and BFGS give same result
     params_ols, loss_ols = LinregModel.ols_fit(Xtrain, ytrain)
-    #obj_fun = lambda params: LinregModel.objective(params, Xtrain, ytrain)
-    #grad_fun = lambda params: LinregModel.gradient(params, Xtrain, ytrain)
     obj_fun = LinregModel.objective
     grad_fun = LinregModel.gradient
     params_bfgs, loss_bfgs, logger = bfgs_fit(params_init, obj_fun, grad_fun, (Xtrain, ytrain), logger) 
     assert(np.allclose(params_bfgs, params_ols))
     assert(np.allclose(loss_bfgs, loss_ols))
 
-    
+    # Check that analytic gradient and automatic gradient give same result
     grad_fun = autograd.grad(obj_fun)
+    grad_auto = grad_fun(params_init, Xtrain, ytrain)
+    grad_finite_diff = autograd.util.nd(lambda p : obj_fun(p, Xtrain, ytrain), params_init)[0]
+    grad_analytic = LinregModel.gradient(params_init, Xtrain, ytrain)
+    assert(np.allclose(grad_auto, grad_finite_diff))
+    assert(np.allclose(grad_auto, grad_analytic))
+
     params_autograd, loss_autograd = bfgs_fit(params_init, obj_fun, grad_fun, (Xtrain, ytrain)) 
     assert(np.allclose(params_bfgs, params_autograd))
     assert(np.allclose(loss_bfgs, loss_autograd))
     
     print "All assertions passed"
 
+    # Plot loss vs time
     print logger.obj_trace
     ax = plot_loss_trace(logger.obj_trace, loss_ols) 
     ax.set_title('BFGS')
     
+    # Plot 2d trajectory of parameter values over time
     loss_fun = lambda w0, w1: LinregModel.objective([w0, w1], xtrain, ytrain)
     ax = plot_error_surface(loss_fun, params_true)
     plot_param_trace(logger.param_trace, ax)

python/demos/linreg_1d_sgd_demo.py
@@ -14,25 +14,23 @@ from utils.linreg_model import LinregModel
 def make_expt_config(N):
     batch_sizes = [N, 10]
     lr_decays = [0.99]
-    momentums = [0]
-    autograds = [False, True]
-    init_lr = 0.01
+    momentums = [0, 0.9]
+    init_lr = 0.005
     n_steps = 200
     expt_config = []
     for batch_size in batch_sizes:
         for lr_decay in lr_decays:
             for momentum in momentums:
-                for use_autograd in autograds:
-                    config = {'batch_size': batch_size, 'lr_decay': lr_decay,
-                        'momentum': momentum, 'use_autograd': use_autograd,
-                        'init_lr': init_lr, 'n_steps': n_steps, 'N': N}
-                    expt_config.append(config)
+                config = {'batch_size': batch_size, 'lr_decay': lr_decay,
+                    'momentum': momentum,
+                    'init_lr': init_lr, 'n_steps': n_steps, 'N': N}
+                expt_config.append(config)
     return expt_config
     
 def config_to_str(config):
     batch_size_frac = config['batch_size'] / np.float(config['N'])
-    cstr = 'B{:0.2f}-L{:0.2f}-M{}-AG{}'.format(batch_size_frac, config['lr_decay'],
-            config['momentum'], config['use_autograd'])
+    cstr = 'B{:0.2f}-L{:0.2f}-M{}'.format(batch_size_frac, config['lr_decay'],
+            config['momentum'])
     return cstr
             
 def main():
@@ -42,17 +40,17 @@ def main():
     Xtrain = np.c_[np.ones(N), xtrain] # add column of 1s
     w_ols, loss_ols = LinregModel.ols_fit(Xtrain, ytrain)
     
-    expt_config = make_expt_config(N)
-    nexpts = len(expt_config)
+    expt_configs = make_expt_config(N)
+    nexpts = len(expt_configs)
     print nexpts
     
     nrows, ncols = nsubplots(nexpts)
     #nrows, ncols = 4, 2
     loss_trace_fig = plt.figure("loss trace fig")
     param_trace_fig = plt.figure("param trace fig")
-    folder = '/Users/kpmurphy/github/pmtk3/python/figures/'
+    folder = 'figures'
     
-    for expt, config in enumerate(expt_config):
+    for expt_num, config in enumerate(expt_configs):
         logger = sgd.SGDLogger(print_freq=10)
         np.random.seed(1)
         batchifier = sgd.MiniBatcher(Xtrain, ytrain, config['batch_size'])
@@ -65,19 +63,19 @@ def main():
         print config
 
         obj_fun = LinregModel.objective
-        grad_fun = LinregModel.gradient
-        #grad_fun = autograd.grad(obj_fun)
+        #grad_fun = LinregModel.gradient
+        grad_fun = autograd.grad(obj_fun)
         result = sgd.sgd_minimize(initial_params, obj_fun, grad_fun,
             batchifier, config['n_steps'], lr_fun, config['momentum'],
-            config['use_autograd'], callback=logger.update)
-
+            callback=logger.update)
         print result
                 
-        ax = loss_trace_fig.add_subplot(nrows, ncols, expt+1)
+        plotnum = expt_num + 1
+        ax = loss_trace_fig.add_subplot(nrows, ncols, plotnum)
         plot_loss_trace(logger.obj_trace, loss_ols, ax)
         ax.set_title(ttl)
         
-        ax = param_trace_fig.add_subplot(nrows, ncols, expt+1)
+        ax = param_trace_fig.add_subplot(nrows, ncols, plotnum)
         loss_fun = lambda w0, w1: LinregModel.objective([w0, w1], xtrain, ytrain)
         plot_error_surface(loss_fun, params_true, ax)
         plot_param_trace(logger.param_trace, ax) 