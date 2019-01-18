e48f5ed47fd2c6c0a5b030f6b1899eadf0c47e03
Kevin Murphy <murphyk@gmail.com>
2019-01-18 05:15:45 +0000

    Use joint model for codes, since factored model was wrong.

--- demos/bayesianCodeBreaker.m
+++ demos/bayesianCodeBreaker.m
@@ -16,27 +16,21 @@ If f(x) in {1,..,K} then we can represent p(f(x)) as a histogram
 over K numbers, and hence the prior over functions as a row-stochastic
 matrix of size A^L * K.
 
-
 If the prior over f is uniform, the agent cannot beat brute force
 enumeration. So instead we will assume the prior has the following
 structure, which in this cases matches the true structure of f*
 (shown below for L=2):
   
 
-c1 x1 x2 c2
- \  /  \  /
-   d1   d2
+x1  x2   c
+ \   |   /
     \   /
       y
 
 where
-p(ci) is the (initially uniform) prior over letter i of the code  
-p(di|ci,xi) = delta(di = (ci != xi)), di in {0,1}
-p(y|d(1:L)) = delta(y = sum_l dl), y in {0, ..., L}
+p(c) is the (initially uniform) prior over codes 
+p(y|x(1:L), c) = delta(y = hammingDistance(x,c)), y in {0, ..., L}
 
-Hence p(f) is a uniform distribution over functions of the form
- f(x) = hammingDistance(x, c)
-where we put a uniform prior over codes c
 
 Note that the agent gets "partial credit" for its guess x,
 in the form of a "distance to target" signal, rather than just 0-1
@@ -46,10 +40,6 @@ For example, if we observe y=1 after guessing x=[1,2,1], we know that
 strings that are edit distance 1 from x (namely: 221, 321, 111, 131, 122, 123)
 This means we can restrict our search to radius 2 from x.
 
-Note that even if the prior over codes is factored,
-p(c) = prod_l p(cl), the posterior is completely correlated due to 
-explaining away induced by y. Hence the posterior over functions is
-represented by a single node f.
 
 Note that although y is treated as a categorical variable, we can
 encode metric structure into the problem by the way we construct p(y|d).
@@ -75,108 +65,81 @@ We plot f*(bt) vs time t for different algorithms:
 
 %}
 
+
+
+
 function bayesianCodeBreaker()
 
 
 %% Bayes net code
- function [G, cnodes, xnodes, dnodes, ynode] = makeBayesNetGraph(L)
-     %{
-     for L=3
-     c1[1] c2[2] c3[3]  x1[4] x2[5] x3[6] 
-         d1[7] d2[8] d3[9]
-             y[9]
-     %}
-    cnodes = (1:L);
-    xnodes = (L+1):(2*L);
-    dnodes = (2*L + 1):(3*L);
-    ynode = 3*L + 1;
-    nnodes = 3*L + 1;
+ function [G, cnode, xnodes, ynode] = makeBayesNetGraph(L)
+    xnodes = (1:L);
+    cnode = L+1;
+    ynode = L+2;
+    nnodes = L+2;
     G = zeros(nnodes, nnodes);
-    for i=1:L
-        G(cnodes(i), dnodes(i))=1;
-        G(xnodes(i), dnodes(i))=1;
-    end
-    G(dnodes, ynode) = 1;
+    G(xnodes, ynode) = 1;
+    G(cnode, ynode) = 1;
  end
 
 
 
-function CPT = makeDiffCPT(A)
-    % CPT(c,x,d) = delta(d = (c != x))
-    % Note that event d=0 is encoded by 1, and d=1 by 2.
-    CPT = zeros(A, A, 2);
-    for c=1:A
-        for x=1:A
-            if c==x
-                CPT(c,x,1) = 1.0;
-            else
-                CPT(c,x,2) = 1.0;
-            end
-        end
-    end
-end
-
-    function CPT = makeSumCPT(L)
-        % CPT(dnode1,...,dnodeL,y) = delta(y = sum_l dnodel)
-        % The event y=0 is encoded by 1, ..., y=L is encoded by L+1
-        CPT = zeros(2^L, L+1);
-        bits = ind2subv(2*ones(1,L), 1:2^L)-1; % binary values for dnodes(1:L)
-        bitsum = sum(bits, 2); % sum over columns
-        %ndx = subv2ind([2^L, L+1], [bits bitsum]);
-        %CPT(bits+1, bitsum+1) = 1.0;
-        %CPT(ndx) = 1.0;
-        for i=1:2^L
-            CPT(i, bitsum(i)+1) = 1.0;
-        end
-        CPT = reshape(CPT, [2*ones(1,L), L+1]);
-    end
-
-  
-
-    function CPTs = makeCodePriors(L, A, code)
-        % CPTs{i} = p(ci) = prior over code letter i
+    function prior = makeCodePriors(L, A, code)
         % set code(i)  = 0 if unknown (uniform prior for ci)
         % set code(i) in {1,..,A} to use delta function for that slot
-        CPTs = cell(1, L);
-        for i=1:L
-            if code(i) > 0
-                % delta function
-                CPTs{i} = zeros(1, A);
-                CPTs{i}(code(i)) = 1.0;
-            else
-                CPTs{i} = normalize(ones(1,A)); % uniform prior
+        cs = ind2subv(A*ones(1,L), 1:A^L);
+        prior = ones(1, A^L);
+        for i=1:A^L
+            for l=1:L
+             if (code(l) > 0) && (cs(i,l) ~= code(l)) 
+                    prior(i) = 0;
+             end
             end
         end
+        prior = normalize(prior);
     end
 
-function pgm = makeBayesNet(L, A, codeCPTs)
-    [G, cnodes, xnodes, dnodes, ynode] = makeBayesNetGraph(L);
+    function CPT = makeHammingCPT(L, A)
+        % CPT(x1,...,xL,c,y)
+        nstr = A^L;
+        CPT = zeros(nstr, nstr, L+1);
+        sz = A*ones(1,L);
+        for xndx = 1:nstr
+            x = ind2subv(sz, xndx);
+            for cndx = 1:nstr
+                c = ind2subv(sz, cndx);
+                h = hammingDistance(x, c);
+                y = h + 1;
+                CPT(xndx, cndx, y) = 1.0;
+            end
+        end
+        CPT = reshape(CPT, [sz nstr L+1]);
+    end
+
+function pgm = makeBayesNet(L, A, codePrior)
+    [G, cnode, xnodes, ynode] = makeBayesNetGraph(L);
     nnodes = size(G,1);
     CPTs = cell(1, nnodes);
+    CPTs{cnode} = codePrior;
     for i=1:L
-        CPTs{cnodes(i)} = codeCPTs{i};
         CPTs{xnodes(i)} = normalize(ones(1,A)); % will always be clamped
-        CPTs{dnodes(i)} = makeDiffCPT(A);
     end
-    CPTs{ynode} = makeSumCPT(L);
+    CPTs{ynode} = makeHammingCPT(L, A);
     nyvals = L+1;
     dgm = dgmCreate(G, CPTs);
     nnodes = size(G, 1);
-    pgm = structure(dgm, nnodes, cnodes, xnodes, dnodes, ynode, L, A, nyvals);
+    pgm = structure(dgm, nnodes, cnode, xnodes, ynode, L, A, nyvals);
 end
 
- function pgm = bayesianUpdate(pgm, x, y)
+ function [pgm, codePost] = bayesianUpdate(pgm, x, y)
      L  = pgm.L; A = pgm.A; 
         clamped = zeros(1, pgm.nnodes);
         clamped(pgm.xnodes) = x;
         clamped(pgm.ynode) = y+1; % since domain is {1,2,...,K}
         nodeBels = dgmInferNodes(pgm.dgm, 'clamped', clamped);
-        codeBel = nodeBels(pgm.cnodes);
-        codeCPT = cell(1,L);
-        for i=1:L
-            codeCPT{i} = codeBel{i}.T;
-        end
-        pgm = makeBayesNet(L, A, codeCPT);
+        codeBel = nodeBels(pgm.cnode);
+        codePost = codeBel{1}.T(:)';
+        pgm = makeBayesNet(L, A, codePost);
     end
 
     function bel = probYGivenX(pgm, x)
@@ -210,28 +173,48 @@ end
 
 
      function e = expectedValue(Fmodel, x)
-            L = Fmodel.L; A = Fmodel.A; prob = Fmodel.Fprob;
+            L = Fmodel.L; A = Fmodel.A; 
             assert(L == length(x));
             ndx = subv2ind(A*ones(1,L), x);
-            prob = reshape(prob, [A^L, length(Fmodel.Frange)]);
+            prob = reshape(Fmodel.Fprob, [A^L, length(Fmodel.Frange)]);
             e = sum(prob(ndx, :) .* Fmodel.Frange);
      end
 
+  function e = expectedImprovement(Fmodel, x, thresh)
+            L = Fmodel.L; A = Fmodel.A; 
+            assert(L == length(x));
+            ndx = subv2ind(A*ones(1,L), x);
+            prob = reshape(Fmodel.Fprob, [A^L, length(Fmodel.Frange)]);
+            probGivenX = prob(ndx, :);
+            fn = @(y) max(0, (thresh-y)); % if y < thresh, we improve
+            ys = Fmodel.Frange;
+            e = 0;
+            for i=1:length(ys)
+                e = e + probGivenX(i)*fn(ys(i));
+            end
+     end
 
     function dispSurrogate(Fmodel, trueCode, varargin)
-        [str, doPlot] = process_options(varargin, 'str', '', 'doPlot', true);
-        L = Fmodel.L;
+        [str, doPlot, drawLines, incumbentVal, showAll] = process_options(varargin, ...
+            'str', '', 'doPlot', true, 'drawLines', true, ...
+            'incumbentVal', 0, 'showAll', false);
+        L = Fmodel.L; A = Fmodel.A;
     xs = Fmodel.Fdomain;
     e = applyFun(xs, @(x) expectedValue(Fmodel, x));
+    eic = applyFun(xs, @(x) expectedImprovement(Fmodel, x, incumbentVal));
     o = applyFun(xs, @(x) hammingDistance(x, trueCode));
-   [ndx, evals] = argminima(e); ovals = o(ndx);
+    [ndx] = argmaxima(eic);
     nmin = length(ndx);
-    
     if doPlot
         Nstr = length(e); 
-        figure; plot(1:Nstr, e, 'r-', 1:Nstr, o, 'b:', 'linewidth', 2);
+        figure; 
+        if drawLines
+            plot(1:Nstr, e, 'r-', 1:Nstr, eic, 'g--', 1:Nstr, o, 'b:', 'linewidth', 2);
+        else
+            plot(1:Nstr, e, 'rx', 1:Nstr, eic, 'g*', 1:Nstr, o, 'bo', 'linewidth', 2);
+        end
         set(gca, 'ylim', [-0.1 L+0.1]);
-        legend('expected', 'true')
+        legend('expected', 'eic', 'true', 'location', 'southeast')
         title(str);
         set(gca, 'xtick', ndx);
         args = cell(1, nmin);
@@ -241,10 +224,18 @@ end
         xticklabelRot(args, 45, 8);
     end
 
+    if showAll
+        ndx = 1:length(o);
+    else
+        [ndx] = argmaxima(eic);
+    end
+    nmin = length(ndx);
     separator = repmat('|', nmin, 1);
     disp(str)
+    disp('args, expected, eic, objective');
+    evals = e(ndx); eicvals = eic(ndx); ovals = o(ndx);
     disp([num2str(xs(ndx,:)), separator, num2str(evals(:)), ...
-            separator, num2str(ovals(:))])
+            separator, num2str(eicvals(:)), separator, num2str(ovals(:))])
      
     end
 
@@ -263,6 +254,12 @@ end
 
     function [ndx, vals] = argminima(scores)
         m = min(scores);
+        ndx = find(scores==m);
+        vals = scores(ndx); % N copies of the value m
+    end
+
+ function [ndx, vals] = argmaxima(scores)
+        m = max(scores);
         ndx = find(scores==m);
         vals = scores(ndx); % N copies of the value m
     end
@@ -320,20 +317,22 @@ end
     assert(expectedValue(Fmodel, [2,2,1]) == 0.25*2 + 0.75*3)
     
     % The prior says that the only optima can be 111, 112, 113, or 114.
-    dispSurrogate(Fmodel, code, 'str', 'oracle2', 'doPlot', true)
+    dispSurrogate(Fmodel, code, 'str', 'oracle2', 'doPlot', false)
     
   
     % If we query 112, we discover f(112)=1, so we still have 3 candidates
     x=[1,1,2]; y = hammingDistance(x, code);
     postPgm = bayesianUpdate(priorPgm, x, y);
     Fmodel = makeSurrogateFromPgm(postPgm);
-    dispSurrogate(Fmodel, code, 'str', 'oracle2+f(112)', 'doPlot', true)
+    dispSurrogate(Fmodel, code, 'str', 'oracle2+f(112)', 'doPlot', false)
     
+  
       % If we query 111, we discover f(111)=0 and we're done!
     x=[1,1,1]; y = hammingDistance(x, code);
     postPgm = bayesianUpdate(priorPgm, x, y);
     Fmodel = makeSurrogateFromPgm(postPgm);
-    dispSurrogate(Fmodel, code, 'str', 'oracle2+f(111)', 'doPlot', true)
+    dispSurrogate(Fmodel, code, 'str', 'oracle2+f(111)', 'doPlot', false)
+  
     end
 
 
@@ -346,7 +345,7 @@ end
     codePriors = makeCodePriors(L, A, codePrior);
     priorPgm = makeBayesNet(L, A, codePriors);
     Fmodel = makeSurrogateFromPgm(priorPgm);
-    dispSurrogate(Fmodel, code, 'str', 'oracle1')
+    dispSurrogate(Fmodel, code, 'str', 'oracle1', 'doPlot', false)
 
     % The prior allows for 4^2=16 possible codes
 
@@ -354,19 +353,67 @@ end
     x=[1,1,2]; y = hammingDistance(x, code);
     postPgm = bayesianUpdate(priorPgm, x, y);
     Fmodel = makeSurrogateFromPgm(postPgm);
-    dispSurrogate(Fmodel, code, 'str', 'oracle1+f(112)', 'doPlot', true)
+    incumbent = x; incumbentVal = y;
+    dispSurrogate(Fmodel, code, 'str', 'oracle1+f(112)', 'doPlot',false, ...
+        'incumbentVal', y)
     
       % If we query 111, we discover f(111)=0 and we're done!
     x=[1,1,1]; y = hammingDistance(x, code);
     postPgm = bayesianUpdate(priorPgm, x, y);
     Fmodel = makeSurrogateFromPgm(postPgm);
-    dispSurrogate(Fmodel, code, 'str', 'oracle1+f(111)', 'doPlot', true)
+    dispSurrogate(Fmodel, code, 'str', 'oracle1+f(111)', 'doPlot', false)
+    end
+
+ function testUnifPrior()
+        L = 3; A = 4;
+    % Uniform prior means 4^3=64 codes.
+    code = [1,1,1];
+    codePrior = [0,0,0];
+    codePriors = makeCodePriors(L, A, codePrior);
+    priorPgm = makeBayesNet(L, A, codePriors);
+    Fmodel = makeSurrogateFromPgm(priorPgm);
+    incumbentVal = max(Fmodel.Frange);
+    dispSurrogate(Fmodel, code, 'str', 'unif', 'incumbentVal', ...
+        incumbentVal, 'drawLines', false)
+
+    % If we query 112, we discover f(112)=1, 
+    % so the posterior is 1 away from 112 and has 9 strings,
+    % each with prob 1/9=0.111: 
+    % 111, 113, 114,  122, 132, 142,  212, 312, 412
+    %
+    x=[1,1,2]; y = hammingDistance(x, code);
+    incumbentVal = y;
+    [postPgm, postCodes] = bayesianUpdate(priorPgm, x, y);
+    Fmodel = makeSurrogateFromPgm(postPgm);
+    dispSurrogate(Fmodel, code, 'str', 'f(112)', 'incumbentVal', ...
+        incumbentVal, 'drawLines', false)
+ 
+    % If we now query 114, we discover f(114)=1, 
+    % so the posterior is 1 away from 112 and 114 and has 2 strings,
+    % each with prob 1/2=0.5: 
+    % 111, 113
+    %
+    x=[1,1,4]; y = hammingDistance(x, code);
+    incumbentVal = min(y, incumbentVal);
+    [postPgm, postCodes] = bayesianUpdate(postPgm, x, y);
+    Fmodel = makeSurrogateFromPgm(postPgm);
+    dispSurrogate(Fmodel, code, 'str', 'f(112),f(114)', 'incumbentVal', ...
+        incumbentVal, 'drawLines', false)
+    
+    x=[1,1,3]; y = hammingDistance(x, code);
+    incumbentVal = min(y, incumbentVal);
+    [postPgm, postCodes] = bayesianUpdate(postPgm, x, y);
+    Fmodel = makeSurrogateFromPgm(postPgm);
+    dispSurrogate(Fmodel, code, 'str', 'f(112),f(114),f(113)', 'incumbentVal', ...
+        incumbentVal, 'drawLines', false)
+ 
     end
 
 %% Main
 
 %testOraclePrior3()
 %testOraclePrior2()
-testOraclePrior1()
+%testOraclePrior1()
+testUnifPrior()
 
 end
